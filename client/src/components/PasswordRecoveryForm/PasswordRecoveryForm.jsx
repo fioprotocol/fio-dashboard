d1649c918c467e1fff49d6e21cf304d3dd1599e1
trukhilio <trukhantech@gmail.com>
2021-04-01 14:03:43 +0300

    fix code style

--- client/src/components/PasswordRecoveryForm/PasswordRecoveryForm.jsx
+++ client/src/components/PasswordRecoveryForm/PasswordRecoveryForm.jsx
@@ -58,29 +58,37 @@ const PasswordRecoveryForm = props => {
   const validateForm = values => {
     const errors = {};
 
-    if (!values.recoveryQuestionOne) {
+    const {
+      recoveryQuestionOne,
+      recoveryQuestionTwo,
+      recoveryAnswerOne,
+      recoveryAnswerTwo,
+    } = values;
+
+    const { length: lengthOne } = recoveryAnswerOne || {};
+    const { length: lengthTwo } = recoveryAnswerTwo || {};
+    const { min_length: minLengthOne } = recoveryQuestionOne || {};
+    const { min_length: minLengthTwo } = recoveryQuestionTwo || {};
+
+    if (!recoveryQuestionOne) {
       errors.recoveryQuestionOne = 'Please Select Question';
     }
-    if (!values.recoveryQuestionTwo) {
+    if (!recoveryQuestionTwo) {
       errors.recoveryQuestionTwo = 'Please Select Question';
     }
 
-    if (!values.recoveryAnswerOne) {
+    if (!recoveryAnswerOne) {
       errors.recoveryAnswerOne = 'Answer Field Should be Filled';
     }
-    if (values.recoveryAnswerOne && values.recoveryQuestionOne) {
-      if (values.recoveryAnswerOne.length < values.recoveryQuestionOne.min_length) {
-        errors.recoveryAnswerOne = `Must have at least ${values.recoveryQuestionOne.min_length} characters`;
-      }
+    if (lengthOne < minLengthOne) {
+      errors.recoveryAnswerOne = `Must have at least ${minLengthOne} characters`;
     }
 
-    if (!values.recoveryAnswerTwo) {
+    if (!recoveryAnswerTwo) {
       errors.recoveryAnswerTwo = 'Answer Field Should be Filled';
     }
-    if (values.recoveryAnswerTwo && values.recoveryQuestionTwo) {
-      if (values.recoveryAnswerTwo.length < values.recoveryQuestionTwo.min_length) {
-        errors.recoveryAnswerTwo = `Must have at least ${values.recoveryQuestionTwo.min_length} characters`;
-      }
+    if (lengthTwo < minLengthTwo) {
+      errors.recoveryAnswerTwo = `Must have at least ${minLengthTwo} characters`;
     }
 
     return errors;
@@ -110,13 +118,15 @@ const PasswordRecoveryForm = props => {
       input: { type, value, name },
     } = props;
 
+    const { question } = value;
+
     return (
       <div className={classes.header} onClick={() => showQuestions(name)}>
         <div className={classes.text}>
           {type === '1'
-            ? value.question || 'Choose Recovery Question 1'
+            ? question || 'Choose Recovery Question 1'
             : type === '2'
-            ? value.question || 'Choose Recovery Question 2'
+            ? question || 'Choose Recovery Question 2'
             : null}
         </div>
         <FontAwesomeIcon icon="chevron-right" className={classes.icon} />
@@ -130,22 +140,17 @@ const PasswordRecoveryForm = props => {
     } = props;
     const { change, getState } = useForm();
 
-    const values = getState().values;
-    const question = name.question;
-    const formSelectedOne =
-      values &&
-      values.recoveryQuestionOne &&
-      values.recoveryQuestionOne.question;
-    const formSelectedTwo =
-      values &&
-      values.recoveryQuestionTwo &&
-      values.recoveryQuestionTwo.question;
+    const { values } = getState() || {};
+    const { recoveryQuestionOne, recoveryQuestionTwo } = values || {};
+    const { question: questionOne } = recoveryQuestionOne || {};
+    const { question: questionTwo } = recoveryQuestionTwo || {};
+    const { question } = name || {};
 
     let isSelected = false;
-    if (question === formSelectedOne) {
+    if (question === questionOne) {
       isSelected = true;
     }
-    if (question === formSelectedTwo) {
+    if (question === questionTwo) {
       isSelected = true;
     }
 
@@ -158,7 +163,7 @@ const PasswordRecoveryForm = props => {
         key={name}
         onClick={() => !isSelected && setQuestion(change, name)}
       >
-        {name.question}
+        {question}
       </div>
     );
   };
