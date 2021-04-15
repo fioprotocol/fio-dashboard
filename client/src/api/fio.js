bea3db5d0e08296a6a96168fa1cb127426eb8635
andreyvEze <andreyv@eze.tech>
2021-04-15 15:15:51 +0300

    Changed window fetch to fix CORS.

--- client/src/api/fio.js
+++ client/src/api/fio.js
@@ -1,17 +1,11 @@
 import { FIOSDK } from '@fioprotocol/fiosdk';
 
-const fetchJson = (uri, opts = {}) => {
-  if (opts.headers) {
-    delete opts.headers['Content-Type'];
-  }
-  return fetch(uri, { ...opts });
-};
 export default class Fio {
   baseurl = process.env.REACT_APP_FIO_BASE_URL;
   publicFioSDK = null;
 
   constructor() {
-    this.publicFioSDK = new FIOSDK('', '', this.baseurl, fetchJson);
+    this.publicFioSDK = new FIOSDK('', '', this.baseurl, window.fetch);
   }
 
   availCheck = fioName => {
