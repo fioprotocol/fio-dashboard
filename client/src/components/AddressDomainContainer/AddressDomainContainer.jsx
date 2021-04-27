f9442c6007f920e4f497f23f98b2e373471c4245
trukhilio <trukhantech@gmail.com>
2021-04-27 18:00:17 +0300

    BD-2420 address/domain cart component

--- client/src/components/AddressDomainContainer/AddressDomainContainer.jsx
+++ client/src/components/AddressDomainContainer/AddressDomainContainer.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import AddressFrom from '../../components/AddressDomainForm';
-import LayoutContainer from '../../components/LayoutContainer/LayoutContainer';
-
-import AddressDomainBadge from '../../components/AddressDomainBadge/AddressDomainBadge';
+import AddressFrom from '../AddressDomainForm';
+import LayoutContainer from '../LayoutContainer/LayoutContainer';
+import AddressDomainBadge from '../AddressDomainBadge/AddressDomainBadge';
+import AddressDomainCart from '../AddressDomainCart';
+import { currentScreenType } from '../../screenType';
+import { SCREEN_TYPE } from '../../constants/screen';
 import classes from './AddressDomainContainer.module.scss';
 
 const AddressDomainContainer = props => {
   const { title, type, formNameGet } = props;
+  const { screenType } = currentScreenType();
+  const isDesktop = screenType === SCREEN_TYPE.DESKTOP;
   return (
     <LayoutContainer title={title}>
       <div className={classes.container}>
         <div className={classes.cardContainer}>
           <AddressFrom formNameGet={formNameGet} type={type} />
+          {!isDesktop && <AddressDomainCart />}
           <AddressDomainBadge type={type} />
         </div>
         <hr className={classes.vertical} />
-        <div className={classes.cart}>CART</div>{' '}
-        {/*todo: plug, replace with cart component */}
+        {isDesktop && <AddressDomainCart />}
       </div>
     </LayoutContainer>
   );
