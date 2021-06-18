552c8b2d1feeee8fb55181ab3f56f8c0b4419f7e
trukhilio <trukhantech@gmail.com>
2021-06-18 16:15:47 +0300

    make dashboard page as home page if user logged in

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,11 +1,20 @@
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
+
+import { compose } from '../../utils';
+
+import { isAuthenticated } from '../../redux/edge/selectors';
 
 import HomePage from './HomePage';
 
-const selector = null;
-
-const actions = {};
+const reduxConnect = connect(
+  createStructuredSelector({
+    isAuthenticated,
+  }),
+  {},
+);
 
 export { HomePage };
 
-export default connect(selector, actions)(HomePage);
+export default withRouter(compose(reduxConnect)(HomePage));
