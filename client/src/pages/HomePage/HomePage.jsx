756dc967330502adf4da4f2a1f0c0ee0920bd39e
Oleksii Trukhanov <trukhantech@gmail.com>
2021-06-22 12:10:04 +0300

    BD-2422. Registration flow fixes. (#91)
    * clear homepage address redux form after select-address form unmount
    
    * make dashboard page as home page if user logged in
    
    * fix price recalculation on cart page, fix item price aftereffects on purchasing

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import AddressWidget from '../../components/AddressWidget';
 import DomainsBanner from '../../components/DomainsBanner/DomainsBanner';
+
+import { ROUTES } from '../../constants/routes';
 import classnames from './HomePage.module.scss';
 
 export default class HomePage extends Component {
+  componentDidMount() {
+    const { isAuthenticated, history } = this.props;
+    if (!isAuthenticated) return;
+    history.push(ROUTES.DASHBOARD);
+  }
+
   render() {
     return (
       <div className={classnames.container}>
