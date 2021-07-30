de0464847145b762ef064aaf6d235bdde8f41396
andreyvEze <andreyv@eze.tech>
2021-07-30 22:41:08 +0300

    Change Processing to tsx

--- client/src/components/common/TransactionProcessing/Processing.tsx
+++ client/src/components/common/TransactionProcessing/Processing.tsx
@@ -4,9 +4,19 @@ import Modal from '../../Modal/Modal';
 import logoAnimation from '../../CreateAccountForm/logo-animation.json';
 import classes from '../../CheckoutPurchaseContainer/CheckoutPurchaseContainer.module.scss';
 
-const Processing = props => {
+type Props = {
+  isProcessing: boolean;
+};
+
+const Processing = (props: Props) => {
   return (
-    <Modal show={props.isProcessing} backdrop="static" hideCloseButton>
+    <Modal
+      show={props.isProcessing}
+      backdrop="static"
+      hideCloseButton
+      closeButton={null}
+      onClose={null}
+    >
       <div className={classes.processingContainer}>
         <div className={classes.logo}>
           <lottie-player
