fed93adb9c952ba30d27ec53b4ecbf0d1f7086bc
Ismael Terreno <elismakof2002@gmail.com>
2021-09-13 16:02:35 -0300

    BD-2806: - Added email user selector.

--- client/src/redux/profile/selectors.js
+++ client/src/redux/profile/selectors.js
@@ -4,6 +4,7 @@ import { prefix } from './actions';
 export const loading = state => state[prefix].loading;
 export const user = state => state[prefix].user;
 export const role = state => state[prefix].user && state[prefix].user.role;
+export const email = state => state[prefix].user && state[prefix].user.email;
 export const isAdmin = state =>
   state[prefix].user && state[prefix].user.role === 'ADMIN';
 export const error = state => state[prefix].error;
