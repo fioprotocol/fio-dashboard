a0dca34162c57f40c798429a7662b9d492f86688
Oleksii Trukhanov <trukhantech@gmail.com>
2021-09-18 16:02:40 +0300

    Add disable modal

--- client/src/components/Settings/components/PasswordRecovery/PasswordRecovery.tsx
+++ client/src/components/Settings/components/PasswordRecovery/PasswordRecovery.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import SecurityItem from '../SecurityItem/SecurityItem';
+import SuccessModal from '../../../Modal/SuccessModal';
+import DangerModal from '../../../Modal/DangerModal';
 
 import classes from './PasswordRecovery.module.scss';
 
@@ -8,6 +10,12 @@ const ITEM_PROPS = {
   title: 'Password Recovery',
   subtitle: `Set up your password recovery, so you don't loose your account forever`,
   modalTitle: 'Confirm Recovery Questions',
+  dangerTitle: 'Are You Sure?',
+  dangerSubtitle:
+    'This is required to recover your account in addition to your username and recovery questions.',
+  successModalTitle: 'PASSWORD RECOVERY DISABLED',
+  successModalSubtitle:
+    'Your Password Recovery has been successfully disabled.',
 };
 
 type Props = {
@@ -17,6 +25,7 @@ type Props = {
   resendRecovery: () => void;
   hasRecoveryQuestions: boolean;
   checkRecoveryQuestions: (username: string) => void;
+  disableQuestionsResult: { status?: number };
 };
 
 const PasswordRecovery: React.FC<Props> = props => {
@@ -27,24 +36,41 @@ const PasswordRecovery: React.FC<Props> = props => {
     resendRecovery,
     hasRecoveryQuestions,
     username,
+    disableQuestionsResult,
   } = props;
+
+  const [showDisableModal, toggleDisableModal] = useState(false);
+  const [showSuccessModal, toggleSuccessModal] = useState(false);
 
   useEffect(() => {
     username && checkRecoveryQuestions(username);
   }, []);
 
+  useEffect(() => {
+    if (disableQuestionsResult && disableQuestionsResult.status)
+      toggleSuccessModal(true);
+  }, [disableQuestionsResult]);
+
   const onClick = () => {
-    showRecoveryModal();
-    changeRecoveryQuestionsOpen();
+    if (hasRecoveryQuestions) {
+      toggleDisableModal(true);
+    } else {
+      showRecoveryModal();
+      changeRecoveryQuestionsOpen();
+    }
   };
 
   const onResendClick = () => {
     resendRecovery(); // todo: handle results, show success modal
   };
 
+  const onDisableClose = () => toggleDisableModal(false);
+
+  const onSuccessClose = () => toggleSuccessModal(false);
+
   const mainButtonText = hasRecoveryQuestions
-    ? 'Re Set up Password Recovery'
-    : 'Set up Password Recovery';
+    ? 'Disable Password Recovery'
+    : 'Setup Password Recovery';
 
   const renderResendButton = (
     <Button onClick={onResendClick} className={classes.resendButton}>
@@ -59,7 +85,23 @@ const PasswordRecovery: React.FC<Props> = props => {
       isGreen={hasRecoveryQuestions}
       onClick={onClick}
       bottomChildren={hasRecoveryQuestions && renderResendButton}
-    />
+    >
+      <DangerModal
+        show={showDisableModal}
+        onClose={onDisableClose}
+        onActionButtonClick={() => {}}
+        buttonText={mainButtonText}
+        showCancel={true}
+        title={ITEM_PROPS.dangerTitle}
+        subtitle={ITEM_PROPS.dangerSubtitle}
+      />
+      <SuccessModal
+        title={ITEM_PROPS.successModalTitle}
+        subtitle={ITEM_PROPS.successModalSubtitle}
+        onClose={onSuccessClose}
+        showModal={showSuccessModal}
+      />
+    </SecurityItem>
   );
 };
 
