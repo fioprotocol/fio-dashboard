3d72b3cc294e9b0986a974f31cd24eba3b36b427
andreyvEze <andreyv@eze.tech>
2021-09-23 16:43:28 +0300

    Hide cart icon from header when user has fio addresses

--- client/src/components/MainHeader/index.ts
+++ client/src/components/MainHeader/index.ts
@@ -13,6 +13,7 @@ import {
 } from '../../redux/profile/selectors';
 import { loading as edgeAuthLoading } from '../../redux/edge/selectors';
 import { list as notifications } from '../../redux/notifications/selectors';
+import { fioAddresses } from '../../redux/fio/selectors';
 import { cartItems } from '../../redux/cart/selectors';
 import {
   refProfileInfo,
@@ -35,6 +36,7 @@ const selector = createStructuredSelector({
   refProfileInfo,
   refProfileLoading,
   homePageLink,
+  fioAddresses,
 });
 
 const actions = (dispatch: AppDispatch, ownProps: MainHeaderProps) => ({
