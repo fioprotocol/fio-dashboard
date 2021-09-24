b9ae2e53a65742f102f0fdeb14d5c028871cfa3d
andreyvEze <andreyv@eze.tech>
2021-09-24 15:25:57 +0300

    Rename ref action success saga. Do not logout after ref action

--- client/src/redux/refProfile/sagas.js
+++ client/src/redux/refProfile/sagas.js
@@ -1,14 +1,10 @@
 import { put, takeEvery, select } from 'redux-saga/effects';
 import { REF_ACTIONS_TO_ROUTES, REF_FLOW_STEPS } from '../../constants/common';
 import { setStep } from './actions';
-import { LOGIN_SUCCESS, logout } from '../profile/actions';
+import { LOGIN_SUCCESS } from '../profile/actions';
 import { FIO_SIGN_NFT_SUCCESS } from '../fio/actions';
 import { SET_REGISTRATION_RESULTS } from '../registrations/actions';
-import {
-  isRefFlow as getIsRefFlow,
-  refProfileQueryParams,
-  homePageLink as getHomePageLink,
-} from './selectors';
+import { isRefFlow as getIsRefFlow, refProfileQueryParams } from './selectors';
 
 export function* refLoginSuccess() {
   yield takeEvery(LOGIN_SUCCESS, function*() {
@@ -30,15 +26,13 @@ export function* fioAddressRegisterSuccess(history) {
   });
 }
 
-export function* nftSignSuccess(history) {
+export function* refActionSuccess() {
   yield takeEvery(FIO_SIGN_NFT_SUCCESS, function*(action) {
     const isRefFlow = yield select(getIsRefFlow);
     if (isRefFlow && action.data.status) {
       const { r } = yield select(refProfileQueryParams);
       const redirectUrl = `${r}?txId=${action.data.transactionId}`;
       yield put(setStep(REF_FLOW_STEPS.FINISH));
-      const homePageLink = yield select(getHomePageLink);
-      yield put(logout({ history }, homePageLink));
       window.location.replace(redirectUrl);
     }
   });
