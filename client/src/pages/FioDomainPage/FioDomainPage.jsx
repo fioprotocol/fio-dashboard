3c90278d3647006737958f9b851aa46f366ed194
andreyvEze <andreyv@eze.tech>
2021-10-13 18:56:20 +0300

    Non active user redirect on address/domain selection page

--- client/src/pages/FioDomainPage/FioDomainPage.jsx
+++ client/src/pages/FioDomainPage/FioDomainPage.jsx
@@ -3,8 +3,10 @@ import AddressDomainContainer from '../../components/AddressDomainContainer';
 
 import { ADDRESS_DOMAIN_BADGE_TYPE } from '../../components/AddressDomainBadge/AddressDomainBadge';
 import { LINK_LABELS } from '../../constants/labels';
+import { useNonActiveUserRedirect } from '../../util/hooks';
 
 const FioDomainPage = () => {
+  useNonActiveUserRedirect();
   return (
     <AddressDomainContainer
       title={LINK_LABELS.FIO_DOMAINS.toUpperCase()}
