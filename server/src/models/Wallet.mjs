c4bf6a6d33c230fb55969442c7757754750b1b49
andreyvEze <andreyv@eze.tech>
2021-10-21 12:08:37 +0300

    Wallets edgeId / createdFrom migration

--- server/src/models/Wallet.mjs
+++ server/src/models/Wallet.mjs
@@ -5,16 +5,26 @@ import Base from './Base';
 import { User } from './User';
 
 const { DataTypes: DT } = Sequelize;
+import { WALLET_CREATED_FROM } from '../config/constants';
 
 export class Wallet extends Base {
+  static get CREATED_FROM() {
+    return WALLET_CREATED_FROM;
+  }
+
   static init(sequelize) {
     super.init(
       {
         id: { type: DT.BIGINT, primaryKey: true, autoIncrement: true },
         publicKey: { type: DT.STRING, allowNull: false },
-        edgeId: { type: DT.STRING, allowNull: false, unique: true },
+        edgeId: { type: DT.STRING, unique: true },
         name: { type: DT.STRING, allowNull: true },
         data: { type: DT.JSON },
+        from: {
+          type: DT.STRING,
+          allowNull: false,
+          defaultValue: Wallet.CREATED_FROM.EDGE,
+        },
       },
       {
         sequelize,
