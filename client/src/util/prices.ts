57fbb7f36fbe014b4aa138314c5780a8eea1426b
andreyvEze <andreyv@eze.tech>
2021-11-12 20:03:57 +0200

    Remove unused arg from setFees method

--- client/src/util/prices.ts
+++ client/src/util/prices.ts
@@ -1,7 +1,7 @@
-import { FeePrice, Prices } from '../types';
+import { FeePrice } from '../types';
 import apis from '../api';
 
-export const setFees = (nativeFee: number, prices: Prices, roe: number) => {
+export const setFees = (nativeFee: number, roe: number) => {
   const fee: FeePrice = {
     nativeFio: null,
     costFio: null,
