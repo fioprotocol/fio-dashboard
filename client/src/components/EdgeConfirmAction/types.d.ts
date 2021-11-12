e435cfaedb03b8bd6db011891c210450cf92e39d
andreyvEze <andreyv@eze.tech>
2021-11-12 17:39:12 +0200

    Add edge confirm action component

---
+++ client/src/components/EdgeConfirmAction/types.d.ts
@@ -0,0 +1,29 @@
+import { EdgeAccount } from 'edge-core-js';
+import { PinConfirmation, WalletKeys } from '../../types';
+import { PinDataType } from '../../redux/modal/types';
+
+export type SubmitActionParams = {
+  edgeAccount?: EdgeAccount;
+  keys?: WalletKeys | null;
+  data?: any;
+};
+
+export type Props = {
+  action: string;
+  confirmingPin: boolean;
+  pinModalIsOpen: boolean;
+  edgeAccountLogoutBefore: boolean;
+  data: any | null;
+  processing: boolean;
+  pinConfirmation: PinConfirmation;
+  hideProcessing?: boolean;
+  fioWalletEdgeId?: string;
+
+  showPinModal: (action: string, data: PinDataType) => void;
+  showGenericErrorModal: (message?: string) => void;
+  submitAction: (params: SubmitActionParams) => Promise<any>;
+  onSuccess: (data: any) => void;
+  onCancel: () => void;
+  setProcessing: (processing: boolean) => void;
+  resetPinConfirm: () => void;
+};
