a3141fb3529118ae8cd9fb38cfbbb2d3f05bd411
andreyvEze <andreyv@eze.tech>
2022-01-08 14:10:21 +0200

    Change executeAction to use preparedTx

--- client/src/constants/fio.ts
+++ client/src/constants/fio.ts
@@ -5,3 +5,40 @@ export const FIO_CHAIN_CODE = 'FIO';
 export const MAX_PUBLIC_ADDRESS_LENGTH = 128;
 
 export const TOKEN_LINK_MIN_WAIT_TIME = 2000;
+
+export const ACTIONS = {
+  transferTokens: 'transferTokens',
+  addPublicAddress: 'addPublicAddress',
+  addPublicAddresses: 'addPublicAddresses',
+  setFioDomainPublic: 'setFioDomainPublic',
+  rejectFundsRequest: 'rejectFundsRequest',
+  requestFunds: 'requestFunds',
+  recordObtData: 'recordObtData',
+  registerFioAddress: 'registerFioAddress',
+  registerFioDomain: 'registerFioDomain',
+  renewFioDomain: 'renewFioDomain',
+  transferFioAddress: 'transferFioAddress',
+  transferFioDomain: 'transferFioDomain',
+  pushTransaction: 'pushTransaction',
+  addBundledTransactions: 'addBundledTransactions',
+  stakeFioTokens: 'stakeFioTokens',
+  unStakeFioTokens: 'unStakeFioTokens',
+};
+
+export const ACTIONS_TO_END_POINT_KEYS = {
+  [ACTIONS.requestFunds]: 'newFundsRequest',
+  [ACTIONS.registerFioAddress]: 'registerFioAddress',
+  [ACTIONS.registerFioDomain]: 'registerFioDomain',
+  [ACTIONS.renewFioDomain]: 'renewFioDomain',
+  [ACTIONS.addPublicAddresses]: 'addPubAddress',
+  [ACTIONS.setFioDomainPublic]: 'setFioDomainPublic',
+  [ACTIONS.rejectFundsRequest]: 'rejectFundsRequest',
+  [ACTIONS.recordObtData]: 'recordObtData',
+  [ACTIONS.transferTokens]: 'transferTokens',
+  [ACTIONS.pushTransaction]: 'pushTransaction',
+  [ACTIONS.transferFioAddress]: 'transferFioAddress',
+  [ACTIONS.transferFioDomain]: 'transferFioDomain',
+  [ACTIONS.stakeFioTokens]: 'pushTransaction',
+  [ACTIONS.unStakeFioTokens]: 'pushTransaction',
+  [ACTIONS.addBundledTransactions]: 'addBundledTransactions',
+};
