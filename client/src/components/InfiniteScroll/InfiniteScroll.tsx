7e8d6116d39b36d145adbdef949393ff2c2d713d
Oleksii Trukhanov <trukhantech@gmail.com>
2022-01-17 18:21:46 +0200

    Create Infinite loader component

---
+++ client/src/components/InfiniteScroll/InfiniteScroll.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import useInfiniteScroll from 'react-infinite-scroll-hook';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import classes from './InfiniteScroll.module.scss';
+
+type Props = {
+  loading: boolean;
+  hasNextPage: boolean;
+  onLoadMore: () => void;
+};
+
+const InfiniteScroll: React.FC<Props> = props => {
+  const { loading, hasNextPage, children, onLoadMore, ...rest } = props;
+
+  const [sentryRef] = useInfiniteScroll({
+    loading,
+    hasNextPage,
+    onLoadMore,
+    rootMargin: '0px 0px 20px 0px',
+    ...rest,
+  });
+
+  return (
+    <div className={classes.container}>
+      {children}
+      {loading && (
+        <div className={classes.loader} ref={sentryRef}>
+          <FontAwesomeIcon
+            icon="spinner"
+            spin={true}
+            className={classes.loaderIcon}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default InfiniteScroll;
