00d1a1e9cb537ed9579eb5c255b74afaf546e161
NikolasStelmah <nikolasstelmah@ukr.net>
2022-01-26 12:42:03 +0200

    Fix typings for ChainCodeCustomDropdown and extend functionality to parse field errors

--- client/src/components/ChainCodeField/ChainCodeCustomDropdown.tsx
+++ client/src/components/ChainCodeField/ChainCodeCustomDropdown.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import { FieldRenderProps } from 'react-final-form';
 import CustomDropdown from '../CustomDropdown';
+import { Label } from '../Input/StaticInputParts';
+import classes from '../Input/Input.module.scss';
+import { ErrorBadge } from '../Input/ErrorBadge';
 
 type Props = {
-  options: { id: string; name: string };
+  options: { id: string; name: string }[];
   customValue: { id: string; name: string };
   toggleToCustom: (isCustom: boolean) => void;
   hasAutoWidth?: boolean;
   isShort?: boolean;
   isSimple?: boolean;
+  isFormField?: boolean;
   isHigh?: boolean;
+  label?: string;
+  uiType?: string;
+  input: {
+    value: string;
+  };
 };
 
-const ChainCodeCustomDropdown: React.FC<FieldRenderProps<Props>> = props => {
+const ChainCodeCustomDropdown: React.FC<Props &
+  FieldRenderProps<Props>> = props => {
   const {
     input,
+    meta,
     options,
     customValue,
     toggleToCustom,
@@ -23,21 +34,59 @@ const ChainCodeCustomDropdown: React.FC<FieldRenderProps<Props>> = props => {
     isShort,
     isSimple,
     isHigh,
+    label,
+    uiType,
+    errorType = '',
+    errorColor = '',
+    hideError,
+    isFormField,
   } = props;
-  const { onChange } = input;
+  const { onChange, value } = input;
+  const {
+    error,
+    data,
+    touched,
+    active,
+    modified,
+    submitError,
+    modifiedSinceLastSubmit,
+    submitSucceeded,
+  } = meta;
+
+  const hasError =
+    ((error || data.error) &&
+      (touched || modified || submitSucceeded || !!value) &&
+      !active) ||
+    (submitError && !modifiedSinceLastSubmit);
+
   return (
-    <CustomDropdown
-      onChange={onChange}
-      options={options}
-      customValue={customValue}
-      toggleToCustom={toggleToCustom}
-      placeholder={placeholder}
-      isShort={isShort}
-      isSimple={isSimple}
-      isWhite={true}
-      hasAutoWidth={hasAutoWidth}
-      isHigh={isHigh}
-    />
+    <>
+      <Label label={label} uiType={uiType} />
+      <CustomDropdown
+        onChange={onChange}
+        options={options}
+        value={value}
+        customValue={customValue}
+        toggleToCustom={toggleToCustom}
+        placeholder={placeholder}
+        isShort={isShort}
+        isSimple={isSimple}
+        isFormField={isFormField}
+        isWhite={true}
+        hasAutoWidth={hasAutoWidth}
+        isHigh={isHigh}
+      />
+      <div className={classes.regInputWrapper}>
+        <ErrorBadge
+          error={error}
+          data={data}
+          hasError={!hideError && !data.hideError && hasError}
+          type={errorType}
+          color={errorColor}
+          submitError={submitError}
+        />
+      </div>
+    </>
   );
 };
 
