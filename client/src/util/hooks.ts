d3bee9d96240e0d0301bd9c978e978d80f2b1255
NikolasStelmah <nikolasstelmah@ukr.net>
2022-02-01 17:09:02 +0200

    Add useInterval hook

--- client/src/util/hooks.ts
+++ client/src/util/hooks.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useLayoutEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import {
@@ -128,3 +128,27 @@ export function useConvertFioToUsdc({
 
   return apis.fio.convertFioToUsdc(fioSuf, roeAmount);
 }
+
+export function useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = useRef(callback);
+
+  // Remember the latest callback if it changes.
+  useLayoutEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    // Don't schedule if no delay is specified.
+    // Note: 0 is a valid value for delay.
+    if (!delay && delay !== 0) {
+      return;
+    }
+
+    const id = setInterval(() => savedCallback.current(), delay);
+
+    return () => clearInterval(id);
+  }, [delay]);
+}
+
+export default useInterval;
