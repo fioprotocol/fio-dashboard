469fe3973857f7179f9fe7f74cbbc4b17d9f634a
Oleksii Trukhanov <trukhantech@gmail.com>
2022-02-01 23:23:40 +0200

    Set lazy loader to FIO Transactions

--- client/src/pages/WalletPage/components/TransactionList.tsx
+++ client/src/pages/WalletPage/components/TransactionList.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import InfoBadge from '../../../components/Badges/InfoBadge/InfoBadge';
 import TransactionItem from './TransactionItem';
 import Loader from '../../../components/Loader/Loader';
+import InfiniteScroll from '../../../components/InfiniteScroll/InfiniteScroll';
 
 import { checkTransactions } from '../../../util/transactions';
 import { FioWalletDoublet, TransactionItemProps } from '../../../types';
@@ -13,16 +14,23 @@ type Props = {
   fioWallet: FioWalletDoublet;
 };
 
+const MIN_VISIBLE_TRANSACTIONS_COUNT = 20;
+
 const TransactionList: React.FC<Props> = props => {
   const { fioWallet } = props;
   const [loading, setLoading] = useState(false);
-  const [list, setList] = useState<TransactionItemProps[] | null>(null);
+  const [transactionList, setTransactionList] = useState<
+    TransactionItemProps[] | null
+  >(null);
+  const [visibleTransactionsCount, setVisibleTransactionsCount] = useState(
+    MIN_VISIBLE_TRANSACTIONS_COUNT,
+  );
 
   const getTransactions = async () => {
     setLoading(true);
     try {
       const transactions = await checkTransactions(fioWallet.publicKey);
-      setList(transactions);
+      setTransactionList(transactions);
     } catch (e) {
       console.error(e);
     }
@@ -40,7 +48,7 @@ const TransactionList: React.FC<Props> = props => {
       </div>
     );
 
-  if (!list)
+  if (!transactionList)
     return (
       <div className={classes.infoBadge}>
         <InfoBadge
@@ -50,11 +58,33 @@ const TransactionList: React.FC<Props> = props => {
       </div>
     );
 
+  const loadMore = () => {
+    setVisibleTransactionsCount(
+      visibleTransactionsCount + MIN_VISIBLE_TRANSACTIONS_COUNT,
+    );
+  };
+
+  const hasNextPage = visibleTransactionsCount < transactionList.length;
+
   return (
     <div className={classes.container}>
-      {list.map(item => (
-        <TransactionItem key={item.txId} {...item} />
-      ))}
+      <InfiniteScroll
+        loading={loading}
+        hasNextPage={hasNextPage}
+        isContentScrollable={
+          transactionList.length > MIN_VISIBLE_TRANSACTIONS_COUNT
+        }
+        onLoadMore={loadMore}
+      >
+        {transactionList
+          .slice(
+            0,
+            !hasNextPage ? transactionList.length : visibleTransactionsCount,
+          )
+          .map(item => (
+            <TransactionItem key={item.txId} {...item} />
+          ))}
+      </InfiniteScroll>
     </div>
   );
 };
