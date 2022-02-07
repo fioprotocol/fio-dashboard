c321b786c157a9bd48beedcd6cd16468882b001f
andreyvEze <andreyv@eze.tech>
2022-02-07 15:07:30 +0200

    src files ts

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,10 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './assets/styles/global.scss';
-import App from './app';
+import './app';
 import * as serviceWorker from './service-worker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
