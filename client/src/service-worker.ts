ec30fd473b8f282e29022b37de88e23f5aacaff2
andreyvEze <andreyv@eze.tech>
2022-02-07 15:07:30 +0200

    src files ts

--- client/src/service-worker.ts
+++ client/src/service-worker.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
@@ -44,7 +45,7 @@ export function register(config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          /* eslint-disable no-console */
+          // tslint:disable-next-line:no-console
           console.log(
             'This web app is being served cache-first by a service ' +
               'worker. To learn more, visit http://bit.ly/CRA-PWA',
@@ -73,7 +74,7 @@ function registerValidSW(swUrl, config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              /* eslint-disable no-console */
+              // tslint:disable-next-line:no-console
               console.log(
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. See http://bit.ly/CRA-PWA.',
@@ -87,7 +88,7 @@ function registerValidSW(swUrl, config) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              /* eslint-disable no-console */
+              // tslint:disable-next-line:no-console
               console.log('Content is cached for offline use.');
 
               // Execute callback
@@ -127,7 +128,7 @@ function checkValidServiceWorker(swUrl, config) {
       }
     })
     .catch(() => {
-      /* eslint-disable no-console */
+      // tslint:disable-next-line:no-console
       console.log(
         'No internet connection found. App is running in offline mode.',
       );
