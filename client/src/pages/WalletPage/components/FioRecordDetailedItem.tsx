dca09bb1297ac4a0ed9e95cc5136e8b9694798bb
NikolasStelmah <nikolasstelmah@ukr.net>
2022-02-14 17:31:13 +0200

    Add message info if Fio Address was moved to another wallet

--- client/src/pages/WalletPage/components/FioRecordDetailedItem.tsx
+++ client/src/pages/WalletPage/components/FioRecordDetailedItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import FioRecordDetailedAcctionButtons from './FioRecordDetailedAcctionButtons';
 import FioRecordFieldsList from './FioRecordFieldsList';
@@ -7,9 +7,11 @@ import { FIO_REQUEST_STATUS_TYPES } from '../../../constants/fio';
 import { FIO_RECORD_TYPES } from '../../WalletPage/constants';
 
 import { FioRecordViewKeysProps, FioRecordViewDecrypted } from '../types';
-import { FioWalletDoublet } from '../../../types';
+import { FioAddressDoublet, FioWalletDoublet } from '../../../types';
 
 import classes from '../styles/FioRecordDetailedItem.module.scss';
+import { useFioAddresses } from '../../../util/hooks';
+import InfoBadge from '../../../components/Badges/InfoBadge/InfoBadge';
 
 type Props = {
   fieldsList: FioRecordViewKeysProps[];
@@ -30,8 +32,42 @@ const FioRecordDetailedItem: React.FC<Props> = props => {
     onCloseModal,
   } = props;
 
+  const [error, setError] = useState(null);
+  const [selectedAddress, setSelectedAddress] = useState(null);
+
+  const walletFioAddresses = useFioAddresses(
+    fioWallet && fioWallet.publicKey,
+  ).sort((fioAddress1: FioAddressDoublet, fioAddress2: FioAddressDoublet) =>
+    fioAddress1.name > fioAddress2.name ? 1 : -1,
+  );
+
+  useEffect(() => {
+    if (fioRecordDecrypted?.fioRecord?.to) {
+      const address = walletFioAddresses.find(
+        ({ name }) => name === fioRecordDecrypted.fioRecord.to,
+      );
+      setSelectedAddress(address);
+      setError(
+        !address
+          ? 'This Fio Crypto Handle no longer exists in current wallet.'
+          : null,
+      );
+    }
+  }, [fioRecordDecrypted, walletFioAddresses]);
+
   if (!fioRecordDecrypted) return null;
 
+  const renderActionButtons = () =>
+    !!selectedAddress &&
+    fioRecordDecrypted.fioRecord.status === FIO_REQUEST_STATUS_TYPES.PENDING &&
+    fioRecordType === FIO_RECORD_TYPES.RECEIVED && (
+      <FioRecordDetailedAcctionButtons
+        fioRecordDecrypted={fioRecordDecrypted}
+        fioWallet={fioWallet}
+        fioRecordType={fioRecordType}
+        onCloseModal={onCloseModal}
+      />
+    );
   return (
     <div className={classes.fieldsContainer}>
       <FioRecordFieldsList
@@ -40,16 +76,20 @@ const FioRecordDetailedItem: React.FC<Props> = props => {
         fioRecordDetailedType={fioRecordDetailedType}
         fioRecordType={fioRecordType}
       />
-      {fioRecordDecrypted.fioRecord.status ===
-        FIO_REQUEST_STATUS_TYPES.PENDING &&
-        fioRecordType === FIO_RECORD_TYPES.RECEIVED && (
-          <FioRecordDetailedAcctionButtons
-            fioRecordDecrypted={fioRecordDecrypted}
-            fioWallet={fioWallet}
-            fioRecordType={fioRecordType}
-            onCloseModal={onCloseModal}
-          />
+      {error &&
+        fioRecordDecrypted.fioRecord.status ===
+          FIO_REQUEST_STATUS_TYPES.PENDING &&
+        fioRecordType === FIO_RECORD_TYPES.RECEIVED &&
+        fioRecordDecrypted?.fioRecord?.to && (
+          <div className={classes.infoBadge}>
+            <InfoBadge
+              isOrange
+              message={error}
+              title={`${fioRecordDecrypted.fioRecord.to}`}
+            />
+          </div>
         )}
+      {renderActionButtons()}
     </div>
   );
 };
