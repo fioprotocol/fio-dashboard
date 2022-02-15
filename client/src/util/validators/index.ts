a0c7c7d4caa6e20e38462b22b0c2e3df6321fb8a
NikolasStelmah <nikolasstelmah@ukr.net>
2022-02-15 14:03:27 +0200

    Add FioWallet validator

--- client/src/util/validators/index.ts
+++ client/src/util/validators/index.ts
@@ -3,4 +3,5 @@ export * from './fio-address-exists.validator';
 export * from './is-fio-address.validator';
 export * from './is-number.validator';
 export * from './is-valid-pub-address.validator';
+export * from './fio-wallet-name.validator';
 export * from './is-amount.validator';
