5380258a9e851f7fa67554e5e431d15dc1f55863
NikolasStelmah <nikolasstelmah@ukr.net>
2022-02-15 20:08:15 +0200

    Add hasNumberCharValidator, hasUppercaseLetterValidator, hasLowercaseLetterValidator

--- client/src/util/validators/index.ts
+++ client/src/util/validators/index.ts
@@ -4,4 +4,7 @@ export * from './is-fio-address.validator';
 export * from './is-number.validator';
 export * from './is-valid-pub-address.validator';
 export * from './fio-wallet-name.validator';
+export * from './has-uppercase-letter.validator';
+export * from './has-lowercase-letter.validator';
+export * from './has-number-char.validator';
 export * from './is-amount.validator';
