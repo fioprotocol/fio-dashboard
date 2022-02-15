3656c4de9185469f51f604db91e815df207a04d9
NikolasStelmah <nikolasstelmah@ukr.net>
2022-02-15 13:38:20 +0200

    Import Wallet form

--- client/src/pages/ImportWalletPage/components/ImportWalletForm/validation.ts
+++ client/src/pages/ImportWalletPage/components/ImportWalletForm/validation.ts
@@ -1,23 +1,29 @@
-import { testWalletName } from '../../../../util/general';
+import { Validators, ValidationSchema } from '@lemoncode/fonk';
+import { createFinalFormValidation } from '@lemoncode/fonk-final-form';
 
-import { ImportWalletValues } from '../../types';
+import { WALLET_NAME_REGEX } from '../../../../constants/regExps';
 
-export const validate = (values: ImportWalletValues) => {
-  if (!values.privateSeed) {
-    return { privateSeed: 'Required' };
-  }
-
-  if (!values.name) {
-    return { name: 'Required' };
-  }
-
-  try {
-    testWalletName(values.name);
-  } catch (e) {
-    return {
-      name: e.message,
-    };
-  }
-
-  return {};
+const validationSchema: ValidationSchema = {
+  field: {
+    privateSeed: [
+      {
+        validator: Validators.required,
+        message: 'Required.',
+      },
+    ],
+    name: [
+      {
+        validator: Validators.required,
+        message: 'Required.',
+      },
+      {
+        validator: Validators.pattern,
+        customArgs: { pattern: WALLET_NAME_REGEX },
+        message:
+          'Name is not valid. Name should contain only letters, digits, spaces, dashes or underscores.',
+      },
+    ],
+  },
 };
+
+export const formValidation = createFinalFormValidation(validationSchema);
