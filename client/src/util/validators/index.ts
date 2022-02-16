10936b63b38458ab296c8d710abcb76727ff3196
NikolasStelmah <nikolasstelmah@ukr.net>
2022-02-16 22:18:37 +0200

    Add is-chain-code.validator, is-hash.validator, is-native-blockchain-public-address.validator, meta.validator

--- client/src/util/validators/index.ts
+++ client/src/util/validators/index.ts
@@ -7,4 +7,8 @@ export * from './fio-wallet-name.validator';
 export * from './has-uppercase-letter.validator';
 export * from './has-lowercase-letter.validator';
 export * from './has-number-char.validator';
+export * from './is-chain-code.validator';
+export * from './is-native-blockchain-public-address.validator';
+export * from './is-hash.validator';
+export * from './meta.validator';
 export * from './is-amount.validator';
