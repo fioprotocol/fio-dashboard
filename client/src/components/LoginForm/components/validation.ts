215a01661027ef1fc08fd7921f93217ec4ab6c2c
NikolasStelmah <nikolasstelmah@ukr.net>
2022-02-17 21:30:29 +0200

    Username Password form

--- client/src/components/LoginForm/components/validation.ts
+++ client/src/components/LoginForm/components/validation.ts
@@ -1,7 +1,7 @@
 import { Validators, ValidationSchema } from '@lemoncode/fonk';
 import { createFinalFormValidation } from '@lemoncode/fonk-final-form';
 
-const validationSchema: ValidationSchema = {
+const twoFactorCodeModalValidationSchema: ValidationSchema = {
   field: {
     backupCode: [
       {
@@ -12,4 +12,31 @@ const validationSchema: ValidationSchema = {
   },
 };
 
-export const formValidation = createFinalFormValidation(validationSchema);
+const usernamePasswordValidationSchema: ValidationSchema = {
+  field: {
+    email: [
+      {
+        validator: Validators.required,
+        message: 'Required.',
+      },
+      {
+        validator: Validators.email,
+        message: 'Invalid Email Address',
+      },
+    ],
+    password: [
+      {
+        validator: Validators.required,
+        message: 'Password Field Should Be Filled',
+      },
+    ],
+  },
+};
+
+export const twoFactorCodeModalValidation = createFinalFormValidation(
+  twoFactorCodeModalValidationSchema,
+);
+
+export const usernamePasswordValidation = createFinalFormValidation(
+  usernamePasswordValidationSchema,
+);
