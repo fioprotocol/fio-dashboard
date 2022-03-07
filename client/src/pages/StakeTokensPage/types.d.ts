99efd896767523b698aef3dc1f32478deb78586d
NikolasStelmah <nikolasstelmah@ukr.net>
2022-03-07 22:16:10 +0700

    Add Stake ui

---
+++ client/src/pages/StakeTokensPage/types.d.ts
@@ -0,0 +1,49 @@
+import { RouteComponentProps } from 'react-router-dom';
+import {
+  FeePrice,
+  FioAddressDoublet,
+  FioWalletDoublet,
+  WalletBalances,
+} from '../../types';
+
+import { FioRecordViewDecrypted } from '../WalletPage/types';
+
+type MatchProps = {
+  publicKey: string;
+};
+
+export type StakeTokensValues = {
+  publicKey: string;
+  fioAddress?: string;
+  amount: string;
+  nativeAmount: string;
+};
+
+export type InitialValues = {
+  fioAddress?: string;
+  publicKey: string;
+};
+
+export type StakeTokensProps = {
+  fioAddresses: FioAddressDoublet[];
+  fee: FeePrice;
+  balance: WalletBalances;
+  loading: boolean;
+  initialValues?: InitialValues;
+  onSubmit: (values: StakeTokensValues) => void;
+};
+
+export interface ContainerOwnProps extends RouteComponentProps<MatchProps> {
+  children?: React.ReactNode;
+}
+
+export interface ContainerProps extends ContainerOwnProps {
+  fioWallet: FioWalletDoublet;
+  loading: boolean;
+  roe: number;
+  feePrice: FeePrice;
+  balance: WalletBalances;
+  refreshBalance: (publicKey: string) => void;
+  getFee: () => void;
+  refreshWalletDataPublicKey: (publicKey: string) => void;
+}
