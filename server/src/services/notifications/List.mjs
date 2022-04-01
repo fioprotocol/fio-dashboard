1c3704aaf12698b657f82e6018c360a798d92853
andreyvEze <andreyv@eze.tech>
2022-04-01 19:13:25 +0300

    Do not return email notifications to dashboard temporary

--- server/src/services/notifications/List.mjs
+++ server/src/services/notifications/List.mjs
@@ -1,10 +1,21 @@
+import Sequelize from 'sequelize';
+
 import Base from '../Base';
 
 import { Notification } from '../../models';
 
+const SHOWN_NOTIFICATION_CONTENT_TYPES = [
+  Notification.CONTENT_TYPE.ACCOUNT_CONFIRMATION,
+  Notification.CONTENT_TYPE.ACCOUNT_CREATE,
+  Notification.CONTENT_TYPE.RECOVERY_PASSWORD,
+];
+
 export default class NotificationsList extends Base {
   async execute() {
-    const notifications = await Notification.list({ userId: this.context.id });
+    const notifications = await Notification.list({
+      userId: this.context.id,
+      contentType: { [Sequelize.Op.in]: SHOWN_NOTIFICATION_CONTENT_TYPES },
+    });
 
     return {
       data: notifications.map(notification =>
