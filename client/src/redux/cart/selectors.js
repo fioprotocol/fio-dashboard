6a9aad65268544b1f3e8b65c47dcf4f8ff7f1409
andreyvEze <andreyv@eze.tech>
2022-04-03 17:13:09 +0300

    Change redux files to ts

--- client/src/redux/cart/selectors.js
+++
@@ -1,6 +0,0 @@
-import { prefix } from './actions';
-
-export const cartItems = state => state[prefix].cartItems || [];
-export const paymentWalletPublicKey = state =>
-  state[prefix].paymentWalletPublicKey || '';
-export const cartDate = state => state[prefix].date || null;
