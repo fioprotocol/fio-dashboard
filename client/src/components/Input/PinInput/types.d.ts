e5cf5df8fb75b026fe1bc009dcfdc2760a096d78
Oleksii Trukhanov <trukhantech@gmail.com>
2022-04-08 17:13:03 +0300

    Use numeric keyboard in pin input

--- client/src/components/Input/PinInput/types.d.ts
+++ client/src/components/Input/PinInput/types.d.ts
@@ -1,14 +1,11 @@
 import { ChangeEvent } from 'react';
 import { FormApi } from 'final-form';
 
-export type IosKeyBoardPlugProp = 'highPlug' | 'extraHighPlug' | 'emptyPlug';
-
 export type PinInputProps = {
   error: string;
   name: string;
   value: string;
   withoutMargin?: boolean;
-  iosKeyboardPlugType?: IosKeyBoardPlugProp | null;
   form?: FormApi;
   onBlur?: () => void;
   onChange: (value: string) => void;
