35c4a4c09db0940f4b667f3be7df617c9ae09044
andreyvEze <andreyv@eze.tech>
2022-04-11 15:11:18 +0300

    Change CheckoutPurchaseContainer to ts

--- client/src/components/CheckoutPurchaseContainer/CheckoutPurchaseContainer.tsx
+++ client/src/components/CheckoutPurchaseContainer/CheckoutPurchaseContainer.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import isEmpty from 'lodash/isEmpty';
+import { History } from 'history';
 
 import PurchaseNow from '../PurchaseNow';
-import { ROUTES } from '../../constants/routes';
 import Processing from '../common/TransactionProcessing';
+
+import { ROUTES } from '../../constants/routes';
+
+import { RegistrationResult } from '../../types';
 
 import classes from './CheckoutPurchaseContainer.module.scss';
 
-const CheckoutPurchaseContainer = props => {
+type Props = {
+  isCheckout: boolean;
+  isPurchase: boolean;
+  isProcessing: boolean;
+  setProcessing: (processing: boolean) => void;
+  setRegistration: (results: RegistrationResult) => void;
+  history: History;
+  children: React.ReactNode | React.ReactNode[];
+  registrationResult: RegistrationResult;
+  closeText: () => void;
+  onClose: () => void;
+};
+
+const CheckoutPurchaseContainer: React.FC<Props> = props => {
   const {
     isCheckout,
     isPurchase,
@@ -25,14 +42,18 @@ const CheckoutPurchaseContainer = props => {
   const hasErrors = !isEmpty(registrationResult.errors || []);
 
   const onClose = () => {
-    setRegistration({});
+    setRegistration({
+      errors: [],
+      registered: [],
+      partial: [],
+    });
     if (parentOnClose) {
       return parentOnClose();
     }
     history.push(ROUTES.HOME);
   };
 
-  const onFinish = results => {
+  const onFinish = (results: RegistrationResult) => {
     setRegistration(results);
 
     setProcessing(false);
