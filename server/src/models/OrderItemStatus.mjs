ec460fd5f0c0b946eafce5ba0b760feabb7c141f
andreyvEze <andreyv@eze.tech>
2022-05-11 17:51:23 +0300

    Add order, payment, transaction tables migration

---
+++ server/src/models/OrderItemStatus.mjs
@@ -0,0 +1,87 @@
+import Sequelize from 'sequelize';
+
+import Base from './Base';
+
+import { BlockchainTransaction } from './BlockchainTransaction.mjs';
+import { Payment } from './Payment.mjs';
+import { OrderItem } from './OrderItem.mjs';
+
+const { DataTypes: DT } = Sequelize;
+
+export class OrderItemStatus extends Base {
+  static init(sequelize) {
+    super.init(
+      {
+        id: { type: DT.BIGINT, primaryKey: true, autoIncrement: true },
+        paymentStatus: {
+          type: DT.INTEGER,
+          allowNull: false,
+          defaultValue: 1,
+          comment: 'NEW (1), PENDING (2), CANCELLED (5), EXPIRED (4), COMPLETED (3)',
+        },
+        txStatus: {
+          type: DT.INTEGER,
+          defaultValue: 1,
+          comment:
+            'Last action processing status. READY (1) / PENDING (2) / RETRY (6) / SUCCESS (3) / REVIEW (4) / CANCEL (5) etc.',
+        },
+      },
+      {
+        sequelize,
+        tableName: 'order-items-status',
+        paranoid: true,
+        indexes: [
+          {
+            fields: ['id'],
+          },
+          {
+            fields: ['txStatus'],
+          },
+          {
+            fields: ['paymentStatus'],
+          },
+          {
+            fields: ['txStatus', 'paymentStatus'],
+          },
+          {
+            fields: ['orderItemId'],
+          },
+          {
+            fields: ['blockchainTransactionId'],
+          },
+          {
+            fields: ['paymentId'],
+          },
+        ],
+      },
+    );
+  }
+
+  static associate() {
+    this.belongsTo(BlockchainTransaction, {
+      foreignKey: 'blockchainTransactionId',
+      targetKey: 'id',
+    });
+    this.belongsTo(Payment, {
+      foreignKey: 'paymentId',
+      targetKey: 'id',
+    });
+    this.belongsTo(OrderItem, {
+      foreignKey: 'orderItemId',
+      targetKey: 'id',
+    });
+  }
+
+  static list(where) {
+    return this.findAll({
+      where,
+      order: [['id', 'ASC']],
+    });
+  }
+
+  static format({ id }) {
+    return {
+      id,
+    };
+  }
+}
