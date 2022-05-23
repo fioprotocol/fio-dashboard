49f37f6f7b19fe7d30e624b81b899038b069d2e3
Oleksii Trukhanov <trukhantech@gmail.com>
2022-05-23 23:56:41 +0300

    Remove contained flow from refProfile redux

--- client/src/redux/refProfile/reducer.ts
+++ client/src/redux/refProfile/reducer.ts
@@ -1,11 +1,8 @@
 import { combineReducers } from 'redux';
 
 import * as actions from './actions';
-import { CONFIRM_EMAIL_SUCCESS } from '../profile/actions';
 
-import { REF_FLOW_STEPS } from '../../constants/common';
-
-import { RefProfile, RefQueryParams } from '../../types';
+import { RefProfile } from '../../types';
 
 export default combineReducers({
   loading(state: boolean = false, action) {
@@ -30,37 +27,10 @@ export default combineReducers({
         return state;
     }
   },
-  params(state: RefQueryParams | null = null, action) {
-    switch (action.type) {
-      case actions.SET_CONTAINED_PARAMS:
-        return action.data;
-      case CONFIRM_EMAIL_SUCCESS: {
-        if (
-          action.data.stateData != null &&
-          action.data.stateData.refProfileQueryParams != null
-        ) {
-          return action.data.stateData.refProfileQueryParams;
-        }
-        return state;
-      }
-      default:
-        return state;
-    }
-  },
   refLinkError(state: string | null = null, action) {
     switch (action.type) {
       case actions.GET_REF_PROFILE_FAILURE:
         return 'Referral link is invalid';
-      case actions.SET_CONTAINED_PARAMS_ERROR:
-        return action.data;
-      default:
-        return state;
-    }
-  },
-  step(state: string = REF_FLOW_STEPS.INIT, action) {
-    switch (action.type) {
-      case actions.SET_STEP:
-        return action.data;
       default:
         return state;
     }
