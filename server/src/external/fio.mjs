89e76b32eb1c95a87a0ea46089fe2ba0280ee99e
andreyvEze <andreyv@eze.tech>
2022-05-24 17:01:33 +0300

    Order items processing job

--- server/src/external/fio.mjs
+++ server/src/external/fio.mjs
@@ -2,14 +2,53 @@ import fiosdkLib from '@fioprotocol/fiosdk';
 import entities from '@fioprotocol/fiosdk/lib/entities/EndPoint';
 import fetch from 'node-fetch';
 
+import logger from '../logger.mjs';
+
+import { FIO_ACTIONS, FIO_ACTIONS_TO_END_POINT_KEYS } from '../config/constants.js';
+
 import MathOp from '../services/math.mjs';
 
 export const FIOSDK = fiosdkLib.FIOSDK;
+export const DEFAULT_ACTION_FEE_AMOUNT = new MathOp(FIOSDK.SUFUnit).mul(800).toNumber();
+export const INSUFFICIENT_FUNDS_ERR_MESSAGE = 'Insufficient funds to cover fee';
 const EndPoint = entities.EndPoint;
+
+const FIO_ACTION_NAMES = {
+  [FIO_ACTIONS.registerFioAddress]: 'regaddress',
+  [FIO_ACTIONS.registerFioDomain]: 'regdomain',
+  [FIO_ACTIONS.renewFioDomain]: 'renewdomain',
+  [FIO_ACTIONS.setFioDomainVisibility]: 'setdomainpub',
+  [FIO_ACTIONS.transferFioAddress]: 'xferaddress',
+  [FIO_ACTIONS.transferFioDomain]: 'xferdomain',
+  [FIO_ACTIONS.addPublicAddress]: 'addaddress',
+  [FIO_ACTIONS.removePublicAddresses]: 'remaddress',
+  [FIO_ACTIONS.removeAllPublicAddresses]: 'remalladdr',
+  [FIO_ACTIONS.requestFunds]: 'newfundsreq',
+  [FIO_ACTIONS.recordObtData]: 'recordobt',
+  [FIO_ACTIONS.rejectFundsRequest]: 'rejectfndreq',
+  [FIO_ACTIONS.transferTokens]: 'trnsfiopubky',
+  [FIO_ACTIONS.addBundledTransactions]: 'addbundles',
+};
 
 class Fio {
   constructor() {
     this.publicFioSDK = new FIOSDK('', '', process.env.FIO_BASE_URL, fetch);
+    const { publicKey: masterPubKey } = FIOSDK.derivedPublicKey(
+      process.env.MASTER_FIOSDK_KEY,
+    );
+    this.masterFioSDK = new FIOSDK(
+      process.env.MASTER_FIOSDK_KEY,
+      masterPubKey,
+      process.env.FIO_BASE_URL,
+      fetch,
+      '',
+      '',
+      true,
+    );
+  }
+
+  logError(e) {
+    if (e.errorCode !== 404) logger.error(e.message);
   }
 
   getWalletSdkInstance(publicKey) {
@@ -32,6 +71,35 @@ class Fio {
       forDomain ? EndPoint.registerFioDomain : EndPoint.registerFioAddress,
     );
     return fee;
+  }
+
+  async executeAction(action, params, auth, keys = {}) {
+    if (keys.private) {
+      // todo: set new sdk and use it
+    }
+
+    if (!params.maxFee) params.maxFee = DEFAULT_ACTION_FEE_AMOUNT;
+
+    try {
+      if (auth.actor) {
+        params.actor = auth.actor;
+        // todo: set permission, probably need to update fiosdk
+      }
+      const preparedTrx = await this.masterFioSDK.pushTransaction(
+        '',
+        FIO_ACTION_NAMES[action],
+        params,
+      );
+      const result = await this.masterFioSDK.executePreparedTrx(
+        this.actionEndPoints[FIO_ACTIONS_TO_END_POINT_KEYS[action]],
+        preparedTrx,
+      );
+
+      return result;
+    } catch (err) {
+      this.logError(err);
+      throw err;
+    }
   }
 }
 
