cd171c12473eebf69948ca0a6bfecbc1bb9e830a
andreyvEze <andreyv@eze.tech>
2022-05-24 17:01:33 +0300

    Order items processing job

---
+++ server/src/services/fallback-funds-email.mjs
@@ -0,0 +1,40 @@
+import emailSender from '../services/emailSender.mjs';
+import logger from '../logger.mjs';
+
+import { templates } from '../emails/emailTemplate.mjs';
+
+const TIMEOUT_HOURS = 1000 * 60 * 60 * 24; // 24 hours
+const lastInsufficientFundsNotificationSent = {
+  date: null,
+};
+
+const sendInsufficientFundsNotification = async (
+  fioName,
+  refProfileName,
+  authorization,
+) => {
+  const now = new Date();
+
+  if (!lastInsufficientFundsNotificationSent.date) {
+    lastInsufficientFundsNotificationSent.date = now.getTime() - 1000 * 60 * 60 * 25;
+  }
+  const diffHours = now.getTime() - lastInsufficientFundsNotificationSent.date;
+  if (diffHours > TIMEOUT_HOURS) {
+    try {
+      await emailSender.send(
+        templates.fallbackFundsEmail,
+        process.env.REG_FALLBACK_NOTIFICATION_EMAIL,
+        { fioName, refProfileName, authorization },
+      );
+    } catch (e) {
+      logger.log('InsufficientFundsNotification send ERROR ===');
+      logger.log(e);
+    }
+
+    lastInsufficientFundsNotificationSent.date = now.getTime();
+  }
+};
+
+module.exports = {
+  sendInsufficientFundsNotification,
+};
