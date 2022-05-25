42013aa9ecee95882b73fc7edba615dd0902fdcb
andreyvEze <andreyv@eze.tech>
2022-05-26 00:03:40 +0300

    Change back prefix prop for editable select. Change appearance for editable select input.

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="react-scripts" />
+
+import { GroupBase } from 'react-select';
+declare module 'react-select/dist/declarations/src/Select' {
+  export interface Props<
+    Option,
+    // eslint-disable-next-line no-unused-vars
+    IsMulti extends boolean,
+    // eslint-disable-next-line no-unused-vars
+    Group extends GroupBase<Option>
+  > {
+    prefix?: string;
+  }
+}
