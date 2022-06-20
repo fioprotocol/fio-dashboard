7af683f2b50d69fbcfac4d8704aab95ac5d296c6
andreyvEze <andreyv@eze.tech>
2022-06-20 20:39:48 +0300

    Payment webhook endpoint and service

---
+++ server/src/routes/payments.mjs
@@ -0,0 +1,11 @@
+import { makeServiceRunner } from '../tools';
+
+import PaymentsWebhook from '../services/payments/Webhook';
+
+export default {
+  webhook: makeServiceRunner(PaymentsWebhook, req => ({
+    body: req.body,
+    headers: req.headers,
+    hostname: req.hostname,
+  })),
+};
