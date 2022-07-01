7ea56dfed37f627722c8e59ee613a1f1be722137
Oleksii Trukhanov <trukhantech@gmail.com>
2022-07-01 11:19:05 +0300

    Add new REG contained flow

--- client/src/util/containedFlow.ts
+++ client/src/util/containedFlow.ts
@@ -6,7 +6,6 @@ export const validateContainedFlowActionQuery = (
   query: ContainedFlowQuery,
 ): boolean => {
   if (!query.action) throw new Error('Contained flow action is required');
-  if (!query.r) throw new Error('Redirect url is required');
 
   switch (removeExtraCharactersFromString(query.action)) {
     case removeExtraCharactersFromString(CONTAINED_FLOW_ACTIONS.SIGNNFT): {
@@ -14,6 +13,9 @@ export const validateContainedFlowActionQuery = (
       throw new Error(
         `Invalid params for ${CONTAINED_FLOW_ACTIONS.SIGNNFT} action`,
       );
+    }
+    case CONTAINED_FLOW_ACTIONS.REG: {
+      return true;
     }
     default:
       throw new Error('Invalid action');
