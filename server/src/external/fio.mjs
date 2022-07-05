d3dc11913562e8ef3d781ae25622645738798ab7
andreyvEze <andreyv@eze.tech>
2022-07-05 18:04:02 +0300

    Fix fio api execute action

--- server/src/external/fio.mjs
+++ server/src/external/fio.mjs
@@ -4,7 +4,11 @@ import fetch from 'node-fetch';
 import { Transactions } from '@fioprotocol/fiosdk/lib/transactions/Transactions';
 import { Constants } from '@fioprotocol/fiosdk/lib/utils/constants';
 
-import { FIO_ACTIONS, FIO_ACTIONS_TO_END_POINT_KEYS } from '../config/constants.js';
+import {
+  FIO_ACTIONS,
+  FIO_ACTIONS_TO_END_POINT_KEYS,
+  FIO_ADDRESS_DELIMITER,
+} from '../config/constants.js';
 
 import MathOp from '../services/math.mjs';
 import logger from '../logger.mjs';
@@ -29,6 +33,10 @@ const FIO_ACTION_NAMES = {
   [FIO_ACTIONS.rejectFundsRequest]: 'rejectfndreq',
   [FIO_ACTIONS.transferTokens]: 'trnsfiopubky',
   [FIO_ACTIONS.addBundledTransactions]: 'addbundles',
+};
+
+const FIO_ACCOUNT_NAMES = {
+  [FIO_ACTIONS.transferTokens]: 'fio.token',
 };
 
 class Fio {
@@ -65,6 +73,24 @@ class Fio {
     return FIOSDK.SUFToAmount(suf);
   }
 
+  amountToSUF(amount) {
+    if (!amount) return 0;
+    const floor = Math.floor(amount);
+    const tempResult = new MathOp(floor).mul(FIOSDK.SUFUnit).toNumber();
+
+    // get remainder
+    const remainder = new MathOp(amount)
+      .mod(1)
+      .round(9, 2)
+      .toNumber();
+
+    const remainderResult = new MathOp(remainder).mul(FIOSDK.SUFUnit).toNumber();
+    const floorRemainder = Math.floor(remainderResult);
+
+    // add integer and remainder
+    return new MathOp(tempResult).add(floorRemainder).toNumber();
+  }
+
   convertFioToUsdc(nativeAmount, roe) {
     if (roe == null) return 0;
 
@@ -72,6 +98,22 @@ class Fio {
       .mul(roe)
       .round(2, 1)
       .toNumber();
+  }
+
+  convertUsdcToFio(amount, roe) {
+    if (roe == null || !roe) throw new Error('INVALID_ROE');
+
+    return new MathOp(amount)
+      .div(roe)
+      .round(9, 2)
+      .toNumber();
+  }
+
+  logError(e) {
+    if (e.errorCode !== 404) logger.error(e.message);
+    if (e.json) {
+      logger.error(e.json);
+    }
   }
 
   async registrationFee(forDomain = false) {
@@ -97,34 +139,82 @@ class Fio {
     }
   }
 
-  async executeAction(action, params, auth, keys = {}) {
+  getActionParams(options) {
+    let actionParams = { tpid: options.tpid };
+    switch (options.action) {
+      case FIO_ACTIONS.registerFioAddress: {
+        actionParams = {
+          fio_address: `${options.address}${FIO_ADDRESS_DELIMITER}${options.domain}`,
+          owner_fio_public_key: options.publicKey,
+        };
+        break;
+      }
+      case FIO_ACTIONS.registerFioDomain: {
+        actionParams = {
+          fio_domain: options.domain,
+          owner_fio_public_key: options.publicKey,
+        };
+        break;
+      }
+      case FIO_ACTIONS.transferTokens: {
+        actionParams = {
+          payee_public_key: options.publicKey,
+          amount: options.amount,
+        };
+        break;
+      }
+      default:
+      //
+    }
+
+    return { ...actionParams, ...options.params };
+  }
+
+  async executeAction(action, params, auth = {}, keys = {}) {
     if (keys.private) {
       // todo: set new sdk and use it
     }
 
-    if (!params.maxFee) params.maxFee = DEFAULT_ACTION_FEE_AMOUNT;
+    if (!params.max_fee) params.max_fee = DEFAULT_ACTION_FEE_AMOUNT;
 
     try {
       if (auth.actor) {
         params.actor = auth.actor;
-        // todo: set permission, probably need to update fiosdk
+        params.permission = auth.permission;
       }
       const fioSdk = this.getMasterFioSDK();
       const preparedTrx = await fioSdk.pushTransaction(
-        '',
+        FIO_ACCOUNT_NAMES[action] || '',
         FIO_ACTION_NAMES[action],
         params,
       );
       const result = await fioSdk.executePreparedTrx(
-        this.actionEndPoints[FIO_ACTIONS_TO_END_POINT_KEYS[action]],
+        EndPoint[FIO_ACTIONS_TO_END_POINT_KEYS[action]],
         preparedTrx,
       );
 
       return result;
     } catch (err) {
       this.logError(err);
-      throw err;
+
+      let errorObj = {
+        message: err.message,
+      };
+
+      if (err.json) {
+        errorObj = { ...errorObj, ...err.json };
+      }
+
+      return errorObj;
     }
+  }
+
+  checkTxError(tx) {
+    const fieldError = Array.isArray(tx.fields) ? tx.fields.find(f => f.error) : null;
+
+    const notes = fieldError ? fieldError.error : JSON.stringify(tx);
+
+    return { notes };
   }
 }
 
