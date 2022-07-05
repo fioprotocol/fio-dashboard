bae151c405540b261b2ae803ada908a0979aa147
andreyvEze <andreyv@eze.tech>
2022-07-05 13:17:51 +0300

    Add common job class

---
+++ server/src/jobs/job.mjs
@@ -0,0 +1,41 @@
+import { parentPort } from 'worker_threads';
+
+import '../db';
+
+import logger from '../logger.mjs';
+
+const CANCEL_MSG = 'cancel';
+
+class CommonJob {
+  constructor() {
+    // store boolean if the job is cancelled
+    this.isCancelled = false;
+    // store boolean if the job is cancelled
+    if (parentPort)
+      parentPort.once('message', message => {
+        if (message === CANCEL_MSG) this.isCancelled = true;
+      });
+  }
+
+  postMessage(msg) {
+    if (parentPort) return parentPort.postMessage(msg);
+
+    logger.info(msg);
+  }
+
+  cancel() {
+    this.postMessage(CANCEL_MSG);
+  }
+
+  finish() {
+    process.exit(0);
+  }
+
+  async execute() {}
+
+  async executeActions(methods) {
+    await Promise.allSettled(methods);
+  }
+}
+
+export default CommonJob;
