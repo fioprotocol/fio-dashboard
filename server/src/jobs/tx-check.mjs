732eb57df7bb6824a2dc62d4c0c7b606464e3ff4
andreyvEze <andreyv@eze.tech>
2022-07-05 17:59:06 +0300

    Add check tx job. Fix BlockhainTransaction model queries

---
+++ server/src/jobs/tx-check.mjs
@@ -0,0 +1,120 @@
+import '../db';
+import {
+  BlockchainTransaction,
+  BlockchainTransactionEventLog,
+  OrderItemStatus,
+} from '../models/index.mjs';
+import CommonJob from './job.mjs';
+
+import { fioApi } from '../external/fio.mjs';
+
+import { FIO_ADDRESS_DELIMITER, FIO_ACTIONS } from '../config/constants.js';
+
+import logger from '../logger.mjs';
+
+class TxCheckJob extends CommonJob {
+  constructor() {
+    super();
+  }
+
+  async execute() {
+    await fioApi.getRawAbi();
+    // get transactions need to check if exists
+    const [items] = await BlockchainTransaction.checkIrreversibility();
+
+    this.postMessage(`Process tx items - ${items.length}`);
+
+    const processTxItem = async item => {
+      if (this.isCancelled) return false;
+
+      const { id, address, domain, action, params, publicKey, btId } = item;
+
+      this.postMessage(`Processing tx item id - ${id}`);
+
+      try {
+        const walletSdk = fioApi.getPublicFioSDK();
+        let status = BlockchainTransaction.STATUS.EXPIRE;
+
+        switch (action) {
+          case FIO_ACTIONS.registerFioAddress:
+          case FIO_ACTIONS.registerFioDomain: {
+            const { fio_addresses, fio_domains } = await walletSdk.getFioNames(
+              (params && params.owner_fio_public_key) || publicKey,
+            );
+            const isAddress = action === FIO_ACTIONS.registerFioAddress;
+            const fioName = isAddress
+              ? `${address}${FIO_ADDRESS_DELIMITER}${domain}`
+              : domain;
+
+            let found;
+            if (isAddress) {
+              found = fio_addresses.find(({ fio_address }) => fio_address === fioName);
+            } else {
+              found = fio_domains.find(({ fio_domain }) => fio_domain === fioName);
+            }
+
+            if (found) status = BlockchainTransaction.STATUS.SUCCESS;
+
+            break;
+          }
+          default:
+          //
+        }
+
+        try {
+          await BlockchainTransaction.sequelize.transaction(async t => {
+            await BlockchainTransaction.update(
+              {
+                status,
+              },
+              {
+                where: {
+                  id: btId,
+                  orderItemId: id,
+                  status: BlockchainTransaction.STATUS.PENDING,
+                },
+                transaction: t,
+              },
+            );
+
+            await BlockchainTransactionEventLog.create(
+              {
+                status,
+                blockchainTransactionId: btId,
+              },
+              { transaction: t },
+            );
+
+            await OrderItemStatus.update(
+              {
+                txStatus: status,
+              },
+              {
+                where: {
+                  orderItemId: id,
+                  blockchainTransactionId: btId,
+                  txStatus: BlockchainTransaction.STATUS.PENDING,
+                },
+                transaction: t,
+              },
+            );
+          });
+        } catch (error) {
+          logger.error(`TX ITEM PROCESSING ERROR ${item.id} - SQL UPDATE`, error);
+        }
+      } catch (e) {
+        logger.error(`TX ITEM PROCESSING ERROR ${item.id}`, e);
+      }
+
+      return true;
+    };
+
+    const methods = Object.values(items).map(item => processTxItem(item));
+
+    await this.executeActions(methods);
+
+    this.finish();
+  }
+}
+
+new TxCheckJob().execute();
