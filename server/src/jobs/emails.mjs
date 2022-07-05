936fd158af18e23f0fc03f2c1dc3bb0f9a7b0982
andreyvEze <andreyv@eze.tech>
2022-07-05 17:08:07 +0300

    Refactor jobs to use common job

--- server/src/jobs/emails.mjs
+++ server/src/jobs/emails.mjs
@@ -1,12 +1,13 @@
-import { parentPort } from 'worker_threads';
-
 import Sequelize from 'sequelize';
 
 import '../db';
-import logger from '../logger.mjs';
-import emailSender from '../services/emailSender.mjs';
 import { User, Notification } from '../models/index.mjs';
+import CommonJob from './job.mjs';
+
+import emailSender from '../services/emailSender.mjs';
 import { templates } from '../emails/emailTemplate.mjs';
+
+import logger from '../logger.mjs';
 
 const NOTIFICATION_LIMIT_PER_JOB = 100;
 const CONTENT_TYPE_EMAIL_TEMPLATE_MAP = {
@@ -18,133 +19,134 @@ const CONTENT_TYPE_EMAIL_TEMPLATE_MAP = {
   [Notification.CONTENT_TYPE.LOW_BUNDLE_TX]: templates.lowBundleCount,
 };
 
-// store boolean if the job is cancelled
-let isCancelled = false;
+class EmailsJob extends CommonJob {
+  constructor() {
+    super();
+  }
 
-// handle cancellation (this is a very simple example)
-if (parentPort)
-  parentPort.once('message', message => {
-    if (message === 'cancel') isCancelled = true;
-  });
-
-(async () => {
-  const notifications = await Notification.findAll({
-    where: {
-      emailDate: {
-        [Sequelize.Op.is]: null,
+  async execute() {
+    const notifications = await Notification.findAll({
+      where: {
+        emailDate: {
+          [Sequelize.Op.is]: null,
+        },
       },
-    },
-    include: [
-      { model: User, where: { status: User.STATUS.ACTIVE }, attributes: ['id', 'email'] },
-    ],
-    limit: NOTIFICATION_LIMIT_PER_JOB,
-    raw: true,
-    nest: true,
-  });
-
-  parentPort.postMessage(`Process notifications - ${notifications.length}`);
-
-  const handleNotifications = async notifications => {
-    if (isCancelled) return false;
-
-    parentPort.postMessage(
-      `Processing notifications id - ${notifications.map(n => n.id)}`,
-    );
-
-    try {
-      const {
-        data,
-        contentType,
-        User: { email },
-      } = notifications[0];
-
-      if (!data.emailData || !Object.keys(data.emailData).length) return false;
-
-      let sentEmailId = null;
-      try {
-        let emailData = data.emailData;
-
-        if (contentType === Notification.CONTENT_TYPE.LOW_BUNDLE_TX) {
-          emailData = {
-            fioCryptoHandles: notifications.map(n => n.data.emailData),
-          };
-        }
-
-        if (contentType === Notification.CONTENT_TYPE.DOMAIN_EXPIRE) {
-          emailData = {
-            domains: notifications.map(n => n.data.emailData),
-            expiringStatus: data.emailData.domainExpPeriod,
-          };
-        }
-
-        const emailResult = await emailSender.send(
-          CONTENT_TYPE_EMAIL_TEMPLATE_MAP[contentType],
-          email,
-          emailData,
-        );
-
-        sentEmailId = emailResult ? emailResult._id : null;
-      } catch (e) {
-        logger.error(`EMAIL SEND ERROR`, e);
-      }
-
-      if (sentEmailId) {
-        for (const notification of notifications) {
-          await Notification.update(
-            { emailDate: new Date(), data: { ...notification.data, sentEmailId } },
-            { where: { id: notification.id } },
-          );
-        }
-        parentPort.postMessage(
-          `Notification processed, email sent - ${notifications.map(n => n.id)}`,
-        );
-      }
-    } catch (e) {
-      logger.error(`NOTIFICATION PROCESSING ERROR`, e);
-    }
-
-    return true;
-  };
-
-  const notificationGroups = notifications.reduce((acc, notification) => {
-    if (!notification.data || !notification.data.emailData) return acc;
-
-    const firstSimilar = notifications.find(searchingItem => {
-      if (
-        notification.userId !== searchingItem.userId ||
-        notification.contentType !== searchingItem.contentType ||
-        !searchingItem.data ||
-        !searchingItem.data.emailData
-      )
-        return false;
-
-      const {
-        data: { emailData },
-      } = searchingItem;
-
-      // join domains into one notification email
-      if (
-        notification.contentType === Notification.CONTENT_TYPE.DOMAIN_EXPIRE &&
-        emailData.domainExpPeriod === notification.data.emailData.domainExpPeriod &&
-        emailData.name !== notification.data.emailData.name
-      )
-        return true;
-
-      return false;
+      include: [
+        {
+          model: User,
+          where: { status: User.STATUS.ACTIVE },
+          attributes: ['id', 'email'],
+        },
+      ],
+      limit: NOTIFICATION_LIMIT_PER_JOB,
+      raw: true,
+      nest: true,
     });
 
-    if (firstSimilar && acc[firstSimilar.id]) {
-      acc[firstSimilar.id] = [...acc[firstSimilar.id], notification];
-    } else acc[notification.id] = [notification];
+    this.postMessage(`Process notifications - ${notifications.length}`);
 
-    return acc;
-  }, {});
+    const handleNotifications = async notifications => {
+      if (this.isCancelled) return false;
 
-  const methods = Object.values(notificationGroups).map(sortedNotifications =>
-    handleNotifications(sortedNotifications),
-  );
+      this.postMessage(`Processing notifications id - ${notifications.map(n => n.id)}`);
 
-  await Promise.allSettled(methods);
+      try {
+        const {
+          data,
+          contentType,
+          User: { email },
+        } = notifications[0];
 
-  process.exit(0);
-})();
+        if (!data.emailData || !Object.keys(data.emailData).length) return false;
+
+        let sentEmailId = null;
+        try {
+          let emailData = data.emailData;
+
+          if (contentType === Notification.CONTENT_TYPE.LOW_BUNDLE_TX) {
+            emailData = {
+              fioCryptoHandles: notifications.map(n => n.data.emailData),
+            };
+          }
+
+          if (contentType === Notification.CONTENT_TYPE.DOMAIN_EXPIRE) {
+            emailData = {
+              domains: notifications.map(n => n.data.emailData),
+              expiringStatus: data.emailData.domainExpPeriod,
+            };
+          }
+
+          const emailResult = await emailSender.send(
+            CONTENT_TYPE_EMAIL_TEMPLATE_MAP[contentType],
+            email,
+            emailData,
+          );
+
+          sentEmailId = emailResult ? emailResult._id : null;
+        } catch (e) {
+          logger.error(`EMAIL SEND ERROR`, e);
+        }
+
+        if (sentEmailId) {
+          for (const notification of notifications) {
+            await Notification.update(
+              { emailDate: new Date(), data: { ...notification.data, sentEmailId } },
+              { where: { id: notification.id } },
+            );
+          }
+          this.postMessage(
+            `Notification processed, email sent - ${notifications.map(n => n.id)}`,
+          );
+        }
+      } catch (e) {
+        logger.error(`NOTIFICATION PROCESSING ERROR`, e);
+      }
+
+      return true;
+    };
+
+    const notificationGroups = notifications.reduce((acc, notification) => {
+      if (!notification.data || !notification.data.emailData) return acc;
+
+      const firstSimilar = notifications.find(searchingItem => {
+        if (
+          notification.userId !== searchingItem.userId ||
+          notification.contentType !== searchingItem.contentType ||
+          !searchingItem.data ||
+          !searchingItem.data.emailData
+        )
+          return false;
+
+        const {
+          data: { emailData },
+        } = searchingItem;
+
+        // join domains into one notification email
+        if (
+          notification.contentType === Notification.CONTENT_TYPE.DOMAIN_EXPIRE &&
+          emailData.domainExpPeriod === notification.data.emailData.domainExpPeriod &&
+          emailData.name !== notification.data.emailData.name
+        )
+          return true;
+
+        return false;
+      });
+
+      if (firstSimilar && acc[firstSimilar.id]) {
+        acc[firstSimilar.id] = [...acc[firstSimilar.id], notification];
+      } else acc[notification.id] = [notification];
+
+      return acc;
+    }, {});
+
+    const methods = Object.values(notificationGroups).map(sortedNotifications =>
+      handleNotifications(sortedNotifications),
+    );
+
+    await this.executeActions(methods);
+
+    this.finish();
+  }
+}
+
+new EmailsJob().execute();
