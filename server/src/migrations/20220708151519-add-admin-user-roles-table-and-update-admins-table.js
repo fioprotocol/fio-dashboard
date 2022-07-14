88cec7b7f225f9869f9cb6d641c89666eb460d68
NikolasStelmah <nikolasstelmah@ukr.net>
2022-07-14 18:38:29 +0300

    DASH-16 Add user roles ann user statuses tables for admins; extend admins table

---
+++ server/src/migrations/20220708151519-add-admin-user-roles-table-and-update-admins-table.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const { ADMIN_ROLES_IDS } = require('../config/constants');
+
+module.exports = {
+  up: async (QI, DT) => {
+    return QI.sequelize.transaction(async t => {
+      await QI.createTable(
+        'admin-users-roles',
+        {
+          id: {
+            type: DT.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+          },
+          role: {
+            type: DT.STRING,
+            unique: true,
+            allowNull: false,
+          },
+          createdAt: { type: DT.DATE },
+          updatedAt: { type: DT.DATE },
+          deletedAt: { type: DT.DATE },
+        },
+        { transaction: t },
+      );
+
+      await QI.bulkInsert(
+        'admin-users-roles',
+        [
+          { role: 'ADMIN', id: ADMIN_ROLES_IDS.ADMIN },
+          { role: 'SUPERADMIN', id: ADMIN_ROLES_IDS.SUPER_ADMIN },
+        ],
+        { transaction: t },
+      );
+
+      await QI.addColumn(
+        'admin-users',
+        'tfaSecret',
+        {
+          type: DT.STRING,
+          allowNull: true,
+        },
+        { transaction: t },
+      );
+
+      await QI.addColumn(
+        'admin-users',
+        'roleId',
+        {
+          type: DT.INTEGER,
+          allowNull: false,
+          defaultValue: 1,
+          references: {
+            model: 'admin-users-roles',
+            key: 'id',
+          },
+          onDelete: 'cascade',
+        },
+        { transaction: t },
+      );
+
+      await QI.removeColumn('admin-users', 'role', { transaction: t });
+    });
+  },
+
+  down: async (QI, DT) => {
+    await QI.removeColumn('admin-users', 'roleId');
+    await QI.removeColumn('admin-users', 'tfaSecret');
+    await QI.dropTable('admin-users-roles');
+    return QI.addColumn('admin-users', 'role', {
+      type: DT.STRING,
+      defaultValue: ADMIN_ROLES_IDS.ADMIN,
+    });
+  },
+};
