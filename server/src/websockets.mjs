d35ab3c9a9f62a8ee8317059b4057dc4f229230c
andreyvEze <andreyv@eze.tech>
2022-07-14 14:16:22 +0300

    DASH-104 Add websocket functionality to server. Add order status websocket. Add websocket hook.

---
+++ server/src/websockets.mjs
@@ -0,0 +1,118 @@
+import url from 'url';
+
+import _ from 'lodash';
+
+import WebSocket from 'ws';
+
+import AuthCheck from './services/auth/Check.mjs';
+import Exception from './services/Exception.mjs';
+import WsOrderStatus from './services/orders/WsStatus.mjs';
+
+import { runService, runWsService } from './tools.mjs';
+import logger from './logger.mjs';
+import X from './services/Exception.mjs';
+
+const WS_ENDPOINTS = {
+  NOTIFICATIONS: 'NOTIFICATIONS',
+  ORDER_STATUS: 'ORDER_STATUS',
+};
+
+const checkAuth = async token => {
+  return runService(AuthCheck, {
+    params: { token: `Bearer ${token}` },
+  });
+};
+
+const runWs = async (service, wsConnection, context, params, isPrivate = true) => {
+  try {
+    if (isPrivate) {
+      const userParams = await checkAuth(params.token);
+      context = { ...context, ...userParams };
+    }
+
+    const restParams = _.omit(params, ['endpoint', 'token']);
+
+    await runWsService(service, {
+      wsConnection,
+      context,
+      params: { data: restParams },
+    });
+  } catch (e) {
+    if (e instanceof Exception) {
+      wsConnection.send(
+        JSON.stringify({
+          status: 0,
+          error: e.toHash(),
+        }),
+      );
+    } else {
+      logger.error({
+        REQUEST_URL: wsConnection.url,
+        REQUEST_PARAMS: params,
+        ERROR_STACK: e.stack,
+      });
+
+      wsConnection.send(
+        JSON.stringify({
+          status: 0,
+          error: {
+            code: 'SERVER_ERROR',
+            message: 'Please, contact your system administrator!',
+          },
+        }),
+      );
+    }
+  }
+};
+
+export default expressServer => {
+  const websocketServer = new WebSocket.Server({
+    noServer: true,
+    path: '/ws',
+  });
+
+  expressServer.on('upgrade', (request, socket, head) => {
+    websocketServer.handleUpgrade(request, socket, head, websocket => {
+      websocketServer.emit('connection', websocket, request);
+    });
+  });
+
+  websocketServer.on('connection', (websocketConnection, connectionRequest) => {
+    const connectionUrl = connectionRequest && connectionRequest.url;
+    let queryParams;
+    try {
+      queryParams = url.parse(connectionUrl, true).query;
+    } catch (e) {
+      logger.error(e);
+    }
+
+    if (!queryParams.endpoint) {
+      throw new X({
+        code: 'NOT_FOUND',
+        fields: {
+          endpoint: 'NOT_FOUND',
+        },
+      });
+    }
+
+    switch (queryParams.endpoint) {
+      case WS_ENDPOINTS.ORDER_STATUS: {
+        runWs(WsOrderStatus, websocketConnection, {}, queryParams);
+        break;
+      }
+      case WS_ENDPOINTS.NOTIFICATIONS: {
+        // todo:
+        break;
+      }
+      default:
+        throw new X({
+          code: 'NOT_FOUND',
+          fields: {
+            endpoint: 'NOT_FOUND',
+          },
+        });
+    }
+  });
+
+  return websocketServer;
+};
