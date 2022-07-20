75e844e2d30c36ab131a5eac0e6c61f2751326ec
NikolasStelmah <nikolasstelmah@ukr.net>
2022-07-20 13:45:58 +0300

    DASH-137 Add usePagination, useQuery hooks and use cases, refactor AdminHomePage

--- client/src/pages/AdminUserListPage/types.d.ts
+++ client/src/pages/AdminUserListPage/types.d.ts
@@ -1,3 +1,15 @@
+import { AdminUser, AdminUserProfile } from '../../types';
+
 export type FormValuesProps = {
   inviteEmail: string;
+};
+
+export type PageProps = {
+  loading: boolean;
+  adminUsersCount: number;
+  adminUsersList: AdminUser[];
+  adminUserProfile?: AdminUserProfile;
+  getAdminList: (limit?: number, offset?: number) => Promise<void>;
+  getAdminUserProfile: (id: string) => Promise<void>;
+  removeAdminUser: (adminUserId: string) => void;
 };
