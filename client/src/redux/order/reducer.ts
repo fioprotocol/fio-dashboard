e4c1349b98dc9f50b875e40374c53835ad50d961
andreyvEze <andreyv@eze.tech>
2022-07-26 14:36:45 +0300

    Create order on free address registration. Clear order on cart.

--- client/src/redux/order/reducer.ts
+++ client/src/redux/order/reducer.ts
@@ -9,11 +9,25 @@ export default combineReducers({
   order(state: Order | null = null, action = {}) {
     switch (action.type) {
       case actions.CREATE_ORDER_REQUEST:
+      case actions.CLEAR_ORDER:
         return null;
       case actions.CREATE_ORDER_SUCCESS:
         return action.data;
       case LOGOUT_SUCCESS:
         return null;
+      default:
+        return state;
+    }
+  },
+  orderLoading(state: boolean = false, action = {}) {
+    switch (action.type) {
+      case actions.CREATE_ORDER_REQUEST:
+        return true;
+      case actions.CREATE_ORDER_SUCCESS:
+      case actions.CREATE_ORDER_FAILURE:
+        return false;
+      case LOGOUT_SUCCESS:
+        return false;
       default:
         return state;
     }
