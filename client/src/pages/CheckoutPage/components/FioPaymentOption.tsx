9f89f110c9242fba7faa64904db85576d042152c
Oleksii Trukhanov <trukhantech@gmail.com>
2022-07-29 14:32:56 +0300

    Fix show only wallets that has enough ballance for checkout

--- client/src/pages/CheckoutPage/components/FioPaymentOption.tsx
+++ client/src/pages/CheckoutPage/components/FioPaymentOption.tsx
@@ -7,30 +7,11 @@ import { PaymentWallet } from './PaymentWallet';
 import { PaymentOptionComponentProps } from '../types';
 
 export const FioPaymentOption: React.FC<PaymentOptionComponentProps> = props => {
-  const {
-    fioWallets,
-    paymentWalletPublicKey,
-    fioWalletsBalances,
-    walletBalances,
-    walletName,
-    costFree,
-    isFree,
-    onFinish,
-    setWallet,
-  } = props;
+  const { onFinish, ...rest } = props;
 
   return (
     <>
-      <PaymentWallet
-        fioWallets={fioWallets}
-        paymentWalletPublicKey={paymentWalletPublicKey}
-        fioWalletsBalances={fioWalletsBalances}
-        walletBalances={walletBalances}
-        walletName={walletName}
-        costFree={costFree}
-        isFree={isFree}
-        setWallet={setWallet}
-      />
+      <PaymentWallet {...rest} />
       <PurchaseNow onFinish={onFinish} />
     </>
   );
