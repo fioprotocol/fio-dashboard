15ae897c0c4681c59d60668706a6351017523567
Oleksii Trukhanov <trukhantech@gmail.com>
2022-08-03 20:51:11 +0300

    Add layout props into Loader component

--- client/src/components/Loader/Loader.tsx
+++ client/src/components/Loader/Loader.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classnames from 'classnames';
 
 import classes from './Loader.module.scss';
 
-const Loader: React.FC = () => {
+type Props = {
+  hasAutoWidth?: boolean;
+  hasInheritFontSize?: boolean;
+  isWhite?: boolean;
+};
+
+const Loader: React.FC<Props> = props => {
+  const { hasAutoWidth, hasInheritFontSize, isWhite } = props;
+
   return (
-    <div className={classes.loader}>
+    <div
+      className={classnames(
+        classes.loader,
+        hasAutoWidth && classes.hasAutoWidth,
+        hasInheritFontSize && classes.hasInheritFontSize,
+        isWhite && classes.isWhite,
+      )}
+    >
       <FontAwesomeIcon icon="spinner" spin />
     </div>
   );
