22c629b565d0a1027cac1770139fb07ab1beeba5
Vadym Karpenko <vadim.karpenko.306@gmail.com>
2022-09-20 17:57:44 +0300

    DASH-163 Fix title and params for ref

--- client/src/redux/refProfile/sagas.ts
+++ client/src/redux/refProfile/sagas.ts
@@ -6,9 +6,11 @@ import { Action } from '../types';
 
 export function* getRefProfileSuccess(): Generator {
   yield takeEvery(GET_REF_PROFILE_SUCCESS, function*(action: Action) {
-    yield window.dataLayer?.push({
-      event: 'userData',
-      refid: action.data?.code,
-    });
+    if (action.data?.code) {
+      yield window.dataLayer?.push({
+        event: 'userData',
+        refid: action.data?.code,
+      });
+    }
   });
 }
