ee2f7fb0997adb513e1f5fd8fca9b7f2cd9bfcdf
Vadym Karpenko <vadym.karpenko@aleph1.io>
2022-09-22 15:02:16 +0300

    DASH-161 Rename event name and param

--- client/src/constants/common.ts
+++ client/src/constants/common.ts
@@ -57,7 +57,7 @@ export const ANALYTICS_EVENT_ACTIONS = {
   SIGN_UP: 'sign_up',
   ENABLE_TWO_FACTOR: 'sec_2fa',
   RECOVERY: 'sec_recovery',
-  PAGE_VIEW: 'page_view',
+  PAGE_VIEW: 'pageview',
   CHAIN_ERROR: 'chain_error',
 };
 
