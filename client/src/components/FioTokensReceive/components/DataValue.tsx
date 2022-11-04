22bbcc0c1e2ea4d6e41460d6de14e38c2cf8aa2f
Vadym Karpenko <vadym.karpenko@aleph1.io>
2022-11-04 10:28:42 +0200

    DASH-148 Add manual pub key selection

--- client/src/components/FioTokensReceive/components/DataValue.tsx
+++ client/src/components/FioTokensReceive/components/DataValue.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { MouseEvent, useCallback, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { copyToClipboard } from '../../../util/general';
@@ -9,18 +9,32 @@ type Props = {
   value: string;
 };
 
-export const DataValue: React.FC<Props> = props => {
-  const onClick = () => {
-    copyToClipboard(props.value);
-  };
+export const DataValue: React.FC<Props> = ({ value }) => {
+  const valueRef = useRef<HTMLDivElement | null>(null);
+
+  const onCopy = useCallback(() => {
+    copyToClipboard(value);
+  }, [value]);
+
+  const onClick = useCallback((event: MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    if (event.detail === 2) {
+      const range = document.createRange();
+      range.selectNodeContents(valueRef.current);
+
+      const selection = window.getSelection();
+      selection?.removeAllRanges();
+      selection?.addRange(range);
+    }
+  }, []);
 
   return (
-    <div className={classes.value}>
-      {props.value}
+    <div className={classes.value} onClick={onClick} ref={valueRef}>
+      {value}
       <FontAwesomeIcon
         className={classes.icon}
         icon={{ prefix: 'far', iconName: 'copy' }}
-        onClick={onClick}
+        onClick={onCopy}
       />
     </div>
   );
