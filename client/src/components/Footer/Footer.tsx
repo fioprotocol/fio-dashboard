d49c4eb0a6bac32630e0b1baf6a9a0ae2a14313c
Oleksii Trukhanov <trukhantech@gmail.com>
2022-11-15 15:24:54 +0200

    DASH-310 Enable hide navigation links on MainHeader and Footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -7,34 +7,49 @@ import { ROUTES } from '../../constants/routes';
 import { currentYear } from '../../utils';
 import classes from './Footer.module.scss';
 
-const Footer: React.FC = props => {
+type Props = {
+  hideNavLinks?: boolean;
+};
+
+const NavLinks: React.FC<Props> = props => {
+  const { hideNavLinks } = props;
+
+  if (hideNavLinks) return null;
+
+  return (
+    <>
+      <Nav.Link
+        to={ROUTES.PRIVACY_POLICY}
+        as={Link}
+        className={classnames(classes.navItem, 'text-white', 'p-0')}
+      >
+        Privacy Policy
+      </Nav.Link>
+      <Nav.Link
+        to={ROUTES.TERMS_OF_SERVICE}
+        as={Link}
+        className={classnames(classes.navItem, 'text-white', 'p-0')}
+      >
+        Terms of Service
+      </Nav.Link>
+    </>
+  );
+};
+
+const Footer: React.FC<Props> = props => {
+  const { hideNavLinks } = props;
+
   return (
     <section className={classes.footer}>
-      <Link to={ROUTES.HOME}>
+      <Link to={ROUTES.HOME} className={classes.logoLink}>
         <div className={classes.logo} />
       </Link>
       <hr className={classes.divider} />
       <Nav className="pr-0 align-items-center">
-        <Nav.Link
-          to={ROUTES.PRIVACY_POLICY}
-          as={Link}
-          className={classnames(classes.navItem, 'text-white', 'p-0')}
-        >
-          Privacy Policy
-        </Nav.Link>
-        <Nav.Link
-          to={ROUTES.TERMS_OF_SERVICE}
-          as={Link}
-          className={classnames(classes.navItem, 'text-white', 'p-0')}
-        >
-          Terms of Service
-        </Nav.Link>
-        <Nav.Link
-          href="#"
-          className={classnames(classes.navItem, 'text-white', 'p-0')}
-        >
+        <NavLinks hideNavLinks={hideNavLinks} />
+        <Nav.Item className={classnames(classes.navItem, 'text-white', 'p-0')}>
           © {currentYear()} FIO
-        </Nav.Link>
+        </Nav.Item>
       </Nav>
     </section>
   );
