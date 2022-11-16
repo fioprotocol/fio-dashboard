ec910300998728aca68d5e466f9b3f8e2f2ea089
Oleksii Trukhanov <trukhantech@gmail.com>
2022-11-16 18:20:03 +0200

    DASH-310 Move google tag manager into own hook

---
+++ client/src/hooks/googleTagManager.ts
@@ -0,0 +1,43 @@
+import { useCallback } from 'react';
+
+import useEffectOnce from './general';
+
+type UseGTMGlobalTagsProps = {
+  disableLoading?: boolean;
+};
+
+export const useGTMGlobalTags = (props?: UseGTMGlobalTagsProps) => {
+  const { disableLoading } = props || {};
+
+  const addGTMGlobalTags = useCallback(() => {
+    if (!process.env.REACT_APP_GOOGLE_TAG_MANAGER_ID) {
+      return;
+    }
+    const script = document.createElement('script');
+    script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.REACT_APP_GOOGLE_TAG_MANAGER_ID}');`;
+    const noscript = document.createElement('noscript');
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://www.googletagmanager.com/ns.html?id=${process.env.REACT_APP_GOOGLE_TAG_MANAGER_ID}`;
+    iframe.height = '0';
+    iframe.width = '0';
+    iframe.style.cssText = 'display:none;visibility:hidden';
+    noscript.appendChild(iframe);
+
+    document.head.append(
+      document.createComment(' Google Tag Manager '),
+      script,
+      document.createComment(' End Google Tag Manager '),
+    );
+    document.body.prepend(
+      document.createComment(' Google Tag Manager (noscript) '),
+      noscript,
+      document.createComment(' End Google Tag Manager (noscript) '),
+    );
+  }, []);
+
+  useEffectOnce(() => {
+    if (!disableLoading) {
+      addGTMGlobalTags();
+    }
+  }, [disableLoading, addGTMGlobalTags]);
+};
