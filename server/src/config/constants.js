ea52cc4f64dbee1073a805cac837bc7f903ae3d2
Vadym Karpenko <vadym.karpenko@aleph1.io>
2023-01-06 16:10:29 +0200

    DASH-233 Update emails subject and format date

--- server/src/config/constants.js
+++ server/src/config/constants.js
@@ -11,18 +11,18 @@ const DOMAIN_EXP_PERIOD = {
 };
 
 const EXPIRING_DOMAINS_EMAIL_SUBJECTS = {
-  [DOMAIN_EXP_PERIOD.ABOUT_TO_EXPIRE]: 'Your FIO domain(s) is about to expire',
-  [DOMAIN_EXP_PERIOD.EXPIRED_30]: 'Your FIO domain(s) has expired',
-  [DOMAIN_EXP_PERIOD.EXPIRED_90]: 'Your FIO domain(s) has expired and will be burned',
+  [DOMAIN_EXP_PERIOD.ABOUT_TO_EXPIRE]: 'Your FIO domain(s) are about to expire',
+  [DOMAIN_EXP_PERIOD.EXPIRED_30]: 'Your FIO domain(s) have expired',
+  [DOMAIN_EXP_PERIOD.EXPIRED_90]: 'Your FIO domain(s) have expired and will be burned',
   [DOMAIN_EXP_PERIOD.EXPIRED]:
     'Your FIO domain(s) and associated addresses have been burned',
 };
 
 const EXPIRING_DOMAINS_EMAIL_TITLE = {
   [DOMAIN_EXP_PERIOD.ABOUT_TO_EXPIRE]: 'FIO Domain(s) Expiring',
-  [DOMAIN_EXP_PERIOD.EXPIRED_30]: 'FIO Domain(s) has Expired',
+  [DOMAIN_EXP_PERIOD.EXPIRED_30]: 'FIO Domain(s) have Expired',
   [DOMAIN_EXP_PERIOD.EXPIRED_90]: 'FIO Domain(s) will be burned',
-  [DOMAIN_EXP_PERIOD.EXPIRED]: 'FIO Domain(s) has been burned',
+  [DOMAIN_EXP_PERIOD.EXPIRED]: 'FIO Domain(s) have been burned',
 };
 
 const FIO_ACTIONS = {
