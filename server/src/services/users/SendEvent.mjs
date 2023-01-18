f27199c05b2fe147340b0df49fd3b11f44d9a8dc
Vadym Karpenko <vadym.karpenko@aleph1.io>
2023-01-18 17:01:12 +0200

    DASH-348 Fix send event for non-authorized user

--- server/src/services/users/SendEvent.mjs
+++ server/src/services/users/SendEvent.mjs
@@ -10,10 +10,12 @@ export default class SendEvent extends Base {
   }
 
   async execute({ event }) {
-    const user = await User.findActive(this.context.id);
+    if (this.context.id) {
+      const user = await User.findActive(this.context.id);
 
-    if (user.isOptIn) {
-      await marketingMailchimp.sendEvent(user.email, event);
+      if (user.isOptIn) {
+        await marketingMailchimp.sendEvent(user.email, event);
+      }
     }
 
     return { data: { success: true } };
