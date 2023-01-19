a763679a7d8f2296fe19106d49b7b3c39a7b9724
Oleksii Trukhanov <trukhantech@gmail.com>
2023-01-20 00:07:52 +0200

    DASH-480 Pre-populate address from homepage redirect

--- client/src/pages/FioAddressSelectionPage/components/AddressFormContainer.tsx
+++ client/src/pages/FioAddressSelectionPage/components/AddressFormContainer.tsx
@@ -6,10 +6,13 @@ import debounce from 'lodash/debounce';
 import TextInput, {
   INPUT_COLOR_SCHEMA,
 } from '../../../components/Input/TextInput';
-
 import { AddressDomainFormContainer } from '../../../components/AddressDomainFormContainer';
 
 import { DEFAULT_DEBOUNCE_TIMEOUT } from '../../../constants/timeout';
+import { QUERY_PARAMS_NAMES } from '../../../constants/queryParams';
+
+import useQuery from '../../../hooks/useQuery';
+import useEffectOnce from '../../../hooks/general';
 
 type Props = {
   loading: boolean;
@@ -30,9 +33,18 @@ export const AddressFormContainer: React.FC<Props> = props => {
     DEFAULT_DEBOUNCE_TIMEOUT,
   );
 
+  const queryParams = useQuery();
+  const address = queryParams.get(QUERY_PARAMS_NAMES.ADDRESS);
+
+  useEffectOnce(() => {
+    if (address) {
+      handleChange(address);
+    }
+  }, [address]);
+
   return (
     <AddressDomainFormContainer>
-      <Form onSubmit={() => null}>
+      <Form onSubmit={() => null} initialValues={{ address }}>
         {({ handleSubmit }) => (
           <form onSubmit={handleSubmit}>
             <Field
