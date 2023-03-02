ace86ca9e6ba51566bf3f925a7b1762cbbd07c71
Ilkin Guluzada <ilking@cybcube.com>
2023-03-02 20:39:22 +0400

    DASH-563: update null on email

--- server/src/services/emailSender.mjs
+++ server/src/services/emailSender.mjs
@@ -22,7 +22,9 @@ class EmailSender {
         break;
 
       case 'SENDINBLUE':
-        transport = new SendinblueTransport({ apiKey: config.mail.sendinblueKey });
+        transport = new SendinblueTransport({
+          apiKey: config.mail.sendinblueKey,
+        });
         break;
 
       case 'SMTP':
@@ -241,9 +243,25 @@ class EmailSender {
         return {
           subject: 'Reg Site insufficient funds',
           body: `
-<p>Registration site got insufficient funds error when registering <b>${sendData.fioName}</b> on profile <b>${sendData.refProfileName}</b> using account <i>${sendData.authorization.actor}</i> and permission <i>${sendData.authorization.permission}</i>.</p>
+<p>Registration site got insufficient funds error when registering <b>${
+            sendData.fioName
+          }${
+            sendData.refProfileName
+              ? '</b> on profile <b>' + sendData.refProfileName
+              : '</b> on <b> FIO'
+          }</b> using account <i>${
+            sendData.authorization.actor
+          }</i> and permission <i>${sendData.authorization.permission}</i>.</p>
 <p>You will receive this email once every 24 hrs or until issue is resolved.</p>`,
-          text: `Registration site got insufficient funds error when registering ${sendData.fioName} on profile ${sendData.refProfileName} using account ${sendData.authorization.actor} and permission ${sendData.authorization.permission}. You will receive this email once every 24 hrs or until issue is resolved.`,
+          text: `Registration site got insufficient funds error when registering ${
+            sendData.fioName
+          } on ${
+            sendData.refProfileName
+              ? 'profile ' + sendData.refProfileName
+              : 'FIO'
+          } using account ${sendData.authorization.actor} and permission ${
+            sendData.authorization.permission
+          }. You will receive this email once every 24 hrs or until issue is resolved.`,
         };
 
       case templates.emailSenderErrorEmail:
@@ -305,7 +323,9 @@ class EmailSender {
         };
     }
 
-    throw new Error(`There is no email template with such name - ${templateName}`);
+    throw new Error(
+      `There is no email template with such name - ${templateName}`,
+    );
   }
 }
 
