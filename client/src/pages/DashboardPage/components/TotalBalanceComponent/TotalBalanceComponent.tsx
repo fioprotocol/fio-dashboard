ec5e6def6339b23d4c49ba833b53e08cbfc1ddd2
Oleksii Trukhanov <trukhantech@gmail.com>
2023-03-07 15:09:13 +0200

    Add ItemWrapper with border for DashboardPage items

--- client/src/pages/DashboardPage/components/TotalBalanceComponent/TotalBalanceComponent.tsx
+++ client/src/pages/DashboardPage/components/TotalBalanceComponent/TotalBalanceComponent.tsx
@@ -5,6 +5,8 @@ import Badge, { BADGE_TYPES } from '../../../../components/Badge/Badge';
 import Loader from '../../../../components/Loader/Loader';
 import SubmitButton from '../../../../components/common/SubmitButton/SubmitButton';
 
+import { ItemWrapper } from '../ItemWrapper';
+
 import { ROUTES } from '../../../../constants/routes';
 
 import MathOp from '../../../../util/math';
@@ -46,70 +48,74 @@ export const TotalBalanceComponent: React.FC<Props> = props => {
   const { loading, totalBalance } = props;
 
   return (
-    <Badge show type={BADGE_TYPES.BLACK} hasDefaultFontSize withoutMargin>
-      <div className={classes.badgeContainer}>
-        <p className={classes.title}>Total FIO Wallet Balance:</p>
-        <div className={classes.balance}>
-          <span className={classes.fioBalance}>
-            <BalanceComponent balance={totalBalance?.fio} loading={loading} />{' '}
-            FIO
-          </span>{' '}
-          (
-          <BalanceComponent
-            balance={totalBalance?.usdc}
-            loading={loading}
-          />{' '}
-          USDC)
+    <ItemWrapper>
+      <Badge show type={BADGE_TYPES.BLACK} hasDefaultFontSize withoutMargin>
+        <div className={classes.badgeContainer}>
+          <p className={classes.title}>Total FIO Wallet Balance:</p>
+          <div className={classes.balance}>
+            <span className={classes.fioBalance}>
+              <BalanceComponent balance={totalBalance?.fio} loading={loading} />{' '}
+              FIO
+            </span>{' '}
+            (
+            <BalanceComponent
+              balance={totalBalance?.usdc}
+              loading={loading}
+            />{' '}
+            USDC)
+          </div>
+          <div className={classes.actionContainer}>
+            <ActionButtonContainer
+              show={!loading && new MathOp(totalBalance?.fio).gt(0)}
+            >
+              <Link to={ROUTES.FIO_TOKENS_GET}>
+                <SubmitButton
+                  hasAutoHeight
+                  isButtonType
+                  isWhite
+                  hasNoSidePaddings
+                  text={
+                    <span className={classes.actionText}>Buy FIO Tokens</span>
+                  }
+                />
+              </Link>
+            </ActionButtonContainer>
+            <ActionButtonContainer
+              show={!loading && new MathOp(totalBalance?.fio).gt(0)}
+            >
+              <Link to={ROUTES.IMPORT_WALLET}>
+                <SubmitButton
+                  hasAutoHeight
+                  isButtonType
+                  isWhiteBordered
+                  isTransparent
+                  hasWhiteHoverBackground
+                  hasHoveredBlackTextColor
+                  hasNoSidePaddings
+                  text={
+                    <span className={classes.actionText}>Import Wallet</span>
+                  }
+                />
+              </Link>
+            </ActionButtonContainer>
+            <ActionButtonContainer
+              show={!loading && new MathOp(totalBalance?.fio).eq(0)}
+            >
+              <Link to={ROUTES.TOKENS}>
+                <SubmitButton
+                  hasAutoHeight
+                  isButtonType
+                  isWhite
+                  hasNoSidePaddings
+                  text={
+                    <span className={classes.actionText}>Manage Wallets</span>
+                  }
+                />
+              </Link>
+            </ActionButtonContainer>
+          </div>
         </div>
-        <div className={classes.actionContainer}>
-          <ActionButtonContainer
-            show={!loading && new MathOp(totalBalance?.fio).gt(0)}
-          >
-            <Link to={ROUTES.FIO_TOKENS_GET}>
-              <SubmitButton
-                hasAutoHeight
-                isButtonType
-                isWhite
-                hasNoSidePaddings
-                text={
-                  <span className={classes.actionText}>Buy FIO Tokens</span>
-                }
-              />
-            </Link>
-          </ActionButtonContainer>
-          <ActionButtonContainer
-            show={!loading && new MathOp(totalBalance?.fio).gt(0)}
-          >
-            <Link to={ROUTES.IMPORT_WALLET}>
-              <SubmitButton
-                hasAutoHeight
-                isButtonType
-                isWhiteBordered
-                isTransparent
-                hasWhiteHoverBackground
-                hasHoveredBlackTextColor
-                hasNoSidePaddings
-                text={<span className={classes.actionText}>Import Wallet</span>}
-              />
-            </Link>
-          </ActionButtonContainer>
-          <ActionButtonContainer
-            show={!loading && new MathOp(totalBalance?.fio).eq(0)}
-          >
-            <Link to={ROUTES.TOKENS}>
-              <SubmitButton
-                hasAutoHeight
-                isButtonType
-                isWhite
-                hasNoSidePaddings
-                text={
-                  <span className={classes.actionText}>Manage Wallets</span>
-                }
-              />
-            </Link>
-          </ActionButtonContainer>
-        </div>
-      </div>
-    </Badge>
+      </Badge>
+    </ItemWrapper>
   );
 };
