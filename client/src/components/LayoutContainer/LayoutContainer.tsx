085ab8493d331282f552f4e9c5758273306c4d7f
Oleksii Trukhanov <trukhantech@gmail.com>
2023-03-07 14:49:31 +0200

    DASH-415 Make title as optional on LayoutContainer, remove unused onTitleClick prop

--- client/src/components/LayoutContainer/LayoutContainer.tsx
+++ client/src/components/LayoutContainer/LayoutContainer.tsx
@@ -4,17 +4,14 @@ import classes from './LayoutContainer.module.scss';
 
 type Props = {
   children?: React.ReactNode;
-  title: string | React.ReactNode;
-  onTitleClick?: () => void;
+  title?: string | React.ReactNode;
 };
 
 const LayoutContainer: React.FC<Props> = props => {
-  const { children, title, onTitleClick } = props;
+  const { children, title } = props;
   return (
     <div className={classes.container}>
-      <h3 className={classes.title} onClick={onTitleClick}>
-        {title}
-      </h3>
+      {title && <h3 className={classes.title}>{title}</h3>}
       {children}
     </div>
   );
