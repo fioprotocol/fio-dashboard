5dfb0296e91dc79bc3474159f2a82a452048a32f
Oleksii Trukhanov <trukhantech@gmail.com>
2023-03-20 18:00:06 +0200

    DASH-600 Redesign FIO101 slider UI

--- client/src/pages/DashboardPage/components/Fio101SliderComponent/Fio101SliderComponent.tsx
+++ client/src/pages/DashboardPage/components/Fio101SliderComponent/Fio101SliderComponent.tsx
@@ -27,49 +27,45 @@ export const Fio101SliderComponent: React.FC<Props> = props => {
 
   return (
     <div className={classes.container}>
-      <div
-        className={classnames(
-          classes.mainContentContainer,
-          videoId && classes.hasVideo,
-        )}
-      >
-        <img src={imageSrc} alt={title} className={classes.img} />
-        <div className={classes.textContainer}>
-          <h5 className={classes.title}>{title}</h5>
-          <p className={classes.text}>{text}</p>
-          <Link to={link} className={classes.link}>
-            <SubmitButton
-              text={<span className={classes.buttonText}>{buttonText}</span>}
-              isButtonType
-              hasLowHeight
-            />
-          </Link>
-        </div>
-      </div>
-      {videoId && (
-        <div className={classes.videoContainer}>
-          <div
-            className={classnames(
-              classes.cover,
-              showVideo && classes.showVideo,
-            )}
-            onClick={onClick}
-          >
-            <PlayCircleFilled className={classes.icon} />
+      <div className={classes.mediaContainer}>
+        {videoId ? (
+          <div className={classes.videoContainer}>
+            <div
+              className={classnames(
+                classes.cover,
+                showVideo && classes.showVideo,
+              )}
+              onClick={onClick}
+            >
+              <PlayCircleFilled className={classes.icon} />
+            </div>
+            <iframe
+              className={classnames(
+                classes.videoFrame,
+                showVideo && classes.showVideo,
+              )}
+              src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+              frameBorder="0"
+            ></iframe>
           </div>
-          <iframe
-            className={classnames(
-              classes.videoFrame,
-              showVideo && classes.showVideo,
-            )}
-            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`}
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-            frameBorder="0"
-          ></iframe>
-        </div>
-      )}
+        ) : (
+          <img src={imageSrc} alt={title} className={classes.img} />
+        )}
+      </div>
+      <div className={classes.textContainer}>
+        <h5 className={classes.title}>{title}</h5>
+        <p className={classes.text}>{text}</p>
+        <Link to={link} className={classes.link}>
+          <SubmitButton
+            text={<span className={classes.buttonText}>{buttonText}</span>}
+            isButtonType
+            hasLowHeight
+          />
+        </Link>
+      </div>
     </div>
   );
 };
