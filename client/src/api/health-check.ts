11f9384a8d87c736ff1d5027e728aa3044aee870
Ilkin Guluzada <ilking@cybcube.com>
2023-03-21 11:40:18 +0400

    DASH-577: adding apis to client side

---
+++ client/src/api/health-check.ts
@@ -0,0 +1,8 @@
+import Base from './base';
+import { HealthCheckResponse } from './responses';
+
+export default class Var extends Base {
+  ping(): Promise<HealthCheckResponse> {
+    return this.apiClient.get('ping');
+  }
+}
---
+++ client/src/api/vars.ts
@@ -0,0 +1,12 @@
+import Base from './base';
+import { VarsResponse } from './responses';
+
+export default class Var extends Base {
+  getVar(key: string): Promise<VarsResponse> {
+    return this.apiClient.get(`vars/${key}`);
+  }
+
+  update(key: string, value: string): Promise<VarsResponse> {
+    return this.apiClient.post(`vars/update/${key}`, { value });
+  }
+}
