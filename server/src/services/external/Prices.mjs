bb88712674f2311b2d53490acc071536c569adad
Oleksii Trukhanov <trukhantech@gmail.com>
2023-03-29 17:00:56 +0300

    DASH-584 Handle fio sdk error on a server side. Throw error if there is no prices.

--- server/src/services/external/Prices.mjs
+++ server/src/services/external/Prices.mjs
@@ -2,6 +2,7 @@ import { fioApi } from '../../external/fio';
 import { getROE } from '../../external/roe';
 import logger from '../../logger';
 import Base from '../Base';
+import X from '../Exception.mjs';
 
 export default class Prices extends Base {
   static get validationRules() {
@@ -26,8 +27,18 @@ export default class Prices extends Base {
 
       pricing.usdtRoe = await roePromise;
       pricing.nativeFio = await pricesPromise;
+
+      if (!pricing.usdtRoe || Object.values(pricing.nativeFio).some(price => !price)) {
+        throw new Error('Cannot get prices or roe');
+      }
     } catch (e) {
-      logger.error(`Get prices from reg site error: ${e}`);
+      logger.error(`[Service Prices] Get Prices error: ${e}`);
+      throw new X({
+        code: 'SERVER_ERROR',
+        fields: {
+          pricing: 'SERVER_ERROR',
+        },
+      });
     }
     return {
       data: { pricing },
