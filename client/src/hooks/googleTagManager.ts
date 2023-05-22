fdc804e7c2427c1e2783448600c1d34f0e3f6cad
Oleksii Trukhanov <trukhantech@gmail.com>
2023-05-22 12:00:59 +0300

    DASH-617 Fix GTM client url

--- client/src/hooks/googleTagManager.ts
+++ client/src/hooks/googleTagManager.ts
@@ -15,7 +15,7 @@ export function useGTMGlobalTags(): void {
     script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');`;
     const noscript = document.createElement('noscript');
     const iframe = document.createElement('iframe');
-    iframe.src = `https://www.googletagmanager.com/ns.html?id=${GOOGLE_TAG_MANAGER_ID}}`;
+    iframe.src = `https://www.googletagmanager.com/ns.html?id=${GOOGLE_TAG_MANAGER_ID}`;
     iframe.height = '0';
     iframe.width = '0';
     iframe.style.cssText = 'display:none;visibility:hidden';
