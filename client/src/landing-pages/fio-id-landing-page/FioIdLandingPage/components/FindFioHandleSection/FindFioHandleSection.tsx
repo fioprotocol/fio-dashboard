5dfcec9b761656516e41b1259e261a827f213944
Oleksii Trukhanov <trukhantech@gmail.com>
2023-06-14 00:45:50 +0300

    DASH-687 Make FioProfileActionBadge buttons more generic. UI fixes.

--- client/src/landing-pages/fio-id-landing-page/FioIdLandingPage/components/FindFioHandleSection/FindFioHandleSection.tsx
+++ client/src/landing-pages/fio-id-landing-page/FioIdLandingPage/components/FindFioHandleSection/FindFioHandleSection.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 
-import { FioProfileActionBadge } from '../../../../../components/FioProfileActionBadge';
+import {
+  FioProfileActionBadge,
+  ACTION_BUTTONS_NAMES,
+} from '../../../../../components/FioProfileActionBadge';
 import { FindFioHandleForm } from '../FindFioHandleForm';
 
 import FioHandleImageSrc from '../../../../../assets/images/fio-handle-image.png';
 
 import classes from './FindFioHandle.module.scss';
 
-export const FindFioHandleSection: React.FC = () => {
+type Props = {
+  fioBaseUrl: string;
+};
+
+export const FindFioHandleSection: React.FC<Props> = props => {
+  const { fioBaseUrl } = props;
+
   return (
     <div className={classes.container}>
-      <FioProfileActionBadge />
+      <FioProfileActionBadge
+        fioBaseUrl={fioBaseUrl}
+        actionButtons={[
+          ACTION_BUTTONS_NAMES.GET_FIO_HANDLE,
+          ACTION_BUTTONS_NAMES.MANAGE_FIO_HANDLE,
+        ]}
+      />
       <div className={classes.contentContainer}>
-        <FindFioHandleForm />
+        <FindFioHandleForm fioBaseUrl={fioBaseUrl} />
         <div className={classes.imageContainer}>
           <img
             src={FioHandleImageSrc}
