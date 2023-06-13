d85c933876ed3667d24ca6a61c6c0bc6637166d1
Oleksii Trukhanov <trukhantech@gmail.com>
2023-06-13 19:17:17 +0300

    DASH-687 Add FCHSpecialBanners component to fio id landing page

--- client/src/landing-pages/fio-id-landing-page/FioIdLandingPage/FioIdLandingPage.tsx
+++ client/src/landing-pages/fio-id-landing-page/FioIdLandingPage/FioIdLandingPage.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { MainLayoutContainer } from '../../../components/MainLayoutContainer';
 import { FindFioHandleSection } from './components/FindFioHandleSection';
+import { FCHSpecialBanners } from './components/FCHSpecialBanners';
 
 const FioIdLandingPage: React.FC = () => {
   return (
     <MainLayoutContainer>
       <FindFioHandleSection />
+      <FCHSpecialBanners />
     </MainLayoutContainer>
   );
 };
