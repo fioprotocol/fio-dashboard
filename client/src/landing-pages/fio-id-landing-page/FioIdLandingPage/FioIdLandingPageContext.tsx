64080af0a07ea2fa2f81022fdcb820859d3d8987
Oleksii Trukhanov <trukhantech@gmail.com>
2023-06-20 19:00:06 +0300

    DASH-688 Implement Profile Payment Tab page

--- client/src/landing-pages/fio-id-landing-page/FioIdLandingPage/FioIdLandingPageContext.tsx
+++ client/src/landing-pages/fio-id-landing-page/FioIdLandingPage/FioIdLandingPageContext.tsx
@@ -1,14 +1,71 @@
+import { useCallback, useState } from 'react';
+
 import { useCheckIfDesktop } from '../../../screenType';
-import { transformBaseUrl } from '../../../util/general';
+import { log, transformBaseUrl } from '../../../util/general';
+import apis from '../../../api';
+import useEffectOnce from '../../../hooks/general';
 
 type UseContext = {
   fioBaseUrl: string;
+  fch: string;
   isDesktop: boolean;
+  isValidating: boolean;
+  resetPath: () => void;
+  setFch: (fch: string) => void;
 };
 
 export const useContext = (): UseContext => {
   const fioBaseUrl = transformBaseUrl();
   const isDesktop = useCheckIfDesktop();
 
-  return { fioBaseUrl, isDesktop };
+  const [fch, setFch] = useState<string>('');
+  const [isValidating, toggleIsValidating] = useState<boolean>(false);
+
+  const pathnames = window.location.pathname?.split('/');
+  const pathname = pathnames[1];
+
+  const validateFCH = useCallback(async (fch: string) => {
+    if (!fch) return;
+
+    try {
+      apis.fio.isFioAddressValid(fch);
+
+      const isAvail = await apis.fio.availCheck(fch);
+
+      return isAvail && isAvail.is_registered === 1;
+    } catch (error) {
+      log.info(error);
+      return false;
+    }
+  }, []);
+
+  const resetPath = useCallback(() => {
+    setFch('');
+    window.history.pushState({}, null, '/');
+  }, []);
+
+  useEffectOnce(() => {
+    const handlePathChange = async () => {
+      toggleIsValidating(true);
+      if (!(await validateFCH(pathname))) {
+        resetPath();
+      } else {
+        if (
+          pathnames.length > 2 ||
+          window.location.search ||
+          window.location.hash
+        ) {
+          window.history.pushState({}, null, `/${pathname}`);
+        }
+        setFch(pathname);
+      }
+      toggleIsValidating(false);
+    };
+
+    if (pathname) {
+      handlePathChange();
+    }
+  }, [pathname, validateFCH]);
+
+  return { fioBaseUrl, fch, isDesktop, isValidating, resetPath, setFch };
 };
---
+++ client/src/landing-pages/fio-id-landing-page/FioIdLandingPage/components/PaymentsTabComponent/components/PublicAddressComponent/PublicAddressComponent.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+import Badge, {
+  BADGE_TYPES,
+} from '../../../../../../../components/Badge/Badge';
+import CommonBadge from '../../../../../../../components/Badges/CommonBadge/CommonBadge';
+
+import classes from './PublicAddressComponent.module.scss';
+
+type Props = {
+  chainCodeName: string;
+  iconSrc: string;
+  publicAddress: string;
+  tokenCodeName: string;
+  tokenCode: string;
+};
+
+export const PublicAddressComponent: React.FC<Props> = props => {
+  const { chainCodeName, iconSrc, tokenCodeName, tokenCode } = props;
+
+  return (
+    <Badge show className={classes.container} type={BADGE_TYPES.WHITE}>
+      <div className={classes.cotnentContainer}>
+        <div className={classes.image}>
+          <img src={iconSrc} alt={tokenCodeName} />
+        </div>
+        <p className={classes.tokenCodeName}>
+          {tokenCodeName} <span>({tokenCode})</span>
+        </p>
+        {chainCodeName === tokenCodeName && (
+          <div className={classes.chainCodeBagde}>
+            <CommonBadge classNames={classes.chainCode}>
+              {chainCodeName}
+            </CommonBadge>
+          </div>
+        )}
+      </div>
+      <ArrowForwardIosIcon className={classes.arrow} />
+    </Badge>
+  );
+};
