b0a26e4e87c83922dfa7c292ef56ac06966d3e53
Oleksii Trukhanov <trukhantech@gmail.com>
2023-07-25 01:59:38 +0300

    DASH-723 Refcator WalletsPage and WaletPage logic

---
+++ client/src/pages/WalletPage/WalletPageContext.tsx
@@ -0,0 +1,141 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router';
+
+import { profileRefreshed as profileRefreshedSelector } from '../../redux/profile/selectors';
+import {
+  fioWalletsData as fioWalletsDataSelector,
+  fioWalletsTxHistory as fioWalletsTxHistorySelector,
+} from '../../redux/fioWalletsData/selectors';
+
+import { QUERY_PARAMS_NAMES } from '../../constants/queryParams';
+
+import { useGetAllFioNamesAndWallets } from '../../hooks/fio';
+import useQuery from '../../hooks/useQuery';
+
+import {
+  PAGE_TYPES,
+  PAGE_TYPES_PROPS,
+} from '../DashboardPage/components/WelcomeComponentItem/constants';
+import { DEFAULT_BALANCES } from '../../util/prices';
+
+import {
+  AllFioNamesAndWalletsProps,
+  FioAddressDoublet,
+  FioWalletData,
+  FioWalletDoublet,
+  FioWalletTxHistory,
+  WalletBalances,
+} from '../../types';
+
+type UseContextProps = {
+  error: string;
+  fioCryptoHandles: FioAddressDoublet[];
+  fioWallet: FioWalletDoublet;
+  fioWalletBalance: WalletBalances;
+  fioWalletData: FioWalletData;
+  fioWalletTxHistory: FioWalletTxHistory;
+  hasNoTransactions: boolean;
+  isOpenLockedList: boolean;
+  showPrivateKeyModal: boolean;
+  showWalletNameEdit: boolean;
+  welcomeComponentProps: {
+    noPaddingTop: boolean;
+    onlyActions: boolean;
+    pageType: PAGE_TYPES_PROPS;
+  } & AllFioNamesAndWalletsProps;
+  closeWalletNameEdit: () => void;
+  onKeyShow: () => void;
+  onShowPrivateModalClose: () => void;
+  onWalletUpdated: () => void;
+};
+
+export const useContext = (): UseContextProps => {
+  const fioWalletsData = useSelector(fioWalletsDataSelector);
+  const fioWalletsTxHistory = useSelector(fioWalletsTxHistorySelector);
+  const profileRefreshed = useSelector(profileRefreshedSelector);
+
+  const [showPrivateKeyModal, setShowPrivateKeyModal] = useState(false);
+  const [showWalletNameEdit, setShowWalletNameEdit] = useState(false);
+  const [error, setError] = useState<string>('');
+
+  const queryParams = useQuery();
+  const publicKey = queryParams.get(QUERY_PARAMS_NAMES.PUBLIC_KEY);
+  const location = useLocation<{ isOpenLockedList: boolean }>();
+  const { isOpenLockedList = false } = location?.state || {};
+
+  const allFioNamesAndWallets = useGetAllFioNamesAndWallets();
+  const {
+    fioAddresses,
+    fioWallets,
+    fioWalletsBalances,
+    userId,
+  } = allFioNamesAndWallets;
+
+  const fioCryptoHandles = fioAddresses.filter(
+    fioAddress => fioAddress.walletPublicKey === publicKey,
+  );
+  const fioWallet = fioWallets.find(
+    fioWallet => fioWallet.publicKey === publicKey,
+  );
+  const fioWalletData = userId
+    ? fioWalletsData[userId][fioWallet.publicKey]
+    : null;
+  const fioWalletTxHistory =
+    userId && fioWalletsTxHistory
+      ? fioWalletsTxHistory[userId][fioWallet.publicKey]
+      : null;
+
+  const fioWalletBalance =
+    fioWalletsBalances.wallets[fioWallet.publicKey] || DEFAULT_BALANCES;
+
+  const hasNoTransactions =
+    fioWalletBalance.total.nativeFio === 0 &&
+    fioWalletTxHistory?.txs.length === 0;
+
+  useEffect(() => {
+    if (publicKey && profileRefreshed && !fioWallet)
+      setError(`FIO Wallet (${publicKey}) is not available`);
+  }, [publicKey, fioWallet, profileRefreshed]);
+
+  const onShowPrivateModalClose = useCallback(
+    () => setShowPrivateKeyModal(false),
+    [],
+  );
+  const closeWalletNameEdit = useCallback(
+    () => setShowWalletNameEdit(false),
+    [],
+  );
+
+  const onKeyShow = useCallback(() => setShowPrivateKeyModal(true), []);
+
+  const onWalletUpdated = useCallback(() => {
+    closeWalletNameEdit();
+  }, [closeWalletNameEdit]);
+
+  const welcomeComponentProps = {
+    ...allFioNamesAndWallets,
+    noPaddingTop: true,
+    onlyActions: true,
+    pageType: PAGE_TYPES.TOK,
+  };
+
+  return {
+    error,
+    fioCryptoHandles,
+    fioWallet,
+    fioWalletBalance,
+    fioWalletData,
+    fioWalletTxHistory,
+    hasNoTransactions,
+    isOpenLockedList,
+    showPrivateKeyModal,
+    showWalletNameEdit,
+    welcomeComponentProps,
+    closeWalletNameEdit,
+    onKeyShow,
+    onShowPrivateModalClose,
+    onWalletUpdated,
+  };
+};
--- client/src/pages/WalletsPage/WalletsPage.tsx
+++ client/src/pages/WalletsPage/WalletsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
@@ -20,102 +20,63 @@ import {
   NOTIFICATIONS_CONTENT,
   NOTIFICATIONS_CONTENT_TYPE,
 } from '../../constants/notifications';
-import { Types } from '../DashboardPage/components/WelcomeComponentItem/constants';
 
-import useEffectOnce from '../../hooks/general';
+import { useContext } from './WalletsPageContext';
 
 import { Props } from './types';
 
 import classes from './styles/WalletsPage.module.scss';
 import unwrapIcon from '../../assets/images/unwrap.svg';
 
-interface LocationState {
-  walletDeleted?: boolean;
-}
+type TitleComponentProps = {
+  onAdd: () => void;
+};
 
-const AUTOCLOSE_TIME = 5000;
+const TitleComponent: React.FC<TitleComponentProps> = props => {
+  const { onAdd } = props;
 
-const WalletsPage: React.FC<Props> = props => {
-  const { fioWallets, balance, refreshBalance, location } = props;
+  return (
+    <Title title="FIO Wallets">
+      <ActionButtonsContainer>
+        <Link to={ROUTES.UNWRAP_TOKENS} className={classes.actionButton}>
+          <Button>
+            <img src={unwrapIcon} alt="unwrap" className={classes.unwrapIcon} />
+            <span>Unwrap Tokens</span>
+          </Button>
+        </Link>
 
-  const [showCreateWallet, setShowCreateWallet] = useState<boolean>(false);
-  const [showWalletImported, setShowWalletImported] = useState<boolean>(false);
-  const [showWalletCreated, setShowWalletCreated] = useState<boolean>(false);
-  const [showWalletDeleted, setShowWalletDeleted] = useState<boolean>(false);
+        <Link to={ROUTES.IMPORT_WALLET} className={classes.actionButton}>
+          <Button>
+            <SystemUpdateAltIcon fontSize="small" />
+            <span>Import</span>
+          </Button>
+        </Link>
 
-  useEffectOnce(() => {
-    for (const { publicKey } of fioWallets) {
-      refreshBalance(publicKey);
-    }
-  }, [fioWallets, refreshBalance]);
+        <Button onClick={onAdd} className={classes.actionButton}>
+          <FontAwesomeIcon icon="plus-circle" />
+          <span>Add</span>
+        </Button>
+      </ActionButtonsContainer>
+    </Title>
+  );
+};
 
-  useEffect(() => {
-    if (location.query && location.query.imported) {
-      setShowWalletImported(true);
-    }
-
-    if (location.state && (location.state as LocationState).walletDeleted) {
-      setShowWalletDeleted(true);
-    }
-  }, [location]);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setShowWalletDeleted(false);
-    }, AUTOCLOSE_TIME);
-
-    if (!showWalletDeleted) {
-      clearTimeout(timeoutId);
-    }
-
-    return () => clearTimeout(timeoutId);
-  }, [showWalletDeleted]);
-
-  const closeCreateWallet = () => setShowCreateWallet(false);
-  const closeImportedWallet = () => setShowWalletImported(false);
-  const closeCreatedWallet = () => setShowWalletCreated(false);
-  const closeDeletedWallet = () => setShowWalletDeleted(false);
-
-  const onAdd = () => {
-    setShowCreateWallet(true);
-  };
-  const onWalletCreated = () => {
-    setShowCreateWallet(false);
-    setShowWalletCreated(true);
-  };
-
-  const renderTitle = () => {
-    return (
-      <Title title="FIO Wallets">
-        <ActionButtonsContainer>
-          <Link to={ROUTES.UNWRAP_TOKENS} className={classes.actionButton}>
-            <Button>
-              <img
-                src={unwrapIcon}
-                alt="unwrap"
-                className={classes.unwrapIcon}
-              />
-              <span>Unwrap Tokens</span>
-            </Button>
-          </Link>
-
-          <Link to={ROUTES.IMPORT_WALLET} className={classes.actionButton}>
-            <Button>
-              <SystemUpdateAltIcon fontSize="small" />
-              <span>Import</span>
-            </Button>
-          </Link>
-
-          <a href="#" onClick={onAdd} className={classes.actionButton}>
-            <Button>
-              <FontAwesomeIcon icon="plus-circle" />
-              <span>Add</span>
-            </Button>
-          </a>
-        </ActionButtonsContainer>
-      </Title>
-    );
-  };
+const WalletsPage: React.FC<Props> = () => {
+  const {
+    fioWallets,
+    fioWalletsBalances,
+    showCreateWallet,
+    showWalletCreated,
+    showWalletDeleted,
+    showWalletImported,
+    welcomeComponentProps,
+    closeCreateWallet,
+    closeCreatedWallet,
+    closeImportedWallet,
+    closeDeletedWallet,
+    onAdd,
+    onWalletCreated,
+  } = useContext();
 
   return (
     <div className={classes.container}>
@@ -124,7 +85,7 @@ const WalletsPage: React.FC<Props> = props => {
         onClose={closeCreateWallet}
         onWalletCreated={onWalletCreated}
       />
-      <LayoutContainer title={renderTitle()}>
+      <LayoutContainer title={<TitleComponent onAdd={onAdd} />}>
         <NotificationBadge
           type={BADGE_TYPES.INFO}
           show={showWalletImported}
@@ -185,11 +146,16 @@ const WalletsPage: React.FC<Props> = props => {
             />
           </div>
         )}
-        <TotalBalanceBadge {...balance.total} isNew isMobile itTotalWallets />
-        <WelcomeComponent onlyActions noPaddingTop type={Types.TOK} />
+        <TotalBalanceBadge
+          {...fioWalletsBalances.total}
+          isNew
+          isMobile
+          itTotalWallets
+        />
+        <WelcomeComponent {...welcomeComponentProps} />
       </LayoutContainer>
       <div className={classes.totalBalanceContainer}>
-        <TotalBalanceBadge {...balance.total} isNew itTotalWallets />
+        <TotalBalanceBadge {...fioWalletsBalances.total} isNew itTotalWallets />
       </div>
     </div>
   );
