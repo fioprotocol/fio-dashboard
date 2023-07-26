d3514d7796edf7dcce3a5818a23ace0ada04efe8
Oleksii Trukhanov <trukhantech@gmail.com>
2023-07-26 23:29:39 +0300

    DASH-727 Implement client side of domain watchlist api

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,27 +1,28 @@
 import config from '../config';
 import ApiClient from './client';
 
+import Account from './account';
+import Admin from './admin';
 import Auth from './auth';
+import ChainCode from './chain-code';
+import Contacts from './contacts';
+import DomainsWatchlist from './domains-watchilst';
 import Edge from './edge';
-import Notifications from './notifications';
-import FioReg from './fio-reg';
 import Fio from './fio';
 import FioHistory from './fio-history';
-import Account from './account';
-import RefProfile from './ref-profile';
-import Contacts from './contacts';
-import Admin from './admin';
-import Payments from './payments';
-import Orders from './orders';
-import ChainCode from './chain-code';
-import GeneratePdfFile from './generatePdf';
-import WrapStatus from './wrap-status';
-import Registration from './registration';
-import Vars from './vars';
-import HealthCheck from './health-check';
-import Twitter from './twitter';
-import InfuraNfts from './infura-nfts';
+import FioReg from './fio-reg';
 import General from './general';
+import GeneratePdfFile from './generatePdf';
+import HealthCheck from './health-check';
+import InfuraNfts from './infura-nfts';
+import Notifications from './notifications';
+import Orders from './orders';
+import Payments from './payments';
+import RefProfile from './ref-profile';
+import Registration from './registration';
+import Twitter from './twitter';
+import Vars from './vars';
+import WrapStatus from './wrap-status';
 
 import { log } from '../util/general';
 
@@ -43,52 +44,54 @@ window.fetch = async (uri: RequestInfo | URL, opts: RequestInit = {}) => {
 };
 
 export type Api = {
+  account: Account;
+  admin: Admin;
   auth: Auth;
+  chainCode: ChainCode;
+  client: ApiClient;
+  contacts: Contacts;
+  domainsWatchlist: DomainsWatchlist;
   edge: Edge;
-  notifications: Notifications;
   fioReg: FioReg;
   fio: Fio;
-  account: Account;
-  refProfile: RefProfile;
-  contacts: Contacts;
-  orders: Orders;
-  client: ApiClient;
-  admin: Admin;
-  payments: Payments;
-  chainCode: ChainCode;
-  generatePdfFile: GeneratePdfFile;
-  wrapStatus: WrapStatus;
-  registration: Registration;
-  vars: Vars;
-  healthCheck: HealthCheck;
-  twitter: Twitter;
-  infuraNfts: InfuraNfts;
   general: General;
+  generatePdfFile: GeneratePdfFile;
+  healthCheck: HealthCheck;
+  infuraNfts: InfuraNfts;
+  notifications: Notifications;
+  orders: Orders;
+  payments: Payments;
+  refProfile: RefProfile;
+  registration: Registration;
+  twitter: Twitter;
+  vars: Vars;
+  wrapStatus: WrapStatus;
 };
 
 const apis = {
+  account: new Account(apiClient),
+  admin: new Admin(apiClient),
   auth: new Auth(apiClient),
+  chainCode: new ChainCode(apiClient),
+  client: apiClient,
+  contacts: new Contacts(apiClient),
+  domainsWatchlist: new DomainsWatchlist(apiClient),
   edge: new Edge(),
-  notifications: new Notifications(apiClient),
+  fioHistory: new FioHistory(),
   fioReg: new FioReg(apiClient),
   fio: new Fio(),
-  fioHistory: new FioHistory(),
-  account: new Account(apiClient),
-  refProfile: new RefProfile(apiClient),
-  contacts: new Contacts(apiClient),
-  orders: new Orders(apiClient),
-  client: apiClient,
-  admin: new Admin(apiClient),
-  payments: new Payments(apiClient),
-  chainCode: new ChainCode(apiClient),
-  generatePdfFile: new GeneratePdfFile(apiClient),
-  wrapStatus: new WrapStatus(apiClient),
-  registration: new Registration(apiClient),
-  vars: new Vars(apiClient),
-  healthCheck: new HealthCheck(apiClient),
-  twitter: new Twitter(apiClient),
-  infuraNfts: new InfuraNfts(apiClient),
   general: new General(apiClient),
+  generatePdfFile: new GeneratePdfFile(apiClient),
+  healthCheck: new HealthCheck(apiClient),
+  infuraNfts: new InfuraNfts(apiClient),
+  notifications: new Notifications(apiClient),
+  orders: new Orders(apiClient),
+  payments: new Payments(apiClient),
+  refProfile: new RefProfile(apiClient),
+  registration: new Registration(apiClient),
+  twitter: new Twitter(apiClient),
+  vars: new Vars(apiClient),
+  wrapStatus: new WrapStatus(apiClient),
 };
 
 export default apis;
