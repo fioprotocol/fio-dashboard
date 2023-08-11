a327c18101a9312ac1ecf83fdbe8ef47e5dc2c3e
Oleksii Trukhanov <trukhantech@gmail.com>
2023-08-11 18:00:54 +0300

    DASH-785 Fix low bundles email link

--- client/src/pages/FioAddressAddBundlesPage/FioAddressAddBundlesPage.tsx
+++ client/src/pages/FioAddressAddBundlesPage/FioAddressAddBundlesPage.tsx
@@ -1,224 +1,115 @@
 import React, { useEffect, useState } from 'react';
-import { Redirect, RouteComponentProps } from 'react-router-dom';
 
-import PseudoModalContainer from '../../components/PseudoModalContainer';
-import PriceBadge from '../../components/Badges/PriceBadge/PriceBadge';
-import PayWithBadge from '../../components/Badges/PayWithBadge/PayWithBadge';
-import LowBalanceBadge from '../../components/Badges/LowBalanceBadge/LowBalanceBadge';
-import SubmitButton from '../../components/common/SubmitButton/SubmitButton';
-import AddBundlesEdgeWallet from './components/AddBundlesEdgeWallet';
-import Results from '../../components/common/TransactionResults';
-import FioLoader from '../../components/common/FioLoader/FioLoader';
-import InfoBadge from '../../components/InfoBadge/InfoBadge';
-import FioNamesInitWrapper from '../../components/FioNamesInitWrapper';
-import Badge, { BADGE_TYPES } from '../../components/Badge/Badge';
-import LedgerWalletActionNotSupported from '../../components/LedgerWalletActionNotSupported';
-import PageTitle from '../../components/PageTitle/PageTitle';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 
-import { convertFioPrices } from '../../util/prices';
-import { useFioWallet, useWalletBalances } from '../../util/hooks';
-import MathOp from '../../util/math';
+import { QUERY_PARAMS_NAMES } from '../../constants/queryParams';
+import {
+  ANALYTICS_EVENT_ACTIONS,
+  CART_ITEM_TYPE,
+} from '../../constants/common';
+import { ACTIONS } from '../../constants/fio';
+import { ROUTES } from '../../constants/routes';
+import { FIO_ADDRESS_DELIMITER } from '../../utils';
 
 import {
-  WALLET_CREATED_FROM,
-  DEFAULT_BUNDLE_SET_VALUE,
-  DEFAULT_BUNDLE_AMOUNT,
-} from '../../constants/common';
-import { ERROR_TYPES } from '../../components/common/TransactionResults/constants';
-import { ROUTES } from '../../constants/routes';
-import { LINKS } from '../../constants/labels';
+  fees as feesSelector,
+  feesLoading as feeLoadingSelector,
+} from '../../redux/fio/selectors';
+import { cartItems as cartItemsSelector } from '../../redux/cart/selectors';
 
-import { ResultsData } from '../../components/common/TransactionResults/types';
-import { AddBundlesValues, ContainerProps } from './types';
+import { getFee } from '../../redux/fio/actions';
 
-import classes from '../../components/FioNameRenew/FioNameRenewContainer.module.scss';
+import apis from '../../api';
 
-type MatchParams = {
-  id: string;
-};
-type LocationState = {
-  backUrl?: string;
-};
+import {
+  fireAnalyticsEvent,
+  getCartItemsDataForAnalytics,
+} from '../../util/analytics';
+import useQuery from '../../hooks/useQuery';
+import { addCartItem } from '../../util/cart';
+import FioLoader from '../../components/common/FioLoader/FioLoader';
+import useEffectOnce from '../../hooks/general';
 
-const FioAddressAddBundlesPage: React.FC<ContainerProps &
-  RouteComponentProps<MatchParams, {}, LocationState>> = props => {
-  const { fioAddresses, feePrice, roe, history, getFee, location } = props;
-  const {
-    query: { name },
-    state: { backUrl = ROUTES.FIO_ADDRESSES } = {},
-  } = location;
+const FioAddressAddBundlesPage: React.FC = () => {
+  const cartItems = useSelector(cartItemsSelector);
+  const fees = useSelector(feesSelector);
+  const feeLoading = useSelector(feeLoadingSelector);
 
-  const { currentWallet, settingWallet } = useFioWallet(fioAddresses, name);
-  const { nativeFio: feeNativeFio, fio, usdc } = feePrice;
-  const [processing, setProcessing] = useState(false);
-  const [submitData, setSubmitData] = useState<AddBundlesValues | null>(null);
-  const [resultsData, setResultsData] = useState<ResultsData | null>(null);
-  const [error, setError] = useState<string>('');
+  const [feeLoadingFinished, toggleFeeLoadingFinished] = useState<boolean>(
+    false,
+  );
+
+  const queryParams = useQuery();
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const fch = queryParams.get(QUERY_PARAMS_NAMES.NAME);
+
+  const addBundledTransactions =
+    apis.fio.actionEndPoints.addBundledTransactions;
+
+  const addBundlesFeePrice = fees[addBundledTransactions];
+  const addBundledFeeLoading = feeLoading[addBundledTransactions];
+
+  useEffectOnce(() => {
+    dispatch(getFee(addBundledTransactions));
+  }, [dispatch, addBundledTransactions]);
 
   useEffect(() => {
-    getFee();
-  }, [getFee]);
+    if (addBundledFeeLoading === false) {
+      toggleFeeLoadingFinished(true);
+    }
+  }, [addBundledFeeLoading]);
 
   useEffect(() => {
-    setError(
-      !fioAddresses.find(
-        ({ name: fioCryptoHandleName }) => fioCryptoHandleName === name,
-      )
-        ? `FIO Crypto Handle (${name}) is not available`
-        : '',
+    if (feeLoading[addBundledTransactions]) return;
+
+    if (feeLoadingFinished && !addBundlesFeePrice) {
+      history.push(ROUTES.FIO_DOMAINS);
+      return;
+    }
+
+    if (!addBundlesFeePrice) return;
+
+    const [address, domain] = fch.split(FIO_ADDRESS_DELIMITER);
+
+    const newCartItem = {
+      address,
+      domain,
+      type: CART_ITEM_TYPE.ADD_BUNDLES,
+      id: `${fch}-${ACTIONS.addBundledTransactions}-${+new Date()}`,
+      allowFree: false,
+      costNativeFio: addBundlesFeePrice?.nativeFio,
+      costFio: addBundlesFeePrice.fio,
+      costUsdc: addBundlesFeePrice.usdc,
+    };
+
+    addCartItem(newCartItem);
+    fireAnalyticsEvent(
+      ANALYTICS_EVENT_ACTIONS.ADD_ITEM_TO_CART,
+      getCartItemsDataForAnalytics([newCartItem]),
     );
-  }, [name, fioAddresses]);
-
-  const { available: walletBalancesAvailable } = useWalletBalances(
-    currentWallet.publicKey,
-  );
-
-  const hasLowBalance =
-    feePrice &&
-    new MathOp(walletBalancesAvailable.nativeFio || 0).lt(feeNativeFio || 0);
-
-  const onSubmit = () => {
-    setSubmitData({
-      fioAddress: name,
-      bundleSets: DEFAULT_BUNDLE_SET_VALUE,
-      maxFee: feeNativeFio,
-    });
-  };
-  const onCancel = () => {
-    setSubmitData(null);
-    setProcessing(false);
-  };
-  const onSuccess = async (result: { fee_collected: number }) => {
-    setResultsData({
-      feeCollected: convertFioPrices(result.fee_collected, roe) || feePrice,
-      name,
-    });
-    setSubmitData(null);
-    setProcessing(false);
-  };
-
-  const onResultsClose = () => {
-    history.goBack();
-  };
-
-  const onResultsRetry = () => {
-    setResultsData(null);
-  };
-
-  const renderDetails = () => {
-    return (
-      <Badge type={BADGE_TYPES.WHITE} show={true}>
-        <div className="d-sm-flex justify-content-between align-items-center w-100">
-          <div className={classes.detailsName}>{name}</div>
-          <div className={classes.detailsValue}>
-            {DEFAULT_BUNDLE_AMOUNT} bundled transactions
-          </div>
-        </div>
-      </Badge>
+    fireAnalyticsEvent(
+      ANALYTICS_EVENT_ACTIONS.BEGIN_CHECKOUT,
+      getCartItemsDataForAnalytics([...cartItems, newCartItem]),
     );
-  };
-
-  if (resultsData)
-    return (
-      <>
-        <PageTitle
-          link={LINKS.FIO_ADDRESS_ADD_BUNDLES_CONFIRMATION}
-          isVirtualPage
-        />
-        <Results
-          results={resultsData}
-          title={
-            resultsData.error
-              ? 'Adding Bundled Transactions Failed!'
-              : 'Bundled Transactions Added!'
-          }
-          onClose={onResultsClose}
-          onRetry={onResultsRetry}
-          errorType={ERROR_TYPES.ADD_BUNDLES_ERROR}
-        >
-          <h5 className={classes.label}>Add Bundled Transactions Details</h5>
-          {renderDetails()}
-        </Results>
-      </>
-    );
-
-  if (error)
-    return (
-      <PseudoModalContainer title="Add bundled transactions" link={backUrl}>
-        <InfoBadge
-          message={error}
-          show={!!error}
-          title="Error"
-          type={BADGE_TYPES.ERROR}
-        />
-      </PseudoModalContainer>
-    );
-
-  if (!currentWallet || currentWallet.balance === null)
-    return <FioLoader wrap={true} />;
-
-  if (
-    (!currentWallet.publicKey && !settingWallet && !processing) ||
-    fioAddresses.length < 1
-  )
-    return <Redirect to={{ pathname: ROUTES.FIO_ADDRESSES }} />;
+    history.push(ROUTES.CART);
+  }, [
+    addBundledTransactions,
+    addBundlesFeePrice,
+    cartItems,
+    fch,
+    feeLoading,
+    feeLoadingFinished,
+    history,
+  ]);
 
   return (
-    <>
-      {currentWallet.from === WALLET_CREATED_FROM.EDGE ? (
-        <AddBundlesEdgeWallet
-          fioWallet={currentWallet}
-          onSuccess={onSuccess}
-          onCancel={onCancel}
-          setProcessing={setProcessing}
-          sendData={submitData}
-          processing={processing}
-        />
-      ) : null}
-
-      {currentWallet.from === WALLET_CREATED_FROM.LEDGER ? (
-        <LedgerWalletActionNotSupported
-          submitData={submitData}
-          onCancel={onCancel}
-        />
-      ) : null}
-
-      <PseudoModalContainer title="Add bundled transactions" link={backUrl}>
-        <div className={classes.container}>
-          <h5 className={classes.label}>Add Bundled Transactions Details</h5>
-          {renderDetails()}
-          <h5 className={classes.label}>Payment Details</h5>
-          <PriceBadge
-            costFio={fio}
-            costUsdc={usdc}
-            title="Total Cost"
-            type={BADGE_TYPES.BLACK}
-          />
-          <PayWithBadge
-            walletBalances={walletBalancesAvailable}
-            walletName={currentWallet.name}
-          />
-          <LowBalanceBadge hasLowBalance={hasLowBalance} />
-          <SubmitButton
-            onClick={onSubmit}
-            text="Add Bundled Transactions"
-            disabled={hasLowBalance || processing}
-            loading={processing || settingWallet}
-            withTopMargin={true}
-          />
-        </div>
-      </PseudoModalContainer>
-    </>
+    <div className="mx-auto my-auto">
+      <FioLoader wrap />
+    </div>
   );
 };
 
-const FioAddressAddBundlesPageWrapper: React.FC<ContainerProps &
-  RouteComponentProps<MatchParams, {}, LocationState>> = props => {
-  return (
-    <FioNamesInitWrapper>
-      <FioAddressAddBundlesPage {...props} />
-    </FioNamesInitWrapper>
-  );
-};
-
-export default FioAddressAddBundlesPageWrapper;
+export default FioAddressAddBundlesPage;
