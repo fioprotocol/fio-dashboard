6aa86a7536449c50260b392f590e8b331048d80a
Oleksii Trukhanov <trukhantech@gmail.com>
2023-08-11 16:55:51 +0300

    DASH-785 Handle renew domain link from email. Remove unused renew domain components.

--- client/src/pages/FioDomainRenewPage/FioDomainRenewPage.tsx
+++ client/src/pages/FioDomainRenewPage/FioDomainRenewPage.tsx
@@ -1,47 +1,110 @@
-import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 
-import FioNamesInitWrapper from '../../components/FioNamesInitWrapper';
-import FioNameRenewContainer from '../../components/FioNameRenew';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 
-import { DOMAIN } from '../../constants/common';
-import { DEFAULT_FEE_PRICES } from '../../util/prices';
+import { QUERY_PARAMS_NAMES } from '../../constants/queryParams';
+import {
+  ANALYTICS_EVENT_ACTIONS,
+  CART_ITEM_TYPE,
+} from '../../constants/common';
+import { ACTIONS, DOMAIN_TYPE } from '../../constants/fio';
+import { ROUTES } from '../../constants/routes';
+
+import {
+  fees as feesSelector,
+  feesLoading as feeLoadingSelector,
+} from '../../redux/fio/selectors';
+import { cartItems as cartItemsSelector } from '../../redux/cart/selectors';
+
+import { getFee } from '../../redux/fio/actions';
 
 import apis from '../../api';
 
-import { FeePrice, FioNameItemProps } from '../../types';
+import {
+  fireAnalyticsEvent,
+  getCartItemsDataForAnalytics,
+} from '../../util/analytics';
+import useQuery from '../../hooks/useQuery';
+import { addCartItem } from '../../util/cart';
+import FioLoader from '../../components/common/FioLoader/FioLoader';
+import useEffectOnce from '../../hooks/general';
 
-type LocationProps = {
-  location: {
-    query: {
-      name: string;
+const FioDomainRenewPage: React.FC = () => {
+  const cartItems = useSelector(cartItemsSelector);
+  const fees = useSelector(feesSelector);
+  const feeLoading = useSelector(feeLoadingSelector);
+
+  const [feeLoadingFinished, toggleFeeLoadingFinished] = useState<boolean>(
+    false,
+  );
+
+  const queryParams = useQuery();
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const domain = queryParams.get(QUERY_PARAMS_NAMES.NAME);
+
+  const renewFioDomain = apis.fio.actionEndPoints.renewFioDomain;
+
+  const renewDomainFeePrice = fees[renewFioDomain];
+  const domainRenewFeeLoading = feeLoading[renewFioDomain];
+
+  useEffectOnce(() => {
+    dispatch(getFee(renewFioDomain));
+  }, [dispatch, renewFioDomain]);
+
+  useEffect(() => {
+    if (domainRenewFeeLoading === false) {
+      toggleFeeLoadingFinished(true);
+    }
+  }, [domainRenewFeeLoading]);
+
+  useEffect(() => {
+    if (feeLoading[renewFioDomain]) return;
+
+    if (feeLoadingFinished && !renewDomainFeePrice) {
+      history.push(ROUTES.FIO_DOMAINS);
+      return;
+    }
+
+    if (!renewDomainFeePrice) return;
+
+    const newCartItem = {
+      domain,
+      type: CART_ITEM_TYPE.DOMAIN_RENEWAL,
+      id: `${domain}-${ACTIONS.renewFioDomain}-${+new Date()}`,
+      allowFree: false,
+      period: 1,
+      costNativeFio: renewDomainFeePrice?.nativeFio,
+      costFio: renewDomainFeePrice.fio,
+      costUsdc: renewDomainFeePrice.usdc,
+      domainType: DOMAIN_TYPE.PRIVATE,
     };
-  };
-};
-
-type Props = {
-  fioNameList: FioNameItemProps[];
-  fees: { [endpoint: string]: FeePrice };
-};
-
-const FioDomainRenewPage: React.FC<Props &
-  RouteComponentProps &
-  LocationProps> = props => {
-  const { fioNameList, location, history, fees } = props;
-  const { name } = location.query;
+    addCartItem(newCartItem);
+    fireAnalyticsEvent(
+      ANALYTICS_EVENT_ACTIONS.ADD_ITEM_TO_CART,
+      getCartItemsDataForAnalytics([newCartItem]),
+    );
+    fireAnalyticsEvent(
+      ANALYTICS_EVENT_ACTIONS.BEGIN_CHECKOUT,
+      getCartItemsDataForAnalytics([...cartItems, newCartItem]),
+    );
+    history.push(ROUTES.CART);
+  }, [
+    cartItems,
+    domain,
+    feeLoading,
+    feeLoadingFinished,
+    history,
+    renewDomainFeePrice,
+    renewFioDomain,
+  ]);
 
   return (
-    <FioNamesInitWrapper>
-      <FioNameRenewContainer
-        fioNameType={DOMAIN}
-        fioNameList={fioNameList}
-        name={name}
-        feePrice={
-          fees[apis.fio.actionEndPoints.renewFioDomain] || DEFAULT_FEE_PRICES
-        }
-        history={history}
-      />
-    </FioNamesInitWrapper>
+    <div className="mx-auto my-auto">
+      <FioLoader wrap />
+    </div>
   );
 };
 
