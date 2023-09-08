fe5dd0eefea113a063570415ae9bdfb08e1ee956
Oleksii Trukhanov <trukhantech@gmail.com>
2023-09-08 14:33:49 +0300

    DASH-564 Remove AuthContainer

--- client/src/pages/CreateAccountPage/CreateAccountPage.tsx
+++ client/src/pages/CreateAccountPage/CreateAccountPage.tsx
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 
 import CreateAccountForm from '../../components/CreateAccountForm';
 
+import classes from './CreateAccountPage.module.scss';
+
 export default class CreateAccountPage extends Component {
   render(): React.ReactElement {
-    return <CreateAccountForm />;
+    return (
+      <div className={classes.container}>
+        <CreateAccountForm />
+      </div>
+    );
   }
 }
