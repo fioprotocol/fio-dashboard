1984968411fc97933f265ed3be602fdf0047b1b3
Oleksii Trukhanov <trukhantech@gmail.com>
2023-09-15 14:01:30 +0300

    DASH-816 Update meta tags.

--- client/src/constants/labels.ts
+++ client/src/constants/labels.ts
@@ -133,7 +133,7 @@ export const LINK_LABELS: { [linkKey: string]: string } = {
   [LINKS.UNAVAILABLE]: 'Unavailable',
 };
 
-export const APP_TITLE = 'FIO Dashboard';
+export const APP_TITLE = 'FIO Protocol Dashboard';
 export const LINK_TITLES: { [linkKey: string]: string } = {
   [LINKS.HOME]: 'Home',
   [LINKS.DASHBOARD]: 'My FIO',
