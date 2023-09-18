ff3ce0fe36acc1e5be960b232eac3d5e41cbbfc7
Oleksii Trukhanov <trukhantech@gmail.com>
2023-09-18 18:54:17 +0300

    DASH-813 Move Moralis api to external files

--- server/src/services/external/GetFioNfts.mjs
+++ server/src/services/external/GetFioNfts.mjs
@@ -1,14 +1,9 @@
-import Moralis from 'moralis';
-import { EvmChain } from '@moralisweb3/common-evm-utils';
+import moralisNftsApi from '../../external/moralis-nft.mjs';
 
 import logger from '../../logger';
 import config from '../../config/index.mjs';
 import Base from '../Base';
 import X from '../Exception';
-
-const CHUNK_SIZE = 2;
-const DELAY_BETWEEN_CHUNKS = 500;
-const FIO_NFT_POLYGON_CONTRACT = process.env.FIO_NFT_POLYGON_CONTRACT || '';
 
 export default class GetFioNfts extends Base {
   static get validationRules() {
@@ -19,142 +14,11 @@ export default class GetFioNfts extends Base {
   }
 
   async execute({ address, chainName = config.nfts.defaultChainName }) {
-    if (!Moralis.Core._isStarted)
-      await Moralis.start({
-        apiKey: config.nfts.apiKey,
-      });
-
-    const chain = EvmChain[chainName];
-
-    const nftsList = [];
-
     try {
-      const getWalletNfts = async cursor =>
-        await Moralis.EvmApi.nft.getWalletNFTs({
-          address,
-          chain,
-          cursor,
-          format: 'decimal',
-          mediaItems: false,
-          normalizeMetadata: true,
-          tokenAddresses: [FIO_NFT_POLYGON_CONTRACT],
-        });
-
-      const getAllWalletNfts = async ({ cursor }) => {
-        let walletNftsRes = {};
-        if (cursor) {
-          walletNftsRes = await cursor.next();
-        } else {
-          walletNftsRes = (await getWalletNfts()) || {};
-        }
-
-        const walletNftsResData = walletNftsRes.toJSON();
-
-        if (walletNftsResData && walletNftsResData.result) {
-          nftsList.push(...walletNftsResData.result);
-
-          if (walletNftsRes.hasNext()) {
-            await getAllWalletNfts({ cursor: walletNftsRes });
-          }
-        }
-      };
-
-      await getAllWalletNfts({});
-
-      if (nftsList.some(nftItem => nftItem.metadata == null)) {
-        const nftItemsWithNoMetadata = nftsList.filter(
-          nftItem => nftItem.metadata == null,
-        );
-
-        const nftItemsWithSyncedMetadata = [];
-
-        const resyncedNftMetadata = async nftItem => {
-          if (!nftItem || !nftItem.token_id) return;
-
-          try {
-            await Moralis.EvmApi.nft.reSyncMetadata({
-              chain,
-              flag: 'uri',
-              mode: 'async',
-              address: FIO_NFT_POLYGON_CONTRACT,
-              tokenId: nftItem.token_id,
-            });
-          } catch (error) {
-            logger.error(
-              `Resync uri for token id - ${nftItem.token_id}: `,
-              error.message,
-            );
-          }
-
-          try {
-            await Moralis.EvmApi.nft.reSyncMetadata({
-              chain,
-              flag: 'metadata',
-              mode: 'async',
-              address: FIO_NFT_POLYGON_CONTRACT,
-              tokenId: nftItem.token_id,
-            });
-          } catch (error) {
-            logger.error(
-              `Resync metadata for token id - ${nftItem.token_id}: `,
-              error.message,
-            );
-          }
-
-          try {
-            const nftItemWithFreshMetadataRes = await Moralis.EvmApi.nft.getNFTMetadata({
-              chain,
-              address: FIO_NFT_POLYGON_CONTRACT,
-              tokenId: nftItem.token_id,
-              normalizeMetadata: true,
-              format: 'decimal',
-              mediaItems: false,
-            });
-            return nftItemWithFreshMetadataRes.toJSON();
-          } catch (error) {
-            logger.error(
-              `Get metadata for token id - ${nftItem.token_id}: `,
-              error.message,
-            );
-          }
-        };
-
-        const processChunk = async chunk => {
-          const nftMetadataPromises = chunk.map(nftItem => resyncedNftMetadata(nftItem));
-
-          const chunkResults = await Promise.allSettled(nftMetadataPromises);
-
-          const resolvedChunkResults = chunkResults
-            .filter(result => result.status === 'fulfilled')
-            .map(result => result.value);
-
-          nftItemsWithSyncedMetadata.push(...resolvedChunkResults);
-        };
-
-        for (let i = 0; i < nftItemsWithNoMetadata.length; i += CHUNK_SIZE) {
-          const chunk = nftItemsWithNoMetadata.slice(i, i + CHUNK_SIZE);
-
-          await processChunk(chunk);
-
-          if (i + CHUNK_SIZE < nftItemsWithNoMetadata.length) {
-            await new Promise(resolve => setTimeout(resolve, DELAY_BETWEEN_CHUNKS));
-          }
-        }
-
-        if (nftItemsWithSyncedMetadata.length) {
-          for (const nftItemWithSyncedMetadata of nftItemsWithSyncedMetadata) {
-            const nonUpdatedMetadataNftItem = nftsList.find(
-              nftItem => nftItem.token_id === nftItemWithSyncedMetadata.token_id,
-            );
-
-            if (nonUpdatedMetadataNftItem) {
-              nonUpdatedMetadataNftItem.metadata = nftItemWithSyncedMetadata.metadata;
-              nonUpdatedMetadataNftItem.normalized_metadata =
-                nftItemWithSyncedMetadata.normalized_metadata;
-            }
-          }
-        }
-      }
+      const nftsList = await moralisNftsApi.getAllWalletNfts({
+        address,
+        chainName,
+      });
 
       return { data: nftsList };
     } catch (error) {
