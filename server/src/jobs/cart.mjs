c507a0626609187c9649657661b1c9b24482d2ac
Oleksii Trukhanov <trukhantech@gmail.com>
2023-11-13 12:08:42 +0200

    DASH-857 Add clear cart period via env var.

--- server/src/jobs/cart.mjs
+++ server/src/jobs/cart.mjs
@@ -5,16 +5,19 @@ import { Cart } from '../models/index.mjs';
 
 import CommonJob from './job.mjs';
 
+const CLEAR_CART_TIME_PERIOD_IN_MINUTES =
+  process.env.CLEAR_CART_TIME_PERIOD_IN_MINUTES || 1440;
+
 class CartJob extends CommonJob {
   async execute() {
     const currentDate = new Date();
-    const twentyFourHoursFromNow = new Date(currentDate);
-    twentyFourHoursFromNow.setHours(currentDate.getHours() - 24);
+    const timeFromNow = new Date(currentDate);
+    timeFromNow.setMinutes(currentDate.getMinutes() - CLEAR_CART_TIME_PERIOD_IN_MINUTES);
 
-    this.postMessage(`Processing clear cart older then - ${twentyFourHoursFromNow}`);
+    this.postMessage(`Processing clear cart older then - ${timeFromNow}`);
 
     const rowsDeleted = await Cart.destroy({
-      where: { createdAt: { [Sequelize.Op.lt]: twentyFourHoursFromNow } },
+      where: { createdAt: { [Sequelize.Op.lt]: timeFromNow } },
     });
 
     this.postMessage(`Cleared ${rowsDeleted} carts`);
