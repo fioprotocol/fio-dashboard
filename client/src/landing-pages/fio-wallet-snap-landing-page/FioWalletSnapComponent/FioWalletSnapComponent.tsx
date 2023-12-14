e635eae20922cea4a1dbadb68e8a24d407dcb5c0
Oleksii Trukhanov <trukhantech@gmail.com>
2023-12-14 21:37:01 +0200

    DASH-853 Init fio wallet snap page.

---
+++ client/src/landing-pages/fio-wallet-snap-landing-page/FioWalletSnapComponent/FioWalletSnapComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+export const FioWalletSnapComponent: React.FC<Props> = props => {
+  return (
+    <div>
+      <h3>FIO Wallet Snap test page</h3>
+    </div>
+  );
+};
