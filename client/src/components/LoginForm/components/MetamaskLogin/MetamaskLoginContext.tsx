0b60cbc72416e1a470357f707ca8a7ab3cf295c4
Oleksii Trukhanov <trukhantech@gmail.com>
2024-01-16 17:01:04 +0200

    DASH-854 Handle Metamask login error.

--- client/src/components/LoginForm/components/MetamaskLogin/MetamaskLoginContext.tsx
+++ client/src/components/LoginForm/components/MetamaskLogin/MetamaskLoginContext.tsx
@@ -1,11 +1,22 @@
 import { useCallback, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { MetamaskSnap } from '../../../../services/MetamaskSnap';
 import apis from '../../../../api';
 import { signNonce } from '../../../../util/snap';
-import { alternateLogin } from '../../../../redux/profile/actions';
+import {
+  alternateLogin,
+  resetAlternativeLoginError,
+  setAlternativeLoginError,
+} from '../../../../redux/profile/actions';
+
+import { alternativeLoginError as alternativeLoginErrorSelector } from '../../../../redux/profile/selectors';
+
 import { WALLET_TYPES } from '../../../../constants/wallets';
+import { log } from '../../../../util/general';
+
+const DEFAULT_METAMASK_ERROR =
+  'Sign in with MetaMask has failed. Please try again.';
 
 type UseContextProps = {
   isModalOpen: boolean;
@@ -15,7 +26,13 @@ type UseContextProps = {
 };
 
 export const useContext = (): UseContextProps => {
-  const { derivationIndex, publicKey, handleConnectClick } = MetamaskSnap();
+  const {
+    derivationIndex,
+    publicKey,
+    snapError,
+    handleConnectClick,
+  } = MetamaskSnap();
+  const alternativeLoginError = useSelector(alternativeLoginErrorSelector);
 
   const [isModalOpen, toggleIsModalOpen] = useState(false);
 
@@ -36,31 +53,44 @@ export const useContext = (): UseContextProps => {
   const connectMetamask = useCallback(() => {
     if (window.ethereum) {
       handleConnectClick();
+      dispatch(resetAlternativeLoginError());
     } else {
       onDetailsClick();
     }
-  }, [handleConnectClick, onDetailsClick]);
+  }, [dispatch, handleConnectClick, onDetailsClick]);
 
   const metamaskLogin = useCallback(async () => {
-    const { nonce } = await apis.auth.generateNonce();
-    const signature = await signNonce({ nonce, derivationIndex });
-    dispatch(
-      alternateLogin({
-        derivationIndex,
-        from: WALLET_TYPES.METAMASK,
-        nonce,
-        publicKey,
-        signature,
-        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-      }),
-    );
+    try {
+      const { nonce } = await apis.auth.generateNonce();
+      const signature = await signNonce({ nonce, derivationIndex });
+
+      dispatch(
+        alternateLogin({
+          derivationIndex,
+          from: WALLET_TYPES.METAMASK,
+          nonce,
+          publicKey,
+          signature,
+          timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        }),
+      );
+    } catch (error) {
+      log.error('Metamask Login error', error);
+      dispatch(setAlternativeLoginError(DEFAULT_METAMASK_ERROR));
+    }
   }, [derivationIndex, dispatch, publicKey]);
 
   useEffect(() => {
-    if (publicKey) {
+    if (publicKey && !alternativeLoginError) {
       metamaskLogin();
     }
-  }, [metamaskLogin, publicKey]);
+  }, [metamaskLogin, publicKey, alternativeLoginError]);
+
+  useEffect(() => {
+    if (snapError) {
+      dispatch(setAlternativeLoginError(DEFAULT_METAMASK_ERROR));
+    }
+  }, [dispatch, snapError]);
 
   return {
     isModalOpen,
