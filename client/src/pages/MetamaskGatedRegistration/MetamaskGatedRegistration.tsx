0f84c6e3869cd50595d8f062f888a0db428c9c6f
Oleksii Trukhanov <trukhantech@gmail.com>
2024-02-06 13:23:56 +0200

    DASH-929 Add helmet title to MetaMask gated registration page.

--- client/src/pages/MetamaskGatedRegistration/MetamaskGatedRegistration.tsx
+++ client/src/pages/MetamaskGatedRegistration/MetamaskGatedRegistration.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 import { FCHBanner } from '../../components/FCHBanner';
 import { FCHSpecialsBanner } from '../../components/SpecialsBanner';
 import { WidelyAdoptedSection } from '../../components/WidelyAdoptedSection';
 import AddressWidget from '../../components/AddressWidget';
+
+import { APP_TITLE } from '../../constants/labels';
 
 import { useContext } from './MetamaskGatedRegistrationContext';
 
@@ -16,22 +19,27 @@ const MetamaskGatedRegistration: React.FC = () => {
   const { addressWidgetContent } = useContext();
 
   return (
-    <div className={classes.container}>
-      <AddressWidget isDarkWhite {...addressWidgetContent} />
-      <FCHBanner fch="bob@metamask" />
-      <FCHSpecialsBanner
-        customNeverExpiresIcon={neverExpiresIcon}
-        customNeverExpiresMobileIcon={neverExpiresIcon}
-        customSendReceiveIcon={sendReceiveIcon}
-      />
-      <WidelyAdoptedSection />
-      <AddressWidget
-        isDarkWhite
-        {...{ ...addressWidgetContent, logoSrc: '' }}
-        isReverseColors
-        isTransparent
-      />
-    </div>
+    <>
+      <Helmet>
+        <title>{APP_TITLE} - Get Your Free @metamask Handle</title>
+      </Helmet>
+      <div className={classes.container}>
+        <AddressWidget isDarkWhite {...addressWidgetContent} />
+        <FCHBanner fch="bob@metamask" />
+        <FCHSpecialsBanner
+          customNeverExpiresIcon={neverExpiresIcon}
+          customNeverExpiresMobileIcon={neverExpiresIcon}
+          customSendReceiveIcon={sendReceiveIcon}
+        />
+        <WidelyAdoptedSection />
+        <AddressWidget
+          isDarkWhite
+          {...{ ...addressWidgetContent, logoSrc: '' }}
+          isReverseColors
+          isTransparent
+        />
+      </div>
+    </>
   );
 };
 
