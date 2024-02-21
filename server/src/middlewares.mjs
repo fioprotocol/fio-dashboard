0bc695533f2b9d01adfaef3d94f8cb4045fe375b
Oleksii Trukhanov <trukhantech@gmail.com>
2024-02-21 17:55:15 +0200

    Debug server issue.

--- server/src/middlewares.mjs
+++ server/src/middlewares.mjs
@@ -3,14 +3,18 @@ import bodyParser from 'body-parser';
 import multipart from 'connect-multiparty';
 import cookieParser from 'cookie-parser';
 
+import logger from './logger.mjs';
+
 export default {
   json: bodyParser.json({
     limit: 1024 * 1024,
     verify: (req, res, buf) => {
+      logger.info('buf', buf);
       try {
         JSON.parse(buf);
         req.rawBody = buf.toString('utf8');
       } catch (e) {
+        logger.error(e);
         res.send({
           status: 0,
           error: {
