7d8dabe9c03ee58938b3ec5139eb0e098f0902d8
Oleksii Trukhanov <trukhantech@gmail.com>
2024-03-05 15:55:08 +0200

    Change add-userProfileType-to-user-table migration. Use STRING type instead of enum.

--- server/src/migrations/20240112173612-add-userProfileType-to-user-table.js
+++ server/src/migrations/20240112173612-add-userProfileType-to-user-table.js
@@ -5,14 +5,13 @@ const { USER_PROFILE_TYPE } = require('../config/constants');
 module.exports = {
   up: async (QI, DT) => {
     await QI.addColumn('users', 'userProfileType', {
-      type: DT.ENUM,
+      type: DT.STRING,
+      allowNull: false,
       defaultValue: USER_PROFILE_TYPE.PRIMARY,
-      values: Object.values(USER_PROFILE_TYPE),
     });
   },
 
   down: async QI => {
     await QI.removeColumn('users', 'userProfileType');
-    await QI.sequelize.query('DROP TYPE IF EXISTS "enum_users_userProfileType";');
   },
 };
