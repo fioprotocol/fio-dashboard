4703e3676a3d6891082375e94e5d3be01c0a925a
Oleksii Trukhanov <trukhantech@gmail.com>
2024-03-28 19:46:07 +0200

    DASH-883 Comment handleExpiredDomains on domains list to remove it into own cron job because of long response.

--- server/src/services/registration/DomainsList.mjs
+++ server/src/services/registration/DomainsList.mjs
@@ -1,7 +1,7 @@
 import Base from '../Base';
 import { Domain, ReferrerProfile, Username } from '../../models';
 
-import { handleExpiredDomains } from '../../utils/fio.mjs';
+// import { handleExpiredDomains } from '../../utils/fio.mjs';
 
 const REF_COOKIE_NAME = process.env.REACT_APP_REFERRAL_PROFILE_COOKIE_NAME || 'ref';
 
@@ -22,13 +22,19 @@ export default class DomainsList extends Base {
     });
     const allRefProfileDomains = await ReferrerProfile.getRefDomainsList();
 
-    const handledExpiredDashboardDomains = await handleExpiredDomains({
-      domainsList: dashboardDomains,
-    });
+    // todo: move to cron job expiration domain handling
 
-    const handledExpiredAllRefProfileDomains = await handleExpiredDomains({
-      domainsList: allRefProfileDomains,
-    });
+    // const handledExpiredDashboardDomains = await handleExpiredDomains({
+    //   domainsList: dashboardDomains,
+    // });
+
+    // const handledExpiredAllRefProfileDomains = await handleExpiredDomains({
+    //   domainsList: allRefProfileDomains,
+    // });
+
+    const handledExpiredDashboardDomains = dashboardDomains;
+
+    const handledExpiredAllRefProfileDomains = allRefProfileDomains;
 
     const allRefProfileDomainsHandledGatedDomains = handledExpiredAllRefProfileDomains.filter(
       refProfileDomain => {
