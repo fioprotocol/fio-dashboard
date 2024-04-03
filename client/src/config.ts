2884f9cdef1f654111ab13fb123af091d67105ea
Kyryl Petkov <kirill.petkov@ditto.trade>
2024-04-03 17:38:41 +0400

    DASH-904 add support link to footer

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,6 +3,7 @@ const config = {
   apiBaseUrl: process.env.REACT_APP_API_BASE_URL,
   baseUrl: process.env.REACT_APP_BASE_URL,
   wsBaseUrl: process.env.REACT_APP_WS_BASE_URL,
+  supportUrl: process.env.REACT_APP_FIOPROTOCOL_SUPPORT_URL,
   userTokenName: process.env.REACT_APP_USER_TOKEN_NAME,
   adminTokenName: process.env.REACT_APP_ADMIN_TOKEN_NAME,
 };
