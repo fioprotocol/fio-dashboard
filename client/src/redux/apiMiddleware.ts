12e4a9347594621983267658e79df5079227432d
Oleksii Trukhanov <trukhantech@gmail.com>
2024-04-09 16:02:31 +0300

    DASH-967 Do not call request to server if page is not visible. Handle this on redux as well. Refactor useMaintenance.

--- client/src/redux/apiMiddleware.ts
+++ client/src/redux/apiMiddleware.ts
@@ -1,8 +1,12 @@
 import { Middleware } from 'redux';
 
+import { HIDDEN_PAGE_SKIP_MESSAGE } from '../constants/errors';
+
 import { Api } from '../api';
 import { ReduxState } from './init';
 import { AnyType } from '../types';
+import { log } from '../util/general';
+import { getIsPageVisible } from '../util/screen';
 
 export default function apiMiddleware(api: Api): Middleware {
   return ({ dispatch, getState }) => next => action => {
@@ -18,12 +22,25 @@ export default function apiMiddleware(api: Api): Middleware {
 
     if (!promise) return next(action);
 
+    const isPageVisible = getIsPageVisible();
+
+    if (!isPageVisible) {
+      log.info(HIDDEN_PAGE_SKIP_MESSAGE);
+      return; // Skip the request silently
+    }
+
     const [REQUEST, SUCCESS, FAILURE] = types;
 
     next({ ...rest, type: REQUEST });
 
     return promise(api, getState).then(
-      (data: AnyType) => next({ ...rest, data, type: SUCCESS }),
+      (data: AnyType) => {
+        if (isPageVisible) {
+          next({ ...rest, data, type: SUCCESS });
+        } else {
+          log.info('Page became hidden before request completed.');
+        }
+      },
       (error: AnyType) => next({ ...rest, error, type: FAILURE }),
     );
   };
