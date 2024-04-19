21f7683e0c7cfac14d2cd494e5e9750378670514
Kyrylo Petkov <sardonyx.wt@gmail.com>
2024-04-19 12:45:23 +0400

    DASH-928 Improve dashboard loading (#1613)
    * DASH-928 Improve dashboard loading
    
    * DASH-928 Cleanup Fio101Component props
    
    ---------
    
    Co-authored-by: Kyryl Petkov <kirill.petkov@ditto.trade>

--- client/src/components/Fio101Component/Fio101ComponentContext.tsx
+++ client/src/components/Fio101Component/Fio101ComponentContext.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { FIO_101_SLIDER_CONTENT, Fio101SliderContentProps } from './constants';
 import { QUERY_PARAMS_NAMES } from '../../constants/queryParams';
@@ -12,7 +12,6 @@ export const useContext = (props: {
   hasFCH: boolean;
   hasOneFCH: boolean;
   hasDomains: boolean;
-  loading: boolean;
   noMappedPubAddresses: boolean;
 }): UseContextProps => {
   const {
@@ -20,71 +19,79 @@ export const useContext = (props: {
     hasFCH,
     hasOneFCH,
     hasDomains,
-    loading,
     noMappedPubAddresses,
   } = props;
-  const [fio101Items, setFio101Items] = useState<Fio101SliderContentProps[]>(
-    [],
-  );
 
-  useEffect(() => {
-    if (!loading) {
-      let fio101ItemsArr = [
+  const prepareSlides = useCallback(() => {
+    let fio101ItemsArr = [
+      FIO_101_SLIDER_CONTENT.DEFAULT,
+      FIO_101_SLIDER_CONTENT.NO_FCH,
+      FIO_101_SLIDER_CONTENT.NO_MAPPED_PUBLIC_ADDRESSES,
+      FIO_101_SLIDER_CONTENT.NO_DOMAINS,
+    ];
+
+    if (hasFCH && !hasDomains) {
+      fio101ItemsArr = [
+        FIO_101_SLIDER_CONTENT.NO_DOMAINS,
         FIO_101_SLIDER_CONTENT.DEFAULT,
         FIO_101_SLIDER_CONTENT.NO_FCH,
         FIO_101_SLIDER_CONTENT.NO_MAPPED_PUBLIC_ADDRESSES,
-        FIO_101_SLIDER_CONTENT.NO_DOMAINS,
       ];
-
-      if (hasFCH && !hasDomains) {
-        fio101ItemsArr = [
-          FIO_101_SLIDER_CONTENT.NO_DOMAINS,
-          FIO_101_SLIDER_CONTENT.DEFAULT,
-          FIO_101_SLIDER_CONTENT.NO_FCH,
-          FIO_101_SLIDER_CONTENT.NO_MAPPED_PUBLIC_ADDRESSES,
-        ];
-      }
-
-      if (hasFCH && noMappedPubAddresses) {
-        if (hasOneFCH) {
-          fio101ItemsArr = [
-            {
-              ...FIO_101_SLIDER_CONTENT.NO_MAPPED_PUBLIC_ADDRESSES,
-              link:
-                FIO_101_SLIDER_CONTENT.NO_MAPPED_PUBLIC_ADDRESSES.oneItemLink +
-                `?${QUERY_PARAMS_NAMES.FIO_HANDLE}=${firstFromListFioAddressName}`,
-            },
-            FIO_101_SLIDER_CONTENT.NO_DOMAINS,
-            FIO_101_SLIDER_CONTENT.DEFAULT,
-            FIO_101_SLIDER_CONTENT.NO_FCH,
-          ];
-        } else {
-          fio101ItemsArr = [
-            FIO_101_SLIDER_CONTENT.NO_MAPPED_PUBLIC_ADDRESSES,
-            FIO_101_SLIDER_CONTENT.NO_DOMAINS,
-            FIO_101_SLIDER_CONTENT.DEFAULT,
-            FIO_101_SLIDER_CONTENT.NO_FCH,
-          ];
-        }
-      }
-
-      if (!hasFCH) {
-        fio101ItemsArr = [
-          FIO_101_SLIDER_CONTENT.NO_FCH,
-          FIO_101_SLIDER_CONTENT.NO_MAPPED_PUBLIC_ADDRESSES,
-          FIO_101_SLIDER_CONTENT.NO_DOMAINS,
-          FIO_101_SLIDER_CONTENT.DEFAULT,
-        ];
-      }
-
-      setFio101Items(fio101ItemsArr);
     }
+
+    if (hasFCH && noMappedPubAddresses) {
+      if (hasOneFCH) {
+        fio101ItemsArr = [
+          {
+            ...FIO_101_SLIDER_CONTENT.NO_MAPPED_PUBLIC_ADDRESSES,
+            link:
+              FIO_101_SLIDER_CONTENT.NO_MAPPED_PUBLIC_ADDRESSES.oneItemLink +
+              `?${QUERY_PARAMS_NAMES.FIO_HANDLE}=${firstFromListFioAddressName}`,
+          },
+          FIO_101_SLIDER_CONTENT.NO_DOMAINS,
+          FIO_101_SLIDER_CONTENT.DEFAULT,
+          FIO_101_SLIDER_CONTENT.NO_FCH,
+        ];
+      } else {
+        fio101ItemsArr = [
+          FIO_101_SLIDER_CONTENT.NO_MAPPED_PUBLIC_ADDRESSES,
+          FIO_101_SLIDER_CONTENT.NO_DOMAINS,
+          FIO_101_SLIDER_CONTENT.DEFAULT,
+          FIO_101_SLIDER_CONTENT.NO_FCH,
+        ];
+      }
+    }
+
+    if (!hasFCH) {
+      fio101ItemsArr = [
+        FIO_101_SLIDER_CONTENT.NO_FCH,
+        FIO_101_SLIDER_CONTENT.NO_MAPPED_PUBLIC_ADDRESSES,
+        FIO_101_SLIDER_CONTENT.NO_DOMAINS,
+        FIO_101_SLIDER_CONTENT.DEFAULT,
+      ];
+    }
+
+    return fio101ItemsArr;
   }, [
     firstFromListFioAddressName,
     hasDomains,
     hasFCH,
     hasOneFCH,
-    loading,
+    noMappedPubAddresses,
+  ]);
+
+  const [fio101Items, setFio101Items] = useState<Fio101SliderContentProps[]>(
+    prepareSlides,
+  );
+
+  useEffect(() => {
+    setFio101Items(prepareSlides);
+  }, [
+    prepareSlides,
+    firstFromListFioAddressName,
+    hasDomains,
+    hasFCH,
+    hasOneFCH,
     noMappedPubAddresses,
   ]);
 
