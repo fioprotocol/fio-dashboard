fba3e33f0e64855b37fad872bcf028613b0a5b20
Oleksii Trukhanov <trukhantech@gmail.com>
2024-06-04 19:36:36 +0300

    DASH-24 Show branded header logo and footer text if ref profile has branded parameter on.

--- client/src/redux/refProfile/selectors.ts
+++ client/src/redux/refProfile/selectors.ts
@@ -5,6 +5,6 @@ import { RefProfile } from '../../types';
 
 export const loading = (state: ReduxState): boolean => state[prefix].loading;
 export const refProfileInfo = (state: ReduxState): RefProfile | null =>
-  state[prefix].data;
+  state[prefix]?.data;
 export const refLinkError = (state: ReduxState): string | null =>
   state[prefix].refLinkError;
