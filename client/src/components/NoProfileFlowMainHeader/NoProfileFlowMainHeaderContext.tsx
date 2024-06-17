96821794d2c0e06bbcb01530467adb18b718ff88
Oleksii Trukhanov <trukhantech@gmail.com>
2024-06-17 15:20:25 +0300

    DASH-24 Fix no profile main header is active state.

--- client/src/components/NoProfileFlowMainHeader/NoProfileFlowMainHeaderContext.tsx
+++ client/src/components/NoProfileFlowMainHeader/NoProfileFlowMainHeaderContext.tsx
@@ -1,31 +1,43 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import Cookies from 'js-cookie';
 
 import { refProfileInfo } from '../../redux/refProfile/selectors';
+import { pathname as pathnameSelector } from '../../redux/navigation/selectors';
 
 import useQuery from '../../hooks/useQuery';
 
-import { RefProfile } from '../../types';
 import { QUERY_PARAMS_NAMES } from '../../constants/queryParams';
 
+import { RefProfile } from '../../types';
+
 type UseContextProps = {
-  activeEventKey: number;
+  activeEventKey: string;
   publicKey?: string;
   refProfile: RefProfile;
-  setActiveEventKey: (activeKey: number) => void;
+  handleEventKeySelect: (activeKey: string) => void;
 };
 
 export const useContext = (): UseContextProps => {
   const refProfile = useSelector(refProfileInfo);
   const queryParams = useQuery();
+  const pathname = useSelector(pathnameSelector);
 
-  const [activeEventKey, setActiveEventKey] = useState(0);
+  const [activeEventKey, setActiveEventKey] = useState<string>(pathname);
+
+  const handleEventKeySelect = useCallback((eventKey: string) => {
+    setActiveEventKey(eventKey);
+  }, []);
 
   const publicKeyQueryParam = queryParams.get(QUERY_PARAMS_NAMES.PUBLIC_KEY);
   const publicKeyCookie = Cookies.get(QUERY_PARAMS_NAMES.PUBLIC_KEY);
 
   const publicKey = publicKeyQueryParam || publicKeyCookie;
 
-  return { activeEventKey, publicKey, refProfile, setActiveEventKey };
+  return {
+    activeEventKey,
+    publicKey,
+    refProfile,
+    handleEventKeySelect,
+  };
 };
