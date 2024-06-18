560f2074bd5d2ec429813014464dd5f5e443e8e6
Oleksii Trukhanov <trukhantech@gmail.com>
2024-06-18 18:50:02 +0300

    DASH-24 Handle multiple domains list on FIO register on no profile flow.

--- client/src/components/NoProfileAddressWidget/NoProfileAddressWidget.tsx
+++ client/src/components/NoProfileAddressWidget/NoProfileAddressWidget.tsx
@@ -25,6 +25,7 @@ export const NoProfileAddressWidget: React.FC<Props> = props => {
   return (
     <AddressWidget
       {...addressWidgetContent}
+      hasRoundRadius
       classNameActionText={classes.actionText}
       title={<div className={classes.title}>{addressWidgetContent?.title}</div>}
       subtitle={
