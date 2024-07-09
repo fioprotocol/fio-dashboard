43a366f16acfb8b8049c96ac74e3bb740e5ac81d
Kyryl Petkov <kirill.petkov@ditto.trade>
2024-07-09 12:16:44 +0400

    DASH-1048 add multiwallet address registration for alternative payment methods

--- client/src/pages/CheckoutPage/components/BeforeSubmitWalletConfirm.tsx
+++ client/src/pages/CheckoutPage/components/BeforeSubmitWalletConfirm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 
 import WalletAction from '../../../components/WalletAction/WalletAction';
 import BeforeSubmitEdgeWallet from './BeforeSubmitEdgeWallet';
@@ -6,29 +6,44 @@ import { BeforeSubmitMetamaskWallet } from './BeforeSubmitMetamaskWallet';
 
 import BeforeSubmitLedgerWallet from './BeforeSubmitLedgerWallet';
 
-import { CONFIRM_PIN_ACTIONS } from '../../../constants/common';
+import {
+  CONFIRM_PIN_ACTIONS,
+  WALLET_CREATED_FROM,
+} from '../../../constants/common';
 
-import { BeforeSubmitProps } from '../types';
+import {
+  BeforeSubmitData,
+  BeforeSubmitProps,
+  BeforeSubmitValues,
+  SignFioAddressItem,
+} from '../types';
+import { FioWalletDoublet } from '../../../types';
 
 const BeforeSubmitWalletConfirm: React.FC<BeforeSubmitProps> = props => {
   const {
     fioWallet,
     fee,
     onCancel,
-    onSuccess,
+    onSuccess: onProcessingEnd,
     submitData,
     processing,
     setProcessing,
   } = props;
 
+  const { signInValuesGroup, onSuccess } = useMultipleWalletAction(
+    fioWallet,
+    submitData,
+    onProcessingEnd,
+  );
+
   return (
     <>
       <WalletAction
-        fioWallet={fioWallet}
+        fioWallet={signInValuesGroup?.signInFioWallet}
         fee={fee}
         onCancel={onCancel}
         onSuccess={onSuccess}
-        submitData={submitData}
+        submitData={signInValuesGroup?.submitData}
         processing={processing}
         setProcessing={setProcessing}
         action={CONFIRM_PIN_ACTIONS.REGISTER_ADDRESS_PRIVATE_DOMAIN}
@@ -38,6 +53,103 @@ const BeforeSubmitWalletConfirm: React.FC<BeforeSubmitProps> = props => {
       />
     </>
   );
+};
+
+type GroupedBeforeSubmitValues = {
+  signInFioWallet: FioWalletDoublet;
+  submitData: BeforeSubmitValues;
+};
+
+const WALLET_TYPE_SIGN_IN_ORDER = [
+  WALLET_CREATED_FROM.EDGE,
+  WALLET_CREATED_FROM.METAMASK,
+  WALLET_CREATED_FROM.LEDGER,
+  WALLET_CREATED_FROM.WITHOUT_REGISTRATION,
+];
+
+const useMultipleWalletAction = (
+  fioWallet: FioWalletDoublet,
+  submitData?: BeforeSubmitValues,
+  onCollectedSuccess?: (results: BeforeSubmitData) => void,
+) => {
+  const onCollectedSuccessRef = useRef(onCollectedSuccess);
+  onCollectedSuccessRef.current = onCollectedSuccess;
+
+  const [result, setResult] = useState<BeforeSubmitData>({});
+  const [groupedBeforeSubmitValues, setGroupedBeforeSubmitValues] = useState<
+    GroupedBeforeSubmitValues[]
+  >([]);
+
+  useEffect(() => {
+    if (!submitData) {
+      return;
+    }
+
+    const { fioAddressItems } = submitData;
+
+    setResult({});
+
+    if (fioAddressItems.length === 0) {
+      setGroupedBeforeSubmitValues([]);
+      return;
+    }
+
+    const result: GroupedBeforeSubmitValues[] = [];
+
+    fioAddressItems.forEach((fioAddressItem: SignFioAddressItem) => {
+      const signInFioWallet = fioAddressItem.fioWallet;
+      let group = result.find(
+        it => it.signInFioWallet.publicKey === signInFioWallet.publicKey,
+      );
+
+      if (!group) {
+        group = {
+          signInFioWallet,
+          submitData: { fioAddressItems: [] },
+        };
+        result.push(group);
+      }
+
+      group.submitData.fioAddressItems.push(fioAddressItem);
+    });
+
+    setResult({});
+    setGroupedBeforeSubmitValues(result);
+  }, [fioWallet, submitData]);
+
+  useEffect(() => {
+    if (
+      groupedBeforeSubmitValues.length === 0 &&
+      Object.keys(result).length > 0
+    ) {
+      onCollectedSuccessRef.current?.(result);
+    }
+  }, [groupedBeforeSubmitValues, result]);
+
+  const onSuccess = (data: BeforeSubmitData) => {
+    setGroupedBeforeSubmitValues(groupedBeforeSubmitValues.slice(1));
+    setResult(result => ({ ...result, ...data }));
+  };
+
+  const sortedGroupedBeforeSubmitValues = useMemo(() => {
+    const groupedBeforeSubmitValuesToSort = [...groupedBeforeSubmitValues];
+
+    groupedBeforeSubmitValuesToSort.sort((g1, g2) => {
+      const g1Priority = WALLET_TYPE_SIGN_IN_ORDER.indexOf(
+        g1.signInFioWallet.from,
+      );
+      const g2Priority = WALLET_TYPE_SIGN_IN_ORDER.indexOf(
+        g2.signInFioWallet.from,
+      );
+      return g1Priority - g2Priority;
+    });
+
+    return groupedBeforeSubmitValuesToSort;
+  }, [groupedBeforeSubmitValues]);
+
+  const [signInValuesGroup] = sortedGroupedBeforeSubmitValues;
+
+  return { onSuccess, signInValuesGroup };
 };
 
 export default BeforeSubmitWalletConfirm;
