d88dac7d1d4d909d5fb2ba76f0fd8db100d51632
Kyrylo Petkov <sardonyx.wt@gmail.com>
2024-08-02 12:06:49 +0400

    DASH-1077 add renew address endpoint

--- server/src/router.mjs
+++ server/src/router.mjs
@@ -294,6 +294,7 @@ const publicApiRouter = express.Router();
 
 publicApiRouter.post('/buy-address', routes.publicApi.buyAddress);
 publicApiRouter.post('/renew', routes.publicApi.renew);
+publicApiRouter.post('/renew-address', routes.publicApi.renew);
 publicApiRouter.post('/summary', routes.publicApi.summary);
 publicApiRouter.get('/is-domain-public/:domain', routes.publicApi.isDomainPublic);
 publicApiRouter.get('/get-domains/:referralCode', routes.publicApi.getDomains);
