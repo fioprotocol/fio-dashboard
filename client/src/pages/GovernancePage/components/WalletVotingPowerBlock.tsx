3c78e3b09be09ebd1282c77fc17c52f20b094d98
Kyrylo Petkov <sardonyx.wt@gmail.com>
2024-10-27 19:35:21 +0200

    DASH-1114 Overview tab interface

---
+++ client/src/pages/GovernancePage/components/WalletVotingPowerBlock.tsx
@@ -0,0 +1,49 @@
+import { FC } from 'react';
+
+import WalletIcon from '../../../assets/images/wallet.svg';
+
+import classes from '../styles/WalletVotingPowerBlock.module.scss';
+import ActionButton from '../../SettingsPage/components/ActionButton';
+import { WalletVoteStatus } from './WalletVoteStatus';
+import { WalletPower } from './WalletPower';
+
+export type WalletVotingPowerBlockProps = {
+  name: string;
+  power: number;
+  boardVote: boolean | 'proxied';
+  blockProducerVote: boolean | 'proxied';
+};
+
+export const WalletVotingPowerBlock: FC<WalletVotingPowerBlockProps> = ({
+  name,
+  power,
+  boardVote,
+  blockProducerVote,
+}) => {
+  const votes = [
+    { name: 'FIO Board Vote', vote: boardVote },
+    { name: 'Block Producer Vote', vote: blockProducerVote },
+  ];
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.titleContainer}>
+        <img src={WalletIcon} alt="wallet" loading="lazy" />
+        <h5 className={classes.title}>{name}</h5>
+      </div>
+      <WalletPower power={power} withLabel={true} />
+      <div className={classes.votesContainer}>
+        {votes.map(({ name, vote }) => (
+          <WalletVoteStatus key={name} name={name} vote={vote} />
+        ))}
+      </div>
+      <ActionButton
+        className={classes.actionButton}
+        title="View"
+        href="https://"
+        isIndigo
+        isSmall
+      />
+    </div>
+  );
+};
---
+++ client/src/pages/GovernancePage/components/WalletVotingPowerTable.tsx
@@ -0,0 +1,62 @@
+import { FC, Fragment } from 'react';
+
+import classnames from 'classnames';
+
+import classes from '../styles/WalletVotingPowerTable.module.scss';
+import WalletIcon from '../../../assets/images/wallet.svg';
+import ActionButton from '../../SettingsPage/components/ActionButton';
+import { WalletPower } from './WalletPower';
+import { WalletVoteStatus } from './WalletVoteStatus';
+
+export type WalletVotingData = {
+  name: string;
+  power: number;
+  boardVote: boolean | 'proxied';
+  blockProducerVote: boolean | 'proxied';
+};
+
+export type WalletVotingPowerTableProps = {
+  className: string;
+  data: WalletVotingData[];
+};
+
+export const WalletVotingPowerTable: FC<WalletVotingPowerTableProps> = ({
+  className,
+  data,
+}) => {
+  return (
+    <div className={classnames(classes.grid, className)}>
+      <div className={classes.gridTitleItem}>Wallet Name</div>
+      <div className={classes.gridTitleItem}>Current Voting Power</div>
+      <div className={classes.gridTitleItem}>FIO Board Vote</div>
+      <div className={classes.gridTitleItem}>Block Producer Vote</div>
+      <div className={classes.gridTitleItem}>Voting Details</div>
+      {data.map(it => (
+        <Fragment key={it.name}>
+          <div className={classes.gridItem}>
+            <img src={WalletIcon} alt="wallet" loading="lazy" />
+            <h5 className={classes.title}>{it.name}</h5>
+          </div>
+          <div className={classes.gridItem}>
+            <WalletPower power={it.power} />
+          </div>
+          <div className={classes.gridItem}>
+            <WalletVoteStatus vote={it.boardVote} />
+          </div>
+          <div className={classes.gridItem}>
+            <WalletVoteStatus vote={it.blockProducerVote} />
+          </div>
+          <div className={classes.gridItem}>
+            <ActionButton
+              className={classes.actionButton}
+              title="View"
+              href="https://"
+              isIndigo
+              isSmall
+            />
+          </div>
+        </Fragment>
+      ))}
+    </div>
+  );
+};
