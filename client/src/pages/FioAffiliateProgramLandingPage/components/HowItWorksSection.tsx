842f415199eabe6ecc4ec0459c7397961f2d85f1
andreyvEze <andreyv@eze.tech>
2024-10-29 14:18:18 +0200

    Text and ui changes for auth and non auth affiliate page

--- client/src/pages/FioAffiliateProgramLandingPage/components/HowItWorksSection.tsx
+++ client/src/pages/FioAffiliateProgramLandingPage/components/HowItWorksSection.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import classnames from 'classnames';
 import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
 
 import { LinksSection } from './LinksSection';
 
 import { ROUTES } from '../../../constants/routes';
+
+import c1 from '../../../assets/images/affiliate/c_1.png';
+import c2 from '../../../assets/images/affiliate/c_2.png';
+import c3 from '../../../assets/images/affiliate/c_3.png';
 
 import { CommonComponentProps } from '../types';
 
@@ -14,14 +19,16 @@ const generalInstructionContent = [
   {
     title: 'Connect',
     text:
-      'You know the drill. Share your link across friends, families and favorite networks.',
+      'Share your FIO Domain and FIO Handle affiliate links across friends, families, and favorite networks.',
   },
   {
     title: 'Earn',
     text:
-      "Get creative: you'll earn FIO Tokens on every domain purchased via the referral link.",
+      'Get creative: you’ll earn on each purchase thought your referral links.',
   },
 ];
+
+const carouselItems = [c1, c2, c3];
 
 export const HowItWorksSection: React.FC<CommonComponentProps> = props => {
   const {
@@ -35,6 +42,27 @@ export const HowItWorksSection: React.FC<CommonComponentProps> = props => {
     <section
       className={classnames(classes.containerLayout, classes.sectionLayout)}
     >
+      <Slider
+        className={classes.slider}
+        dots
+        infinite
+        arrows={false}
+        autoplay
+        autoplaySpeed={5000}
+        appendDots={dots => <ul className={classes.sliderDots}>{dots}</ul>}
+        dotsClass={classes.sliderDots}
+        customPaging={() => <div className={classes.dot}></div>}
+      >
+        {carouselItems.map(item => (
+          <div>
+            <div
+              key={item}
+              className={classes.img}
+              style={{ backgroundImage: `url(${item})` }}
+            />
+          </div>
+        ))}
+      </Slider>
       <h1 className={classes.title}>How It Works</h1>
       <div className={classes.instruction}>
         <div className={classes.instructionRow}>
@@ -46,23 +74,23 @@ export const HowItWorksSection: React.FC<CommonComponentProps> = props => {
                 <Link to={ROUTES.FIO_ADDRESSES_SELECTION}>
                   Get yourself a FIO Handle
                 </Link>{' '}
-                and activate affiliate link. Already have a FIO Handle? Even
-                simpler, just{' '}
+                and activate your FIO Domain and FIO Handle affiliate links.
+                Already have a FIO Handle? Even simpler, just{' '}
                 {/* eslint-disable-next-line no-script-url,jsx-a11y/anchor-is-valid */}
                 <a href="javascript:void(0);" onClick={showAffiliateModal}>
-                  activate your link
+                  activate your links
                 </a>
               </div>
             ) : (
               <div className={classes.instructionRowInfoText}>
                 <Link to={ROUTES.CREATE_ACCOUNT}>Create an account</Link>, get
-                yourself a FIO Handle and activate affiliate link. Already have
-                an account? Even simpler,{' '}
+                yourself a FIO Handle and activate your FIO Domain and FIO
+                Handle affiliate links. Already have an account? Even simpler,{' '}
                 {/* eslint-disable-next-line no-script-url,jsx-a11y/anchor-is-valid */}
                 <a href="javascript:void(0);" onClick={showLogin}>
                   sign in
                 </a>{' '}
-                & activate your link.
+                & activate your links.
               </div>
             )}
           </div>
