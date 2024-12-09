dfe568533106e7e2b2db60b70db64f413be769d8
andrewEze <andreyv@eze.tech>
2024-12-09 19:08:49 +0200

    DASH-1237. Refactor use get active cart for cart endpoints.

--- server/src/services/cart/HandleUsersFreeCartItems.mjs
+++ server/src/services/cart/HandleUsersFreeCartItems.mjs
@@ -23,12 +23,7 @@ export default class HandleUsersFreeCartItems extends Base {
       const userId = this.context.id || null;
       const guestId = this.context.guestId || null;
 
-      const where = {};
-      if (userId) where.userId = userId;
-      if (guestId) where.guestId = guestId;
-
-      // todo: get active
-      const cart = await Cart.findOne({ where });
+      const cart = await Cart.getActive({ userId, guestId });
 
       if (!cart) {
         return {
