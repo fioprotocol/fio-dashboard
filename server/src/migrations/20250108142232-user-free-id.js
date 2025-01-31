9dbd121d1a0963265dbd1f56d6fd5051c816d05d
andrewEze <andreyv@eze.tech>
2025-01-31 15:19:56 +0200

    DASH-1254. Optimize users free id migration.

--- server/src/migrations/20250108142232-user-free-id.js
+++ server/src/migrations/20250108142232-user-free-id.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const USER_PROFILE_TYPE = {
+  PRIMARY: 'PRIMARY',
+  ALTERNATIVE: 'ALTERNATIVE',
+  WITHOUT_REGISTRATION: 'WITHOUT_REGISTRATION',
+};
+
 module.exports = {
   up: async (QI, DT) => {
     await QI.sequelize.transaction(async t => {
@@ -17,56 +23,87 @@ module.exports = {
         },
       );
 
-      const limit = 20000;
-      let finish = false;
-      while (!finish) {
-        const users = await QI.sequelize.query(
-          `
+      const setFreeId = async userType => {
+        const limit = 20000;
+        let finish = false;
+        while (!finish) {
+          const users = await QI.sequelize.query(
+            `
           SELECT id, username, "userProfileType"
           FROM "users"
-          WHERE "freeId" IS NULL
+          WHERE "freeId" IS NULL AND "userProfileType" = '${userType}'
           ORDER BY "id" ASC
           LIMIT ${limit};
         `,
-          { type: QI.sequelize.QueryTypes.SELECT, transaction: t },
-        );
-        if (users && users.length) {
-          const usersToUpdate = [];
-          for (const user of users) {
-            let freeId = user.username || user.id;
-            if (user.userProfileType !== 'PRIMARY') {
-              try {
-                const wallets = await QI.sequelize.query(
-                  `
-              SELECT id, "publicKey", "from"
-              FROM "wallets"
-              WHERE "userId" = '${user.id}' AND (("from" = 'METAMASK' AND "data"->>'derivationIndex' = '0') OR "from" = 'WITHOUT_REGISTRATION')
-              ORDER BY "createdAt" ASC
-              LIMIT 1;
-            `,
-                  { type: QI.sequelize.QueryTypes.SELECT, transaction: t },
-                );
-                freeId = wallets[0].publicKey;
-                usersToUpdate.push({ id: user.id, freeId });
-              } catch (err) {
-                // no wallets found for freeId value
+            { type: QI.sequelize.QueryTypes.SELECT, transaction: t },
+          );
 
-                if (user.userProfileType === 'WITHOUT_REGISTRATION') {
-                  await QI.sequelize.query(
-                    `
-                    DELETE FROM "users"
-                    WHERE "id" = '${user.id}';
-                  `,
-                    { type: QI.sequelize.QueryTypes.DELETE, transaction: t },
-                  );
-                }
-              }
-            } else {
-              usersToUpdate.push({ id: user.id, freeId });
+          if (users && users.length) {
+            let usersToUpdate = [];
+            if (userType === USER_PROFILE_TYPE.PRIMARY) {
+              usersToUpdate = users.map(user => ({
+                id: user.id,
+                freeId: user.username || user.id,
+              }));
             }
-          }
-          await QI.sequelize.query(
-            `
+
+            if (userType === USER_PROFILE_TYPE.WITHOUT_REGISTRATION) {
+              const wallets = await QI.sequelize.query(
+                `
+                  SELECT id, "publicKey", "from", "userId"
+                  FROM "wallets"
+                  WHERE "userId" IN(${users
+                    .map(({ id }) => `'${id}'`)
+                    .join(',')}) AND "from" = 'WITHOUT_REGISTRATION'
+                `,
+                { type: QI.sequelize.QueryTypes.SELECT, transaction: t },
+              );
+              usersToUpdate = wallets.map(({ userId, publicKey }) => ({
+                id: userId,
+                freeId: publicKey || userId,
+              }));
+              const walletsMap = wallets.reduce((acc, wallet) => {
+                acc[wallet.userId] = wallet.publicKey;
+                return acc;
+              }, {});
+
+              const noWalletUsers = users.filter(({ id }) => !walletsMap[id]);
+
+              if (noWalletUsers.length) {
+                // no wallets found for freeId value
+                await QI.sequelize.query(
+                  `
+                    DELETE FROM "users"
+                    WHERE "id" IN(${noWalletUsers.map(({ id }) => `'${id}'`).join(',')})
+                  `,
+                  { type: QI.sequelize.QueryTypes.DELETE, transaction: t },
+                );
+              }
+            }
+
+            if (userType === USER_PROFILE_TYPE.ALTERNATIVE) {
+              const wallets = await QI.sequelize.query(
+                `
+                  SELECT id, "publicKey", "from", "userId"
+                  FROM "wallets"
+                  WHERE "userId" IN(${users.map(({ id }) => `'${id}'`).join(',')}) 
+                    AND "from" = 'METAMASK' AND "data"->>'derivationIndex' = '0';
+                `,
+                { type: QI.sequelize.QueryTypes.SELECT, transaction: t },
+              );
+              const walletsMap = wallets.reduce((acc, wallet) => {
+                acc[wallet.userId] = wallet.publicKey;
+                return acc;
+              }, {});
+
+              usersToUpdate = users.map(({ id }) => ({
+                id,
+                freeId: walletsMap[id] || id,
+              }));
+            }
+
+            await QI.sequelize.query(
+              `
             UPDATE "users"
             SET "freeId" = data_table.free_id
             FROM
@@ -80,12 +117,17 @@ module.exports = {
                           .join(',')}]) as free_id) as data_table
             WHERE id = data_table.u_id;
           `,
-            { type: QI.sequelize.QueryTypes.UPDATE, transaction: t },
-          );
-        } else {
-          finish = true;
+              { type: QI.sequelize.QueryTypes.UPDATE, transaction: t },
+            );
+          } else {
+            finish = true;
+          }
         }
-      }
+      };
+
+      await setFreeId(USER_PROFILE_TYPE.PRIMARY);
+      await setFreeId(USER_PROFILE_TYPE.ALTERNATIVE);
+      await setFreeId(USER_PROFILE_TYPE.WITHOUT_REGISTRATION);
     });
   },
 
