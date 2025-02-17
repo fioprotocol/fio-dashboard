e69cfcb66fe2fce8af5091809fa103c204fcb6d4
andrewEze <andreyv@eze.tech>
2025-02-17 09:42:50 +0200

    DASH-1323. Refactor types for EIP6963 provider check changes.

--- client/src/util/ethereum.ts
+++ client/src/util/ethereum.ts
@@ -1,47 +1,9 @@
+import {
+  EIP6963AnnounceProviderEvent,
+  EIP6963ProviderDetail,
+} from '@metamask/providers';
+
 import { log } from './general';
-
-declare global {
-  interface WindowEventMap {
-    'eip6963:announceProvider': CustomEvent<EIP6963AnnounceProviderEvent>;
-  }
-}
-
-interface EIP1193Provider {
-  isStatus?: boolean;
-  host?: string;
-  path?: string;
-  sendAsync?: (
-    request: { method: string; params?: Array<unknown> },
-    callback: (error: Error | null, response: unknown) => void,
-  ) => void;
-  send?: (
-    request: { method: string; params?: Array<unknown> },
-    callback: (error: Error | null, response: unknown) => void,
-  ) => void;
-  request: (request: {
-    method: string;
-    params?: Array<unknown>;
-  }) => Promise<unknown>;
-}
-
-interface EIP6963ProviderInfo {
-  rdns: string;
-  uuid: string;
-  name: string;
-  icon: string;
-}
-
-interface EIP6963ProviderDetail {
-  info: EIP6963ProviderInfo;
-  provider: EIP1193Provider;
-}
-
-type EIP6963AnnounceProviderEvent = {
-  detail: {
-    info: EIP6963ProviderInfo;
-    provider: Readonly<EIP1193Provider>;
-  };
-};
 
 let providers: EIP6963ProviderDetail[] = [];
 const subscribe = () => {
