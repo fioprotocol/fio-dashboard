fe7ced5fc1e30eeb4d3072d08571d5fb951bcb4e
Oleksii Trukhanov <trukhantech@gmail.com>
2025-08-04 17:26:02 +0300

    DASH-1412 Handle get transaction result if it has executed:false result. Try to get data from other servers.

--- server/src/external/fio-history.mjs
+++ server/src/external/fio-history.mjs
@@ -37,7 +37,10 @@ export default class FioHistory {
       block_hint: 0,
     }).toString();
 
-    for (const url of this.historyNodeUrls) {
+    for (let i = 0; i < this.historyNodeUrls.length; i++) {
+      const url = this.historyNodeUrls[i];
+      const isLastUrl = i === this.historyNodeUrls.length - 1;
+
       try {
         const result = await fetchWithRateLimit({
           url: `${url}history/get_transaction?${queryString}`,
@@ -46,7 +49,20 @@ export default class FioHistory {
 
         const data = await result.json();
 
-        return data;
+        // If we have data and executed is true, return immediately
+        if (data && data.executed !== false) {
+          return data;
+        }
+
+        // If this is the last URL, return whatever we got
+        if (isLastUrl) {
+          return data;
+        }
+
+        // Otherwise, continue to next URL
+        logger.info(
+          `Transaction ${transactionId} has executed: false or no result for URL: ${url}, trying next URL`,
+        );
       } catch (err) {
         logger.error(
           `Get FIO History For Transaction: ${transactionId} Error for URL: ${url}`,
